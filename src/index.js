986a8b656e82b48a0d1cfb7268337ad1da02d462
Kaka Huynh <dunghd.it@gmail.com>
2018-07-31 16:29:15 +0700

    feat(error report): add logrocket for replay session

--- src/index.js
+++ src/index.js
@@ -2,21 +2,18 @@
 import * as React from 'react';
 import { render } from 'react-dom';
 import { ApolloProvider } from 'react-apollo';
-import Raven from 'raven-js';
 import FullStory from 'react-fullstory';
 import LanguageProvider from './modules/language';
 import UIProvider from './modules/ui';
-import { isDevEnvironment, isAppInProduction } from './utils/env';
 import apolloClient from './apollo';
 import Routes from './routes';
 import loadFonts from './fonts';
+import { isAppInProduction } from './utils/env';
+import errorReport from './errorReport';
 import './styles/reset.css';
 
 loadFonts();
-
-if (!isDevEnvironment) {
-  Raven.config(process.env.ZENPORT_SENTRY_URL || '').install();
-}
+errorReport();
 
 const container = document.querySelector('#root');
 
@@ -24,22 +21,21 @@ if (!container) {
   throw new Error(`couldn't find element with id root`);
 }
 
-/* eslint-disable react/jsx-filename-extension */
 const renderApp = Component => {
   render(
-    <ApolloProvider client={apolloClient}>
-      <LanguageProvider>
-        <UIProvider>
-          <React.Fragment>
-            {isAppInProduction && <FullStory org={process.env.ZENPORT_FULLSTORY_ID} />}
+    <React.Fragment>
+      {isAppInProduction && <FullStory org={process.env.ZENPORT_FULLSTORY_ID} />}
+      <ApolloProvider client={apolloClient}>
+        <LanguageProvider>
+          <UIProvider>
             {/* $FlowFixMe: React Flow typings are not updated to React 16.3 yet */}
             <React.StrictMode>
               <Component />
             </React.StrictMode>
-          </React.Fragment>
-        </UIProvider>
-      </LanguageProvider>
-    </ApolloProvider>,
+          </UIProvider>
+        </LanguageProvider>
+      </ApolloProvider>
+    </React.Fragment>,
     container
   );
 };
