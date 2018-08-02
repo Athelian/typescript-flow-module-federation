7838676466b0378c3191cc13133704113f3f27d4
Kaka Huynh <dunghd.it@gmail.com>
2018-08-02 15:36:41 +0700

    fix(order): calculate the total columns base on browser width

--- src/modules/order/list/components/OrderGridView.js
+++ src/modules/order/list/components/OrderGridView.js
@@ -1,5 +1,9 @@
 // @flow
 import * as React from 'react';
+import {
+  AutoSizer,
+  /* $FlowFixMe: not have flow type yet */
+} from 'react-virtualized';
 import GridView from 'components/GridView';
 import LoadingIcon from 'components/LoadingIcon';
 import OrderItem from './OrderItem';
@@ -9,41 +13,48 @@ type Props = {
   onLoadMore: Function,
   hasMore: boolean,
   isLoading: boolean,
-  totalColumns?: number,
 };
 
-function OrderGridView({ items, onLoadMore, isLoading, hasMore, totalColumns = 3 }: Props) {
-  const isRowLoaded = ({ index }) => !hasMore || index < items.length;
-  return (
-    <GridView
-      hasNextPage={hasMore}
-      isNextPageLoading={isLoading}
-      onLoadNextPage={onLoadMore}
-      list={items}
-      width={window.outerWidth}
-      height={window.outerHeight}
-      rowCount={Math.ceil(items.length / totalColumns)}
-      rowHeight={150}
-      columnWidth={200}
-      columnCount={totalColumns}
-      cellRenderer={({ key, columnIndex, rowIndex, style }) =>
-        isRowLoaded({ index: rowIndex * totalColumns + columnIndex }) ? (
-          <div key={key} style={style}>
-            <h3>#{rowIndex * totalColumns + columnIndex}</h3>
-            <OrderItem order={items[rowIndex * totalColumns + columnIndex]} key={key} />
-          </div>
-        ) : (
-          <div key={key} style={style}>
-            <LoadingIcon />
-          </div>
-        )
-      }
-    />
-  );
+function totalColumns(width, columnWidth) {
+  return parseInt(width / columnWidth, 10) || 1;
 }
 
-OrderGridView.defaultProps = {
-  totalColumns: 3,
-};
+function OrderGridView({ items, onLoadMore, isLoading, hasMore }: Props) {
+  const isRowLoaded = ({ index }) => !hasMore || index < items.length;
+  const columnWidth = 200;
+  return (
+    <AutoSizer disableHeight>
+      {({ width }) => (
+        <GridView
+          hasNextPage={hasMore}
+          isNextPageLoading={isLoading}
+          onLoadNextPage={onLoadMore}
+          list={items}
+          width={window.outerWidth}
+          height={window.outerHeight}
+          rowCount={Math.ceil(items.length / totalColumns(width, columnWidth))}
+          rowHeight={150}
+          columnWidth={columnWidth}
+          columnCount={totalColumns(width, columnWidth)}
+          cellRenderer={({ key, columnIndex, rowIndex, style }) =>
+            isRowLoaded({ index: rowIndex * totalColumns(width, columnWidth) + columnIndex }) ? (
+              <div key={key} style={style}>
+                <h3>#{rowIndex * totalColumns(width, columnWidth) + columnIndex}</h3>
+                <OrderItem
+                  order={items[rowIndex * totalColumns(width, columnWidth) + columnIndex]}
+                  key={key}
+                />
+              </div>
+            ) : (
+              <div key={key} style={style}>
+                <LoadingIcon />
+              </div>
+            )
+          }
+        />
+      )}
+    </AutoSizer>
+  );
+}
 
 export default OrderGridView;
