69cc038457df4af76a977a4743c182b9c98ffbfa
Kaka Huynh <dunghd.it@gmail.com>
2018-08-08 14:42:10 +0700

    feat(form): add tag input component

--- src/components/Form/TagsInput/index.stories.js
+++ src/components/Form/TagsInput/index.stories.js
@@ -4,16 +4,85 @@ import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import TagsInput from './index';
 
-storiesOf('Form/TagsInput', module).add('with label', () => (
-  <form onSubmit={action('submit')}>
-    <TagsInput
-      required
-      name="qty"
-      label="Qty"
-      onChange={action('onChange')}
-      onBlur={action('onBlur')}
-      value={10}
-      permissions="rw"
-    />
-  </form>
-));
+storiesOf('Form/TagsInput', module)
+  .add('with selected value', () => (
+    <form onSubmit={action('submit')}>
+      <TagsInput
+        tags={[
+          {
+            id: 1,
+            name: 'Tag',
+            description: '',
+            color: '#ccc',
+          },
+          {
+            id: 2,
+            name: 'Tag 2',
+            description: '',
+            color: '#ccc',
+          },
+          {
+            id: 3,
+            name: 'Tag 3',
+            description: '',
+            color: '#ccc',
+          },
+        ]}
+        name="tags"
+        value={[
+          {
+            id: 1,
+            name: 'Tag',
+            description: '',
+            color: '#ccc',
+          },
+        ]}
+        label="Tag Inputs"
+        permissions="rw"
+        onChange={action('onChange')}
+        onBlur={action('onBlur')}
+      />
+    </form>
+  ))
+  .add('readonly', () => (
+    <form onSubmit={action('onSubmit')}>
+      <TagsInput
+        permissions="r"
+        tags={[
+          {
+            id: 1,
+            name: 'Tag',
+            description: '',
+            color: '#ccc',
+          },
+          {
+            id: 2,
+            name: 'Tag 2',
+            description: '',
+            color: '#ccc',
+          },
+          {
+            id: 3,
+            name: 'Tag 3',
+            description: '',
+            color: '#ccc',
+          },
+        ]}
+        name="tags"
+        value={[
+          {
+            id: 1,
+            name: 'Tag',
+            description: '',
+            color: '#ccc',
+          },
+        ]}
+        label="Tag Inputs"
+      />
+    </form>
+  ))
+  .add('no permission', () => (
+    <form onSubmit={action('onSubmit')}>
+      <TagsInput permissions="" />
+    </form>
+  ));
