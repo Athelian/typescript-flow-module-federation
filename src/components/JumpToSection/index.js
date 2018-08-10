12caa004c76294da2d9fab22ede19c02983f7433
Kaka Huynh <dunghd.it@gmail.com>
2018-08-10 21:54:45 +0700

    fix(ui): use intersection observer for section

--- src/components/JumpToSection/index.js
+++ src/components/JumpToSection/index.js
@@ -1,44 +1,80 @@
 // @flow
 import * as React from 'react';
+import logger from 'utils/logger';
 
 type Props = {
   children: React.Node,
+  rootViewPort?: ?HTMLDivElement,
+  threshold?: Array<number>,
 };
+
 type State = {
   activeNode: ?string,
+  ratio: number,
 };
 
 class JumpToSection extends React.PureComponent<Props, State> {
+  static defaultProps = {
+    rootViewPort: null,
+    threshold: [0, 0.2, 0.4, 0.6, 0.8, 1],
+  };
+
   state = {
     activeNode: null,
+    ratio: 0,
   };
 
   componentDidMount() {
-    window.addEventListener('scroll', this.handleScroll);
-    this.handleScroll();
-    this.ensureLastHeightOfSection();
+    logger.warn('create IntersectionObserver');
+    this.sectionIds = [];
+    const { rootViewPort, threshold } = this.props;
+    this.io = new IntersectionObserver(
+      entries => {
+        logger.warn('entries', entries, this.sectionIds);
+        if (entries.length) {
+          const intersectSections = entries.filter(item => item.isIntersecting);
+          const hideSections = entries.filter(item => !item.isIntersecting);
+          logger.warn('intersect', intersectSections);
+          logger.warn('hide', hideSections);
+          const [activeSection] = intersectSections.sort(
+            (first, second) => second.intersectionRatio - first.intersectionRatio
+          );
+          logger.warn('active', activeSection);
+
+          if (!activeSection) {
+            logger.warn('not found active section', this.state);
+            return;
+          }
+
+          const {
+            intersectionRatio,
+            target: { id: activeNode },
+          } = activeSection;
+          const ratio = intersectionRatio * 100;
+          logger.warn('intersectionRatio', ratio);
+          if (ratio > 50) this.setState(() => ({ activeNode, ratio }));
+        }
+      },
+      {
+        threshold,
+        root: rootViewPort,
+        rootMargin: '0px',
+      }
+    );
+    const { children } = this.props;
+    React.Children.forEach(children, child => {
+      const element = document.querySelector(`#${child.props.link}`);
+      if (element) {
+        this.sectionIds.push(child.props.link);
+        this.io.observe(element);
+      }
+    });
   }
 
   componentWillUnmount() {
-    window.removeEventListener('scroll', this.handleScroll);
+    logger.warn('remove IntersectionObserver');
+    this.io.disconnect();
   }
-
-  handleScroll = () => {
-    const { children } = this.props;
-    const ids = React.Children.map(children, child => child.props.link);
-    ids.forEach(id => {
-      const node = document.querySelector(`#${id}`) || {};
-      const { pageYOffset } = window;
-      const nodeYPosition = node.offsetTop + node.offsetHeight;
-
-      const scrollPassedTheTop = pageYOffset >= node.offsetTop;
-      const scrollNotPassedTheBottom = nodeYPosition > pageYOffset;
-      const scrollInNodeArea = scrollPassedTheTop && scrollNotPassedTheBottom;
-      if (scrollInNodeArea) {
-        this.setState({ activeNode: id });
-      }
-    });
-  };
 
   handleClick = (id: string) => () => {
     const node = document.querySelector(`#${id}`);
@@ -47,22 +83,9 @@ class JumpToSection extends React.PureComponent<Props, State> {
     }
   };
 
-  ensureLastHeightOfSection = () => {
-    const { offsetHeight: documentHeight } = document.body || {};
-    const { children } = this.props;
-    const childNum = React.Children.count(children);
-    const lastChild = React.Children.toArray(children)[childNum - 1];
-    const lastSectionId = lastChild && lastChild.props.link;
+  sectionIds: Array<string>;
 
-    const lastSectionNode = document.querySelector(`#${lastSectionId}`) || {};
-    const lastSectionNodePosition = lastSectionNode.offsetTop + lastSectionNode.offsetHeight;
-    const screenHeight = window.screen.height;
-    const topOffsetAtButtonOfScreen = documentHeight - screenHeight;
-    if (lastSectionNode.offsetTop >= topOffsetAtButtonOfScreen) {
-      const growNumber = screenHeight - (documentHeight - lastSectionNodePosition);
-      lastSectionNode.style.minHeight = `${growNumber}px`;
-    }
-  };
+  io: IntersectionObserver;
 
   render() {
     const { children } = this.props;
