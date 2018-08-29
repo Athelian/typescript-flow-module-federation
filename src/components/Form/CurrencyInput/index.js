3f5ee2c3061b417caccaa1d38a3660e51257e72f
shuzo <suzo.yolo@gmail.com>
2018-08-29 14:59:28 +0900

    fix(structure): mv enum inputs into StyledInputs

--- src/components/Form/CurrencyInput/index.js
+++ src/components/Form/CurrencyInput/index.js
@@ -1,5 +1,5 @@
 // @flow
 import React from 'react';
-import EnumInput from 'components/Form/EnumInput';
+import EnumInput from 'components/Form/StyledInputs/StyledEnumInput';
 
 export default (rest: Object) => <EnumInput {...rest} enumType="Currency" />;
