30d6f38884f3442c936c1c78503b2fc14161bcb6
shuzo <suzo.yolo@gmail.com>
2018-08-31 17:12:28 +0900

    chore(type): add pureSearchSelectInputProps for partner select

--- src/components/Form/StyledInputs/StyledSelectInput/StyledSearchSelectInput/PartnerSelectInput/type.js
+++ src/components/Form/StyledInputs/StyledSelectInput/StyledSearchSelectInput/PartnerSelectInput/type.js
@@ -1,11 +1,14 @@
 // @flow
 import { type PureInputProps } from 'components/Form/PureInputs/type';
+import { type PureSearchSelectInputProps } from 'components/Form/PureInputs/PureSearchSelectInput/type';
 
 type OptionalProps = PureInputProps & {
   types: Array<string>,
 };
 
-export type PartnerSelectInputProps = OptionalProps;
+export type PartnerSelectInputProps = OptionalProps & {
+  pureInputOptions: PureSearchSelectInputProps,
+};
 
 export const defaultPartnerSelectInputProps = {
   types: ['Exporter', 'Supplier', 'Forwarder'],
