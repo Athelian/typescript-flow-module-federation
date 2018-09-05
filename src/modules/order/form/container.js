72f1857adee2c6d241cc5588cdf76a0a8d440817
Kaka Huynh <dunghd.it@gmail.com>
2018-09-05 16:04:41 +0900

    fix(order): update order with tags

--- src/modules/order/form/container.js
+++ src/modules/order/form/container.js
@@ -2,6 +2,7 @@
 import { Container } from 'unstated';
 import * as Yup from 'yup';
 import { isEquals } from 'utils/fp';
+import { removeTypename } from 'utils/data';
 import logger from 'utils/logger';
 
 type FormState = {
@@ -25,9 +26,11 @@ export default class OrderFormContainer extends Container<FormState> {
 
   isDirty = (values: any) => !isEquals(values, this.order);
 
-  initDetailValues = (values: any) => {
-    this.setState(values);
-    this.order = values;
+  initDetailValues = (values: Object) => {
+    const parsedValues = removeTypename(values);
+    // $FlowFixMe missing type for map function in ramda
+    this.setState(parsedValues);
+    this.order = parsedValues;
 
     logger.warn('setValues for order detail', values);
     logger.warn('order detail', this.order);
