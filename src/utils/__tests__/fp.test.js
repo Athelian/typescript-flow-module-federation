67cdc9a37b20f4724bd87c5bd9aca7a9cbf9e6f7
Kaka Huynh <dunghd.it@gmail.com>
2018-09-06 22:24:52 +0900

    feat(utils): add setIn helper

--- src/utils/__tests__/fp.test.js
+++ src/utils/__tests__/fp.test.js
@@ -1,4 +1,4 @@
-import { getByPath, getByPathWithDefault } from '../fp';
+import { getByPath, getByPathWithDefault, setIn } from '../fp';
 
 describe('Functional helper', () => {
   it('should return value from object path', () => {
@@ -19,5 +19,35 @@ describe('Functional helper', () => {
     };
     expect(getByPathWithDefault(0, 'a', testObj)).toEqual(1);
     expect(getByPathWithDefault(0, 'd', testObj)).toEqual(0);
+  });
+
+  it('should set new value by path', () => {
+    const testObj = {
+      a: 1,
+      b: {
+        c: 2,
+        d: {
+          e: 3,
+        },
+      },
+    };
+    expect(setIn('b.c', 3, testObj)).toEqual({
+      a: 1,
+      b: {
+        c: 3,
+        d: {
+          e: 3,
+        },
+      },
+    });
+    expect(setIn('b.d.e', 4, testObj)).toEqual({
+      a: 1,
+      b: {
+        c: 2,
+        d: {
+          e: 4,
+        },
+      },
+    });
   });
 });
