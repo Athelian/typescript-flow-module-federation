a43ebd7fe492b4485d840fc125094d2251ae0df3
Kaka Huynh <dunghd.it@gmail.com>
2018-09-07 18:17:02 +0900

    fix(core): comment out log rocket because react 16.5 didn't work

--- src/errorReport.js
+++ src/errorReport.js
@@ -1,16 +1,17 @@
-import LogRocket from 'logrocket';
+// import LogRocket from 'logrocket';
 import Raven from 'raven-js';
-import setupLogRocketReact from 'logrocket-react';
+// import setupLogRocketReact from 'logrocket-react';
 import { isAppInProduction } from './utils/env';
 
 const errorReport = () => {
   if (isAppInProduction) {
-    LogRocket.init(process.env.ZENPORT_LOG_ROCKET_APP_ID);
-    setupLogRocketReact(LogRocket);
+    // LogRocket.init(process.env.ZENPORT_LOG_ROCKET_APP_ID);
+    // setupLogRocketReact(LogRocket);
     Raven.config(process.env.ZENPORT_SENTRY_URL || '').install();
     Raven.setDataCallback(data => {
       const { extra } = data;
-      return { ...data, extra: { ...extra, sessionURL: LogRocket.sessionURL } };
+      // return { ...data, extra: { ...extra, sessionURL: LogRocket.sessionURL } };
+      return { ...data, extra: { ...extra } };
     });
   }
 };
