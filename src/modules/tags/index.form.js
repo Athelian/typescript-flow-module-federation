82fa5142169925d0a160f1475262426103a99693
Jifa Jiang <thaddeusjiang@gmail.com>
2018-09-15 10:39:17 +0800

    feat(tag): update mutation

--- src/modules/tags/index.form.js
+++ src/modules/tags/index.form.js
@@ -1,19 +1,23 @@
 // @flow
 import * as React from 'react';
+import { navigate } from '@reach/router';
 import { Provider, Subscribe } from 'unstated';
 
-import { Query } from 'react-apollo';
+import { Query, Mutation } from 'react-apollo';
 import LoadingIcon from 'components/LoadingIcon';
 import Layout from 'components/Layout';
 import NavBar, { EntityIcon } from 'components/NavBar';
 import { SaveButton, CancelButton } from 'components/NavButtons';
 import { UIConsumer } from 'modules/ui';
-import { decodeId } from 'utils/id';
+import { FormContainer } from 'modules/form';
+import { decodeId, encodeId } from 'utils/id';
 import { getByPathWithDefault } from 'utils/fp';
+import logger from 'utils/logger';
 import TagForm from './form';
 import TagContainer from './form/containers';
 
 import query from './form/query';
+import { createTagMutation, updateTagMutation } from './form/mutation';
 
 type Props = {
   tagId?: string,
@@ -26,53 +30,122 @@ const defaultProps = {
 export default class TagFormContainer extends React.PureComponent<Props> {
   static defaultProps = defaultProps;
 
+  onCancel = () => {
+    navigate('/tags');
+  };
+
+  onSave = (formData: Object, saveTag: Function, onSuccess: Function = () => {}) => {
+    const { tagId } = this.props;
+    const isNew = tagId === 'new';
+
+    const { state } = formData;
+
+    const input = {
+      name: state.name,
+      description: state.description,
+      color: state.color,
+      entityTypes: state.entityTypes,
+    };
+
+    if (isNew) {
+      saveTag({ variables: { input } });
+    } else if (tagId) {
+      saveTag({ variables: { input, id: decodeId(tagId) } });
+    }
+
+    onSuccess();
+  };
+
+  onMutationCompleted = (result: Object) => {
+    const { tagId } = this.props;
+    const isNew = tagId === 'new';
+    if (isNew) {
+      const {
+        tagCreate: {
+          tagId: { id },
+        },
+      } = result;
+      navigate(`/tags/${encodeId(id)}`);
+    }
+    logger.warn('result', result);
+  };
+
   render() {
     const { tagId } = this.props;
     const isNew = tagId === 'new';
+
+    let mutationKey = {};
+    if (tagId && !isNew) {
+      mutationKey = { key: decodeId(tagId) };
+    }
 
     return (
       <Provider>
         <UIConsumer>
           {uiState => (
-            <Layout
-              {...uiState}
-              navBar={
-                <NavBar>
-                  <EntityIcon icon="TAGS" color="ORDER" />
-                  <CancelButton />
-                  <SaveButton />
-                </NavBar>
-              }
+            <Mutation
+              mutation={isNew ? createTagMutation : updateTagMutation}
+              onCompleted={this.onMutationCompleted}
+              {...mutationKey}
             >
-              <div>
-                {isNew || !tagId ? (
-                  <TagForm isNew />
-                ) : (
-                  <Subscribe to={[TagContainer]}>
-                    {tagState => (
-                      <Query
-                        query={query}
-                        variables={{ id: decodeId(tagId) }}
-                        fetchPolicy="network-only"
-                        onCompleted={result => {
-                          const { tag } = result;
-                          tagState.initDetailValues(tag);
-                        }}
-                      >
-                        {({ loading, data, error }) => {
-                          if (error) {
-                            return error.message;
-                          }
-                          if (loading) return <LoadingIcon />;
+              {(saveTag, { loading: isLoading, error: apiError }) => (
+                <Layout
+                  {...uiState}
+                  navBar={
+                    <NavBar>
+                      <EntityIcon icon="TAGS" color="ORDER" />
+                      <Subscribe to={[TagContainer, FormContainer]}>
+                        {(tagState, form) =>
+                          (isNew || tagState.isDirty()) && (
+                            <>
+                              <CancelButton onClick={this.onCancel} />
+                              <SaveButton
+                                disabled={!form.isReady()}
+                                onClick={() =>
+                                  this.onSave({ ...tagState }, saveTag, () => {
+                                    form.onReset();
+                                  })
+                                }
+                              />
+                            </>
+                          )
+                        }
+                      </Subscribe>
+                    </NavBar>
+                  }
+                >
+                  {isLoading && <LoadingIcon />}
+                  {apiError && <p>Error: Please try again.</p>}
 
-                          return <TagForm tag={getByPathWithDefault({}, 'tag', data)} />;
-                        }}
-                      </Query>
-                    )}
-                  </Subscribe>
-                )}
-              </div>
-            </Layout>
+                  {isNew || !tagId ? (
+                    <TagForm isNew />
+                  ) : (
+                    <Subscribe to={[TagContainer]}>
+                      {tagState => (
+                        <Query
+                          query={query}
+                          variables={{ id: decodeId(tagId) }}
+                          fetchPolicy="network-only"
+                          onCompleted={result => {
+                            const { tag } = result;
+                            tagState.initDetailValues(tag);
+                          }}
+                        >
+                          {({ loading, data, error }) => {
+                            if (error) {
+                              return error.message;
+                            }
+                            if (loading) return <LoadingIcon />;
+
+                            return <TagForm tag={getByPathWithDefault({}, 'tag', data)} />;
+                          }}
+                        </Query>
+                      )}
+                    </Subscribe>
+                  )}
+                </Layout>
+              )}
+            </Mutation>
           )}
         </UIConsumer>
       </Provider>
