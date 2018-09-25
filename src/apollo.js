029fece0f3a4d50c8fb0603eb9a32a5465c84845
maxime.perrimond <maxime.perrimond@zenport.io>
2018-09-25 20:14:08 +0900

    feat(subscription): add sse link for apollo client

--- src/apollo.js
+++ src/apollo.js
@@ -1,14 +1,72 @@
 // @flow
 import { navigate } from '@reach/router';
+// $FlowFixMe
+import { getOperationAST } from 'graphql/utilities/getOperationAST';
+// $FlowFixMe
+import { print } from 'graphql/language/printer';
 import { ApolloClient } from 'apollo-client';
 import { InMemoryCache, IntrospectionFragmentMatcher } from 'apollo-cache-inmemory';
-import { ApolloLink } from 'apollo-link';
+import { ApolloLink, Observable, type Operation } from 'apollo-link';
 import { createHttpLink } from 'apollo-link-http';
 import { onError } from 'apollo-link-error';
 import Raven from 'raven-js';
 import { isDevEnvironment } from './utils/env';
 import introspectionQueryResultData from './generated/fragmentTypes.json';
 import logger from './utils/logger';
+
+class SubscriptionSSE {
+  // $FlowFixMe
+  source: ?EventSource;
+
+  subscribe(operation, handler) {
+    const { query, variables, operationName } = operation;
+
+    this.source = new EventSource(
+      encodeURI(
+        `${process.env.ZENPORT_SERVER_URL ||
+          ''}/graphql?query=${query}&operationName=${operationName}&variables=${JSON.stringify(
+          variables
+        )}`
+      )
+    );
+    this.source.onmessage = msg => {
+      handler(JSON.parse(msg.data));
+    };
+    this.source.onerror = msg => {
+      logger.error(msg.message);
+      this.unsubscribe();
+
+      const retryTimeout = setTimeout(() => {
+        this.subscribe(operation, handler);
+        clearTimeout(retryTimeout);
+      }, 1000);
+    };
+  }
+
+  unsubscribe() {
+    if (this.source) {
+      this.source.close();
+    }
+  }
+}
+
+const SSELink = new ApolloLink((operation: Operation, forward) => {
+  const operationAST = getOperationAST(operation.query, operation.operationName);
+
+  if (!operationAST || operationAST.operation !== 'subscription') {
+    return forward(operation);
+  }
+
+  return new Observable(observer => {
+    const subscription = new SubscriptionSSE();
+
+    subscription.subscribe(Object.assign(operation, { query: print(operation.query) }), data =>
+      observer.next({ data })
+    );
+
+    return () => subscription.unsubscribe();
+  });
+});
 
 const errorLogger = errors => {
   errors.forEach(error => {
@@ -41,7 +99,7 @@ const httpLink = createHttpLink({
 });
 
 const client = new ApolloClient({
-  link: ApolloLink.from([errorLink, httpLink]),
+  link: ApolloLink.from([errorLink, SSELink, httpLink]),
   cache: new InMemoryCache({
     fragmentMatcher: new IntrospectionFragmentMatcher({
       introspectionQueryResultData,
