749b76d8a782891c153b5d2861882b4574b942c9
Kevin Nguyen <kcvin94@gmail.com>
2018-09-28 18:16:18 +0900

    feat(shipment): finish card design prelim

--- src/components/Cards/ShipmentCard/index.js
+++ src/components/Cards/ShipmentCard/index.js
@@ -3,9 +3,19 @@ import React from 'react';
 import { navigate } from '@reach/router';
 import logger from 'utils/logger';
 import { encodeId } from 'utils/id';
+import Tag from 'components/Tag';
 import { HorizontalLayout } from 'modules/shipment/form/components/TimelineSection/components/Timeline';
 import BaseCard, { CardAction } from '../BaseCard';
-import { ShipmentCardWrapperStyle } from './style';
+import {
+  ShipmentCardWrapperStyle,
+  ShipmentInfoWrapperStyle,
+  ShipmentLeftWrapperStyle,
+  ShipmentNoStyle,
+  ShipmentBLStyle,
+  ShipmentRightWrapperStyle,
+  ShipmentTagsWrapperStyle,
+  DividerStyle,
+} from './style';
 
 type Props = {
   shipment: ?Object,
@@ -29,6 +39,18 @@ const ShipmentCard = ({ shipment }: Props) => {
         onClick={() => navigate(`/shipment/${encodeId(id)}`)}
         role="presentation"
       >
+        <div className={ShipmentInfoWrapperStyle}>
+          <div className={ShipmentLeftWrapperStyle}>
+            <div className={ShipmentNoStyle}>{shipment.no}</div>
+            <div className={ShipmentBLStyle}>{shipment.blNo}</div>
+          </div>
+          <div className={ShipmentRightWrapperStyle}>
+            <div className={ShipmentTagsWrapperStyle}>
+              {shipment.tags.length > 0 && shipment.tags.map(tag => <Tag key={tag.id} tag={tag} />)}
+            </div>
+          </div>
+        </div>
+        <div className={DividerStyle} />
         <HorizontalLayout shipment={shipment} />
       </div>
     </BaseCard>
