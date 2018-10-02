917e9ed1ed0097b34d1c0033cc18e6ff85c9cd3f
Kaka Huynh <dunghd.it@gmail.com>
2018-10-02 14:44:25 +0900

    feat(history): add comment on order history

--- src/modules/history/order/list/index.js
+++ src/modules/history/order/list/index.js
@@ -1,14 +1,16 @@
 // @flow
 import * as React from 'react';
 import { StringValue } from 'react-values';
-import { Query } from 'react-apollo';
+import { Query, Mutation } from 'react-apollo';
 import { getByPathWithDefault } from 'utils/fp';
 import scrollIntoView from 'utils/scrollIntoView';
 import loadMore from 'utils/loadMore';
+import LoadingIcon from 'components/LoadingIcon';
 import MessageInput from 'modules/history/components/MessageInput';
 import CommentWrapperStyle from 'modules/history/order/list/style';
 import OrderEventsGridView from './OrderEventsGridView';
 import query from './query';
+import { eventCommentCreateMutation } from './mutation';
 
 type Props = {
   perPage: number,
@@ -25,51 +27,66 @@ class OrderEventsList extends React.PureComponent<Props> {
   render() {
     const { perPage, id } = this.props;
     return (
-      <div>
-        <div>
-          <Query
-            query={query}
-            variables={{
-              id,
-              perPage,
-              page: 1,
-            }}
-            fetchPolicy="network-only"
-          >
-            {({ loading, data, fetchMore, error }) => {
-              if (error) {
-                return error.message;
-              }
+      <Query
+        query={query}
+        variables={{
+          id,
+          perPage,
+          page: 1,
+        }}
+        fetchPolicy="network-only"
+      >
+        {({ loading, data, fetchMore, error }) => {
+          if (error) {
+            return error.message;
+          }
 
-              const nextPage = getByPathWithDefault(1, 'order.timeline.events.page', data) + 1;
-              const totalPage = getByPathWithDefault(1, 'order.timeline.events.totalPage', data);
-              const hasMore = nextPage <= totalPage;
+          const nextPage = getByPathWithDefault(1, 'order.timeline.events.page', data) + 1;
+          const totalPage = getByPathWithDefault(1, 'order.timeline.events.totalPage', data);
+          const hasMore = nextPage <= totalPage;
 
-              return (
-                <OrderEventsGridView
-                  items={getByPathWithDefault([], 'order.timeline.events.nodes', data)}
-                  onLoadMore={() => loadMore({ fetchMore, data }, {}, 'order.timeline.events')}
-                  hasMore={hasMore}
-                  isLoading={loading}
-                />
-              );
-            }}
-          </Query>
-          <div id="topCommentArea" />
-        </div>
-        <div className={CommentWrapperStyle}>
-          <StringValue>
-            {({ value, set }) => (
-              <MessageInput
-                name="comment"
-                value={value}
-                onChange={evt => set(evt.target.value)}
-                onSubmit={() => console.warn(value)}
+          const timelineId = getByPathWithDefault(1, 'order.timeline.id', data);
+          return (
+            <>
+              <OrderEventsGridView
+                items={getByPathWithDefault([], 'order.timeline.events.nodes', data)}
+                onLoadMore={() => loadMore({ fetchMore, data }, {}, 'order.timeline.events')}
+                hasMore={hasMore}
+                isLoading={loading}
               />
-            )}
-          </StringValue>
-        </div>
-      </div>
+              <div id="topCommentArea" />
+              <div className={CommentWrapperStyle}>
+                <StringValue>
+                  {({ value, set }) => (
+                    <Mutation mutation={eventCommentCreateMutation} onCompleted={() => set('')}>
+                      {(addComment, { loading: isLoading }) => (
+                        <>
+                          {isLoading && <LoadingIcon />}
+                          <MessageInput
+                            name="comment"
+                            value={value}
+                            onChange={evt => set(evt.target.value)}
+                            onSubmit={() =>
+                              addComment({
+                                variables: {
+                                  input: {
+                                    content: value,
+                                    timelineId,
+                                  },
+                                },
+                              })
+                            }
+                          />
+                        </>
+                      )}
+                    </Mutation>
+                  )}
+                </StringValue>
+              </div>
+            </>
+          );
+        }}
+      </Query>
     );
   }
 }
