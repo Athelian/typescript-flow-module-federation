fcf7c4575b158a95c209dbd971519ffcf6b3e0c6
Kaka Huynh <dunghd.it@gmail.com>
2018-10-15 09:32:37 +0700

    chore(cra2): upgrade create react app v2.0.5

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -1,5 +1,3 @@
-'use strict';
-
 const path = require('path');
 const webpack = require('webpack');
 const PnpWebpackPlugin = require('pnp-webpack-plugin');
@@ -15,10 +13,9 @@ const InterpolateHtmlPlugin = require('react-dev-utils/InterpolateHtmlPlugin');
 const WorkboxWebpackPlugin = require('workbox-webpack-plugin');
 const ModuleScopePlugin = require('react-dev-utils/ModuleScopePlugin');
 const getCSSModuleLocalIdent = require('react-dev-utils/getCSSModuleLocalIdent');
+const ModuleNotFoundPlugin = require('react-dev-utils/ModuleNotFoundPlugin');
 const paths = require('./paths');
 const getClientEnvironment = require('./env');
-const getCacheIdentifier = require('react-dev-utils/getCacheIdentifier');
-const ModuleNotFoundPlugin = require('react-dev-utils/ModuleNotFoundPlugin');
 
 // Webpack uses `publicPath` to determine where the app is being served from.
 // It requires a trailing slash, or the file assets will get an incorrect path.
@@ -28,12 +25,17 @@ const publicPath = paths.servedPath;
 const shouldUseRelativeAssetPaths = publicPath === './';
 // Source maps are resource heavy and can cause out of memory issue for large source files.
 const shouldUseSourceMap = process.env.GENERATE_SOURCEMAP !== 'false';
+// Some apps do not need the benefits of saving a web request, so not inlining the chunk
+// makes for a smoother build process.
+const shouldInlineRuntimeChunk = process.env.INLINE_RUNTIME_CHUNK !== 'false';
 // `publicUrl` is just like `publicPath`, but we will provide it to our app
 // as %PUBLIC_URL% in `index.html` and `process.env.PUBLIC_URL` in JavaScript.
 // Omit trailing slash as %PUBLIC_URL%/xyz looks better than %PUBLIC_URL%xyz.
 const publicUrl = publicPath.slice(0, -1);
 // Get environment variables to inject into our app.
 const env = getClientEnvironment(publicUrl);
+
+const isRunningAnalyze = process.env.ANALYZE;
 
 // Assert this just to be safe.
 // Development builds of React are slow and not intended for production.
@@ -94,80 +96,6 @@ const getStyleLoaders = (cssOptions, preProcessor) => {
   return loaders;
 };
 
-const plugins = [
-  // Generates an `index.html` file with the <script> injected.
-  new HtmlWebpackPlugin({
-    inject: true,
-    template: paths.appHtml,
-    minify: {
-      removeComments: true,
-      collapseWhitespace: true,
-      removeRedundantAttributes: true,
-      useShortDoctype: true,
-      removeEmptyAttributes: true,
-      removeStyleLinkTypeAttributes: true,
-      keepClosingSlash: true,
-      minifyJS: true,
-      minifyCSS: true,
-      minifyURLs: true,
-    },
-  }),
-  // Inlines the webpack runtime script. This script is too small to warrant
-  // a network request.
-  new InlineChunkHtmlPlugin(HtmlWebpackPlugin, [/runtime~.+[.]js/]),
-  // Makes some environment variables available in index.html.
-  // The public URL is available as %PUBLIC_URL% in index.html, e.g.:
-  // <link rel="shortcut icon" href="%PUBLIC_URL%/favicon.ico">
-  // In production, it will be an empty string unless you specify "homepage"
-  // in `package.json`, in which case it will be the pathname of that URL.
-  new InterpolateHtmlPlugin(HtmlWebpackPlugin, env.raw),
-  // This gives some necessary context to module not found errors, such as
-  // the requesting resource.
-  new ModuleNotFoundPlugin(paths.appPath),
-  // Makes some environment variables available to the JS code, for example:
-  // if (process.env.NODE_ENV === 'production') { ... }. See `./env.js`.
-  // It is absolutely essential that NODE_ENV was set to production here.
-  // Otherwise React will be compiled in the very slow development mode.
-  new webpack.DefinePlugin(env.stringified),
-  new MiniCssExtractPlugin({
-    // Options similar to the same options in webpackOptions.output
-    // both options are optional
-    filename: 'static/css/[name].[contenthash:8].css',
-    chunkFilename: 'static/css/[name].[contenthash:8].chunk.css',
-  }),
-  // Generate a manifest file which contains a mapping of all asset filenames
-  // to their corresponding output file so that tools can pick it up without
-  // having to parse `index.html`.
-  new ManifestPlugin({
-    fileName: 'asset-manifest.json',
-    publicPath: publicPath,
-  }),
-  // Moment.js is an extremely popular library that bundles large locale files
-  // by default due to how Webpack interprets its code. This is a practical
-  // solution that requires the user to opt into importing specific locales.
-  // https://github.com/jmblog/how-to-optimize-momentjs-with-webpack
-  // You can remove this if you don't use Moment.js:
-  new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
-  // Generate a service worker script that will precache, and keep up to date,
-  // the HTML & assets that are part of the Webpack build.
-  new WorkboxWebpackPlugin.GenerateSW({
-    clientsClaim: true,
-    exclude: [/\.map$/, /asset-manifest\.json$/],
-    importWorkboxFrom: 'cdn',
-    navigateFallback: publicUrl + '/index.html',
-    navigateFallbackBlacklist: [
-      // Exclude URLs starting with /_, as they're likely an API call
-      new RegExp('^/_'),
-      // Exclude URLs containing a dot, as they're likely a resource in
-      // public/ and not a SPA route
-      new RegExp('/[^/]+\\.[^/]+$'),
-    ],
-  }),
-];
-
-if (process.env.ANALYZE) {
-  plugins.push(new BundleAnalyzerPlugin());
-}
 // This is the production configuration.
 // It compiles slowly and is focused on producing a fast and minimal bundle.
 // The development configuration is different and lives in a separate file.
@@ -189,7 +117,7 @@ module.exports = {
     filename: 'static/js/[name].[chunkhash:8].js',
     chunkFilename: 'static/js/[name].[chunkhash:8].chunk.js',
     // We inferred the "public path" (such as / or /my-project) from homepage.
-    publicPath: publicPath,
+    publicPath,
     // Point sourcemap entries to original disk location (format as URL on Windows)
     devtoolModuleFilenameTemplate: info =>
       path.relative(paths.appSrc, info.absoluteResourcePath).replace(/\\/g, '/'),
@@ -479,7 +407,77 @@ module.exports = {
       },
     ],
   },
-  plugins,
+  plugins: [
+    // Generates an `index.html` file with the <script> injected.
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: paths.appHtml,
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true,
+        removeRedundantAttributes: true,
+        useShortDoctype: true,
+        removeEmptyAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        keepClosingSlash: true,
+        minifyJS: true,
+        minifyCSS: true,
+        minifyURLs: true,
+      },
+    }),
+    // Inlines the webpack runtime script. This script is too small to warrant
+    // a network request.
+    shouldInlineRuntimeChunk && new InlineChunkHtmlPlugin(HtmlWebpackPlugin, [/runtime~.+[.]js/]),
+    // Makes some environment variables available in index.html.
+    // The public URL is available as %PUBLIC_URL% in index.html, e.g.:
+    // <link rel="shortcut icon" href="%PUBLIC_URL%/favicon.ico">
+    // In production, it will be an empty string unless you specify "homepage"
+    // in `package.json`, in which case it will be the pathname of that URL.
+    new InterpolateHtmlPlugin(HtmlWebpackPlugin, env.raw),
+    // This gives some necessary context to module not found errors, such as
+    // the requesting resource.
+    new ModuleNotFoundPlugin(paths.appPath),
+    // Makes some environment variables available to the JS code, for example:
+    // if (process.env.NODE_ENV === 'production') { ... }. See `./env.js`.
+    // It is absolutely essential that NODE_ENV was set to production here.
+    // Otherwise React will be compiled in the very slow development mode.
+    new webpack.DefinePlugin(env.stringified),
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: 'static/css/[name].[contenthash:8].css',
+      chunkFilename: 'static/css/[name].[contenthash:8].chunk.css',
+    }),
+    // Generate a manifest file which contains a mapping of all asset filenames
+    // to their corresponding output file so that tools can pick it up without
+    // having to parse `index.html`.
+    new ManifestPlugin({
+      fileName: 'asset-manifest.json',
+      publicPath,
+    }),
+    // Moment.js is an extremely popular library that bundles large locale files
+    // by default due to how Webpack interprets its code. This is a practical
+    // solution that requires the user to opt into importing specific locales.
+    // https://github.com/jmblog/how-to-optimize-momentjs-with-webpack
+    // You can remove this if you don't use Moment.js:
+    new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+    // Generate a service worker script that will precache, and keep up to date,
+    // the HTML & assets that are part of the Webpack build.
+    new WorkboxWebpackPlugin.GenerateSW({
+      clientsClaim: true,
+      exclude: [/\.map$/, /asset-manifest\.json$/],
+      importWorkboxFrom: 'cdn',
+      navigateFallback: `${publicUrl}/index.html`,
+      navigateFallbackBlacklist: [
+        // Exclude URLs starting with /_, as they're likely an API call
+        new RegExp('^/_'),
+        // Exclude URLs containing a dot, as they're likely a resource in
+        // public/ and not a SPA route
+        new RegExp('/[^/]+\\.[^/]+$'),
+      ],
+    }),
+    isRunningAnalyze && new BundleAnalyzerPlugin(),
+  ].filter(Boolean),
   // Some libraries import Node modules but don't use them in the browser.
   // Tell Webpack to provide empty mocks for them so importing them works.
   node: {
