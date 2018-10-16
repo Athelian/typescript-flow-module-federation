ac105eb2f1cfd1585a0c7d3fdd17254abf4e5ef3
ThaddeusJiang <thaddeusjiang@gmail.com>
2018-10-16 14:10:29 +0900

    test(e2e): warehouse update and clone

--- cypress/integration/warehouse_spec.js
+++ cypress/integration/warehouse_spec.js
@@ -1,3 +1,14 @@
+const WAREHOUSE = {
+  name: 'test name',
+  updatedName: '[updated] test name',
+  clonedName: '[cloned] [updated] test name',
+  street: 'test street',
+  locality: 'test locality',
+  region: 'test region',
+  postalCode: '123456',
+  country: 'China',
+  surface: '200',
+};
 describe('Warehouse', () => {
   before(() => {
     cy.login();
@@ -8,42 +19,62 @@ describe('Warehouse', () => {
   });
 
   it('new a warehouse', () => {
-    const formContent = {
-      name: 'test name',
-      street: 'test street',
-      locality: 'test locality',
-      region: 'test region',
-      postalCode: '123456',
-      country: 'China',
-      surface: '200',
-    };
-
     cy.visit('/warehouse/new');
 
     cy.get('input[name="name"]')
-      .type(formContent.name)
-      .should('have.value', formContent.name);
+      .type(WAREHOUSE.name)
+      .should('have.value', WAREHOUSE.name);
     cy.get('input[name="street"]')
-      .type(formContent.street)
-      .should('have.value', formContent.street);
+      .type(WAREHOUSE.street)
+      .should('have.value', WAREHOUSE.street);
     cy.get('input[name="locality"]')
-      .type(formContent.locality)
-      .should('have.value', formContent.locality);
+      .type(WAREHOUSE.locality)
+      .should('have.value', WAREHOUSE.locality);
     cy.get('input[name="region"]')
-      .type(formContent.region)
-      .should('have.value', formContent.region);
+      .type(WAREHOUSE.region)
+      .should('have.value', WAREHOUSE.region);
     cy.get('input[name="postalCode"]')
-      .type(formContent.postalCode)
-      .should('have.value', formContent.postalCode);
+      .type(WAREHOUSE.postalCode)
+      .should('have.value', WAREHOUSE.postalCode);
     // TODO: downshift cypress: https://github.com/paypal/downshift/blob/master/cypress/integration/combobox.js
     // cy.get('input[name="country"]')
-    //   .type(formContent.country)
-    //   .should('have.value', formContent.country);
+    //   .type(WAREHOUSE.country)
+    //   .should('have.value', WAREHOUSE.country);
     cy.get('input[name="surface"]')
-      .type(formContent.surface)
+      .type(WAREHOUSE.surface)
       .blur()
       .get('input[name="surface"]')
-      .should('have.value', formContent.surface);
+      .should('have.value', WAREHOUSE.surface);
+
+    cy.getByTestId('saveButton')
+      .click()
+      .wait(100)
+      .should('not.exist');
+  });
+  it('update a warehouse', () => {
+    cy.get('input[name="name"]')
+      .clear()
+      .blur();
+
+    cy.getByTestId('saveButton').should('be.disabled');
+
+    cy.get('input[name="name"]')
+      .type(WAREHOUSE.updatedName)
+      .get('input[name="name"]')
+      .should('have.value', WAREHOUSE.updatedName)
+      .blur();
+
+    cy.getByTestId('saveButton')
+      .click()
+      .wait(100)
+      .should('not.exist');
+  });
+
+  it('clone a warehouse', () => {
+    cy.getByTestId('cloneButton').click();
+    cy.url().should('include', 'clone');
+    cy.wait(500);
+    cy.get('input[name="name"]').should('have.value', WAREHOUSE.clonedName);
 
     cy.getByTestId('saveButton')
       .click()
