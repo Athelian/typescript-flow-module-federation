ff5390bf87592c16a342728e4ab72c660287907b
Kaka Huynh <dunghd.it@gmail.com>
2018-10-19 15:03:39 +0700

    chore(i18n): add validation message for batch card

---
+++ src/components/Cards/OrderBatchCard/messages.js
@@ -0,0 +1,13 @@
+// @flow
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+  no: {
+    id: 'validation.no.required',
+    defaultMessage: 'No is required field',
+  },
+  quantity: {
+    id: 'validation.quantity.required',
+    defaultMessage: 'Quantity is must be a number',
+  },
+});
