b5e6f7f3068913958ec236ab9e91d2041aa4f7b0
Kaka Huynh <dunghd.it@gmail.com>
2018-10-19 15:03:39 +0700

    chore(i18n): add validation message for batch card

--- src/components/Cards/ShipmentBatchCard/validator.js
+++ src/components/Cards/ShipmentBatchCard/validator.js
@@ -1,10 +1,13 @@
 // @flow
+import * as React from 'react';
 import * as Yup from 'yup';
+import { FormattedMessage } from 'react-intl';
+import messages from './messages';
 
 const validator = ({ no, quantity }: { no: string, quantity: string }) =>
   Yup.object().shape({
-    [no]: Yup.string().required('No is required field'),
-    [quantity]: Yup.number('Quantity is must be a number'),
+    [no]: Yup.string().required(<FormattedMessage {...messages.no} />),
+    [quantity]: Yup.number(<FormattedMessage {...messages.quantity} />),
   });
 
 export default validator;
