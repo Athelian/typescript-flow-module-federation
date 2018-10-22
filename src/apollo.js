41aa11f188e640b9e479cec8e18d1886c90722be
Kaka Huynh <dunghd.it@gmail.com>
2018-10-22 15:38:15 +0700

    fix(form): tracking the remains error on validation

--- src/apollo.js
+++ src/apollo.js
@@ -113,15 +113,8 @@ const cache = new InMemoryCache({
 });
 
 const defaultOptions = {
-  watchQuery: {
-    fetchPolicy: 'cache-and-network',
-    errorPolicy: 'all',
-  },
   query: {
     fetchPolicy: 'network-only',
-    errorPolicy: 'all',
-  },
-  mutate: {
     errorPolicy: 'all',
   },
 };
