5ae4bf5b3dc57877bd4d66c63cf8d8602cbc35a6
Kaka Huynh <dunghd.it@gmail.com>
2018-10-24 16:33:16 +0700

    refactor(relation-map): pull out the constants to its file

---
+++ src/modules/relationMap/constants.js
@@ -0,0 +1,8 @@
+export const ORDER_HEADER = 'ORDER_HEADER';
+export const ORDER = 'ORDER';
+export const ORDER_ITEM = 'ORDER_ITEM';
+export const ORDER_ITEM_ALL = 'ORDER_ITEM_ALL';
+export const BATCH = 'BATCH';
+export const BATCH_ALL = 'BATCH_ALL';
+export const SHIPMENT = 'SHIPMENT';
+export const SHIPMENT_ALL = 'SHIPMENT_ALL';
