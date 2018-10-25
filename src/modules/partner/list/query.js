df546683d3cee6a05c98331775d43341b058aade
Kaka Huynh <dunghd.it@gmail.com>
2018-10-25 09:12:00 +0700

    fix(partner): pagination on list view

--- src/modules/partner/list/query.js
+++ src/modules/partner/list/query.js
@@ -16,6 +16,8 @@ export const partnerListQuery = gql`
                 ...partnerCardFragment
               }
             }
+            page
+            totalPage
           }
         }
       }
