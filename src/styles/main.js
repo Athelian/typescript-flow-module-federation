30088f01e995c13d6d9465bf5ea6e6e17988a958
Kaka Huynh <dunghd.it@gmail.com>
2018-10-26 11:05:48 +0700

    chore(stylint): integrate stylint

--- src/styles/main.js
+++ src/styles/main.js
@@ -5,8 +5,7 @@ import { transitions } from './common';
 export const AppWrapperStyle: string = css``;
 
 export const DesktopWrapperStyle = (isSideBarExpanded?: boolean): string => css`
-  ${transitions.MAIN};
-  min-height: 100vh;
+  ${transitions.MAIN} min-height: 100vh;
   height: 100%;
   margin-left: ${isSideBarExpanded ? '200px' : '50px'};
   margin-left: ${isSideBarExpanded === undefined && '0px'};
