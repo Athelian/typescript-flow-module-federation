35eba7bbcb38a6a9370f65a95a8dcec4af385f1b
Tostyle <unplugged2d@gmail.com>
2018-10-26 11:26:53 +0700

    feat(relation-map): delete action

---
+++ src/modules/relationMap/common/Dialog/BatchDeleteDialog.js
@@ -0,0 +1,76 @@
+// @flow
+import React from 'react';
+import { ApolloConsumer } from 'react-apollo';
+import DefaultDialog from 'components/Dialog/ArchiveDialog';
+import { orderFormQuery } from 'modules/order/form/query';
+import { updateOrderMutation, prepareUpdateOrderInput } from 'modules/order/form/mutation';
+import { removeTypename } from 'utils/data';
+
+type OptionalProps = {
+  onConfirm: () => void,
+};
+
+const defaultProps = {
+  onConfirm: () => {},
+};
+
+type Props = OptionalProps & {
+  isOpen: boolean,
+  onRequestClose: () => void,
+  batch: Object,
+};
+// $FlowFixMe
+const getOrderById = (client, orderId) =>
+  client.query({
+    query: orderFormQuery,
+    variables: {
+      id: orderId,
+    },
+  });
+
+const BatchDeleteDialog = ({ isOpen, onRequestClose, batch, onConfirm }: Props) => (
+  <ApolloConsumer>
+    {client => (
+      <DefaultDialog
+        isOpen={isOpen}
+        onRequestClose={onRequestClose}
+        onCancel={onRequestClose}
+        onConfirm={async () => {
+          const {
+            data: { order = {} },
+          } = await getOrderById(client, batch.orderId);
+          const removedOrderItems = order.orderItems.map(item => {
+            const removedBatch =
+              batch.orderItemId === item.id
+                ? item.batches.filter(currentBatch => currentBatch.id !== batch.id)
+                : item.batches;
+            return {
+              ...item,
+              batches: removedBatch,
+            };
+          });
+          await client.mutate({
+            mutation: updateOrderMutation,
+            variables: {
+              id: batch.orderId,
+              input: removeTypename(
+                prepareUpdateOrderInput({
+                  ...order,
+                  orderItems: removedOrderItems,
+                })
+              ),
+            },
+          });
+          onRequestClose();
+          onConfirm();
+        }}
+        width={360}
+        message="Confirm To delete batch"
+      />
+    )}
+  </ApolloConsumer>
+);
+
+BatchDeleteDialog.defaultProps = defaultProps;
+
+export default BatchDeleteDialog;
---
+++ src/modules/relationMap/common/Dialog/index.js
@@ -0,0 +1,4 @@
+export { default as DeleteDialog } from './DeleteDialog';
+export { default as OrderItemDeleteDialog } from './OrderItemDeleteDialog';
+export { default as BatchDeleteDialog } from './BatchDeleteDialog';
+export { default as ShipmentDeleteDialog } from './ShipmentDeleteDialog';
