75ccbef18298154a1a009eb930f127a0ac273751
Kaka Huynh <dunghd.it@gmail.com>
2018-10-29 13:37:51 +0700

    test(i18n): add e2e case for i18n

---
+++ cypress/integration/i18n_spec.js
@@ -0,0 +1,41 @@
+describe('I18n', () => {
+  before(() => {
+    cy.login();
+  });
+  after(() => {
+    cy.logout();
+  });
+
+  it('should show language on UI base on user setting', () => {
+    cy.task('token').then(({ data: { login: { token } } }) => {
+      cy.task('me', token.token).then(({ data: { viewer: { user } } }) => {
+        cy.visit('/')
+          .wait(1000)
+          .contains(user.language === 'en' ? 'RELATION MAP' : 'リレーションマップ');
+      });
+    });
+  });
+
+  it('should change language', () => {
+    cy.task('token').then(({ data: { login: { token } } }) => {
+      cy.task('me', token.token).then(({ data: { viewer: { user } } }) => {
+        cy.task('language', {
+          ...token,
+          variables: { id: user.id, input: { language: 'en' } },
+        }).then(() => {
+          cy.visit('/')
+            .wait(1000)
+            .contains('RELATION MAP');
+          cy.task('language', {
+            ...token,
+            variables: { id: user.id, input: { language: 'ja' } },
+          }).then(() => {
+            cy.visit('/')
+              .wait(1000)
+              .contains('リレーションマップ');
+          });
+        });
+      });
+    });
+  });
+});
