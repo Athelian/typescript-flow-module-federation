4b6bf79b5e890213fd1870d07bafab8ada91c3bb
ThaddeusJiang <thaddeusjiang@gmail.com>
2018-11-08 14:37:39 +0900

    fix(metric undefined): add default value for syncProductProvider

--- src/modules/batch/form/container.js
+++ src/modules/batch/form/container.js
@@ -9,6 +9,13 @@ type Metric = {
   metric: string,
 };
 
+type ProductProvider = {
+  packageName: string,
+  packageCapacity: number,
+  packageGrossWeight: Metric,
+  packageVolume: Metric,
+};
+
 export function calculateVolume(
   volumeMetric: string,
   height: Metric,
@@ -115,8 +122,13 @@ export default class BatchFormContainer extends Container<BatchFormState> {
     this.originalValues = Object.assign({}, parsedValues);
   };
 
-  syncProductProvider = (productProvider: Object) => {
-    const { packageCapacity, packageGrossWeight, packageName, packageVolume } = productProvider;
+  syncProductProvider = (productProvider: ProductProvider) => {
+    const {
+      packageName = '',
+      packageCapacity = 0,
+      packageGrossWeight = { value: 0, metric: 'kg' },
+      packageVolume = { value: 0, metric: 'm³' },
+    } = productProvider;
 
     this.setState({
       packageCapacity,
