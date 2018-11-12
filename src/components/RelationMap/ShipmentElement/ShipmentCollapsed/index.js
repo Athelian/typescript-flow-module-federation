f60d17e164122c0b911a61b3515658fb3464c58b
Khuc Anh Minh Luong <luong@ttab.me>
2018-11-12 19:52:28 +0700

    fix(my-rm): volume calculation

--- src/components/RelationMap/ShipmentElement/ShipmentCollapsed/index.js
+++ src/components/RelationMap/ShipmentElement/ShipmentCollapsed/index.js
@@ -11,15 +11,24 @@ const ShipmentCollapsed = ({ shipment }: Props) => {
   const { batches = [] } = shipment;
 
   const totalPackageGrossWeight = batches
-    ? batches.reduce((accumulator, { packageVolume }) => {
-        const currentValue = (packageVolume && packageVolume.value) || 0;
-        return accumulator + currentValue;
+    ? batches.reduce((accumulator, { packageVolume, packageQuantity = 0 }) => {
+        const { metric, value } = packageVolume || {};
+        const addingValue = (packageVolume && value) || 0;
+
+        switch (metric) {
+          case 'cm³':
+            return accumulator + packageQuantity * addingValue;
+          case 'm³':
+            return accumulator + packageQuantity * addingValue * 1000;
+          default:
+            return accumulator;
+        }
       }, 0)
     : 0;
 
   return (
     <div className={ShipmentCardWrapperStyle}>
-      <ShipmentLabel name={shipment.no} vol={totalPackageGrossWeight.toFixed(2)} />
+      <ShipmentLabel name={shipment.no} vol={totalPackageGrossWeight} metric="cm³" />
       <div className={ShipmentBlankContent} />
     </div>
   );
