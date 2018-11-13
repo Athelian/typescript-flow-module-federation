f0257633b956f92c59497de54510cacb8a487a6f
ThaddeusJiang <thaddeusjiang@gmail.com>
2018-11-13 15:53:49 +0900

    feat(metadata): metadata item with checkbox

---
+++ src/modules/metadata/components/CustomFieldsTemplateForm/MetadataItem/index.js
@@ -0,0 +1,40 @@
+// @flow
+import * as React from 'react';
+
+import { Label } from 'components/Form';
+import Icon from 'components/Icon';
+
+import { AdjustmentWrapperStyle, AdjustmentFieldsWrapperStyle, CheckBoxStyle } from './style';
+
+type OptionalProps = {
+  width: string,
+};
+
+type Props = OptionalProps & {
+  value: {
+    checked: boolean,
+    key: string,
+    value: string,
+  },
+};
+
+const defaultProps = {
+  width: '200px',
+};
+
+const MetadataItem = ({ width, value: { checked, key, value } }: Props) => (
+  <div className={AdjustmentWrapperStyle}>
+    <div className={AdjustmentFieldsWrapperStyle}>
+      <div className={CheckBoxStyle(checked)}>
+        <Icon icon="CONFIRM" />
+      </div>
+
+      <Label width={width}>{key}</Label>
+      <Label width={width}>{value}</Label>
+    </div>
+  </div>
+);
+
+MetadataItem.defaultProps = defaultProps;
+
+export default MetadataItem;
