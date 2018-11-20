7eddd03d0d56743452816c743e9b84a6076b8d3d
ThaddeusJiang <thaddeusjiang@gmail.com>
2018-11-20 18:56:23 +0900

    feat(custom fields): fieldDefinitions arrange

--- src/modules/metadata/components/FieldDefinitionsForm/index.js
+++ src/modules/metadata/components/FieldDefinitionsForm/index.js
@@ -3,11 +3,12 @@ import * as React from 'react';
 import { FormattedMessage } from 'react-intl';
 
 // $FlowFixMe https://github.com/atlassian/react-beautiful-dnd/issues/286#issuecomment-426604779
-// import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 import { SaveButton, CancelButton, NewButton } from 'components/Buttons';
+import GridColumn from 'components/GridColumn';
 
 import DefaultMetadataStyle from 'components/Form/Inputs/Styles/DefaultStyle/DefaultMetadataStyle';
-// import { isEquals } from 'utils/fp';
+
 import { uuid } from 'utils/id';
 
 import FormHeader from '../FormHeader';
@@ -37,12 +38,12 @@ const defaultProps = {
   onFormReady: () => {},
 };
 
-// const reorder = (list, startIndex, endIndex) => {
-//   const result = Array.from(list);
-//   const [removed] = result.splice(startIndex, 1);
-//   result.splice(endIndex, 0, removed);
-//   return result;
-// };
+const reorder = (list, startIndex, endIndex) => {
+  const result = Array.from(list);
+  const [removed] = result.splice(startIndex, 1);
+  result.splice(endIndex, 0, removed);
+  return result;
+};
 
 class FieldDefinitionsForm extends React.Component<Props> {
   static defaultProps = defaultProps;
@@ -53,27 +54,16 @@ class FieldDefinitionsForm extends React.Component<Props> {
     if (onFormReady) onFormReady();
   }
 
-  // shouldComponentUpdate(nextProps: Props) {
-  //   const { fieldDefinitions } = this.props;
-
-  //   return !isEquals(fieldDefinitions, nextProps.fieldDefinitions);
-  // }
-
-  // componentDidUpdate() {
-  //   const { onFormReady } = this.props;
-
-  //   if (onFormReady) onFormReady();
-  // }
-
-  // onDragEnd = (result: any) => {
-  //   if (!result.destination) {
-  //     return;
-  //   }
-  //   const { fieldDefinitions } = this.state;
-  //   this.setState({
-  //     fieldDefinitions: reorder(fieldDefinitions, result.source.index, result.destination.index),
-  //   });
-  // };
+  onDragEnd = (result: any) => {
+    if (!result.destination) {
+      return;
+    }
+    const { fieldDefinitions, setFieldArrayValue } = this.props;
+    setFieldArrayValue(
+      'fieldDefinitions',
+      reorder(fieldDefinitions, result.source.index, result.destination.index)
+    );
+  };
 
   render() {
     const { onCancel, onSave, setFieldArrayValue, fieldDefinitions, removeArrayItem } = this.props;
@@ -92,19 +82,44 @@ class FieldDefinitionsForm extends React.Component<Props> {
         </div>
         <div className={ContainerWrapperStyle}>
           <div className={ContainerStyle}>
-            {fieldDefinitions &&
-              fieldDefinitions.map((customField, index) => (
-                <DefaultMetadataStyle
-                  key={customField.id}
-                  isKeyReadOnly={false}
-                  isValueReadOnly
-                  targetName={`fieldDefinitions.${index}`}
-                  width="200px"
-                  metadata={customField}
-                  setFieldArrayValue={setFieldArrayValue}
-                  onRemove={() => removeArrayItem(`fieldDefinitions.${index}`)}
-                />
-              ))}
+            <DragDropContext onDragEnd={this.onDragEnd}>
+              <Droppable droppableId="droppable">
+                {dropProvided => (
+                  <div>
+                    <GridColumn gap="10px">
+                      <div ref={dropProvided.innerRef}>
+                        <GridColumn gap="10px">
+                          {fieldDefinitions &&
+                            fieldDefinitions.map((customField, index) => (
+                              <Draggable
+                                key={customField.id}
+                                draggableId={customField.id}
+                                index={index}
+                              >
+                                {provided => (
+                                  <div ref={provided.innerRef} {...provided.draggableProps}>
+                                    <DefaultMetadataStyle
+                                      rearrange
+                                      dragHandleProps={provided.dragHandleProps}
+                                      isKeyReadOnly={false}
+                                      isValueReadOnly
+                                      targetName={`fieldDefinitions.${index}`}
+                                      width="200px"
+                                      metadata={customField}
+                                      setFieldArrayValue={setFieldArrayValue}
+                                      onRemove={() => removeArrayItem(`fieldDefinitions.${index}`)}
+                                    />
+                                  </div>
+                                )}
+                              </Draggable>
+                            ))}
+                        </GridColumn>
+                      </div>
+                    </GridColumn>
+                  </div>
+                )}
+              </Droppable>
+            </DragDropContext>
 
             <div className={AddButtonWrapperStyle}>
               <NewButton
