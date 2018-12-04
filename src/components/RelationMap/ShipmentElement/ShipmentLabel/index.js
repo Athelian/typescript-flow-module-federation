6358a734f64d7aa3d6d0d53296887ee8dd4b5040
Tostyle <unplugged2d@gmail.com>
2018-12-04 17:11:14 +0700

    fix(relation-map): calculation error bug

--- src/components/RelationMap/ShipmentElement/ShipmentLabel/index.js
+++ src/components/RelationMap/ShipmentElement/ShipmentLabel/index.js
@@ -6,17 +6,17 @@ import { ShipmentLabelStyle } from './style';
 type Props = {
   name: string,
   value: number,
-  metric: 'cm³' | 'm³',
+  metric: 'cm³' | 'm³' | 'm',
 };
 
 type TotalVolumeProps = {
   value: number,
-  metric: 'cm³' | 'm³',
+  metric: 'cm³' | 'm³' | 'm',
 };
 
 const TotalVolume = ({ value, metric }: TotalVolumeProps) => (
   <span>
-    <FormattedNumber value={value} /> {metric}
+    <FormattedNumber value={value || 0} /> {metric}
   </span>
 );
 
@@ -24,7 +24,7 @@ export default function ShipmentLabel({ name = '', value = 0, metric = 'cm³' }:
   return (
     <div className={ShipmentLabelStyle}>
       <span>{name}</span>
-      {value && <TotalVolume value={value} metric={metric} />}
+      <TotalVolume value={value} metric={metric} />
     </div>
   );
 }
