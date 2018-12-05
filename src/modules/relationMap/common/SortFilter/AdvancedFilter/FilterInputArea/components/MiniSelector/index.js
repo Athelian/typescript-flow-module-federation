fddb046a61ad6aa600193beca0b4a0aa849faa54
Kaka Huynh <dunghd.it@gmail.com>
2018-12-05 18:29:39 +0700

    fix(relation-map): filter by search text

--- src/modules/relationMap/common/SortFilter/AdvancedFilter/FilterInputArea/components/MiniSelector/index.js
+++ src/modules/relationMap/common/SortFilter/AdvancedFilter/FilterInputArea/components/MiniSelector/index.js
@@ -1,5 +1,6 @@
 // @flow
 import * as React from 'react';
+import { StringValue } from 'react-values';
 import { Query } from 'react-apollo';
 import { getByPathWithDefault } from 'utils/fp';
 import loadMore from 'utils/loadMore';
@@ -39,62 +40,69 @@ export default function MiniSelector({
   hideToggles,
 }: Props) {
   return (
-    <div className={MiniSelectorWrapperStyle}>
-      <div className={MiniSelectorSearchWrapperStyle}>
-        {!hideToggles && (
-          <>
-            <div className={MiniSelectorStatusTogglesWrapperStyle}>
-              <Icon icon="ACTIVE" />
-              <ToggleButton isOn />
-            </div>
-            <div className={MiniSelectorStatusTogglesWrapperStyle}>
-              <Icon icon="ARCHIVE" />
-              <ToggleButton isOn />
-            </div>
-          </>
-        )}
-        <SearchInput name="hardcoded" value="" onChange={() => {}} onClear={() => {}} />
-      </div>
-      <div className={MiniSelectorBodyWrapperStyle}>
-        <Query
-          query={query}
-          variables={{
-            page: 1,
-            perPage: 10,
-            filterBy,
-          }}
-          fetchPolicy="network-only"
-        >
-          {({ loading: isLoading, data, fetchMore, error }) => {
-            if (error) {
-              return error.message;
-            }
+    <StringValue>
+      {({ value, set }) => (
+        <div className={MiniSelectorWrapperStyle}>
+          <div className={MiniSelectorSearchWrapperStyle}>
+            {!hideToggles && (
+              <>
+                <div className={MiniSelectorStatusTogglesWrapperStyle}>
+                  <Icon icon="ACTIVE" />
+                  <ToggleButton isOn />
+                </div>
+                <div className={MiniSelectorStatusTogglesWrapperStyle}>
+                  <Icon icon="ARCHIVE" />
+                  <ToggleButton isOn />
+                </div>
+              </>
+            )}
+            <SearchInput name="query" value={value} onChange={set} onClear={() => set('')} />
+          </div>
+          <div className={MiniSelectorBodyWrapperStyle}>
+            <Query
+              query={query}
+              variables={{
+                page: 1,
+                perPage: 10,
+                filter: {
+                  ...filterBy,
+                  query: value,
+                },
+              }}
+              fetchPolicy="network-only"
+            >
+              {({ loading: isLoading, data, fetchMore, error }) => {
+                if (error) {
+                  return error.message;
+                }
 
-            const nextPage = getByPathWithDefault(1, `${entityType}.page`, data) + 1;
-            const totalPage = getByPathWithDefault(1, `${entityType}.totalPage`, data);
-            const hasMore = nextPage <= totalPage;
+                const nextPage = getByPathWithDefault(1, `${entityType}.page`, data) + 1;
+                const totalPage = getByPathWithDefault(1, `${entityType}.totalPage`, data);
+                const hasMore = nextPage <= totalPage;
 
-            const items = getByPathWithDefault([], `${entityType}.nodes`, data);
-            const onLoadMore = () => loadMore({ fetchMore, data }, filterBy, entityType);
+                const items = getByPathWithDefault([], `${entityType}.nodes`, data);
+                const onLoadMore = () => loadMore({ fetchMore, data }, filterBy, entityType);
 
-            return (
-              <GridView
-                onLoadMore={onLoadMore}
-                hasMore={hasMore}
-                isLoading={isLoading}
-                itemWidth="200px"
-                isEmpty={items.length === 0}
-                emptyMessage={<FormattedMessage {...messages.noDataFound} />}
-                padding="0"
-                gap="5px"
-              >
-                {items.map(item => renderItem(item))}
-              </GridView>
-            );
-          }}
-        </Query>
-      </div>
-    </div>
+                return (
+                  <GridView
+                    onLoadMore={onLoadMore}
+                    hasMore={hasMore}
+                    isLoading={isLoading}
+                    itemWidth="200px"
+                    isEmpty={items.length === 0}
+                    emptyMessage={<FormattedMessage {...messages.noDataFound} />}
+                    padding="0"
+                    gap="5px"
+                  >
+                    {items.map(item => renderItem(item))}
+                  </GridView>
+                );
+              }}
+            </Query>
+          </div>
+        </div>
+      )}
+    </StringValue>
   );
 }
 
