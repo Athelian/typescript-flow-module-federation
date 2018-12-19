d80fbac15e120439a34fbcb23e19d6a3027e09e1
Tostyle <unplugged2d@gmail.com>
2018-12-19 20:24:47 +0700

    fix(rm): remove default filter

--- src/modules/relationMap/index.orders.js
+++ src/modules/relationMap/index.orders.js
@@ -33,7 +33,7 @@ type Props = {
 };
 
 const Order = ({ intl }: Props) => (
-  <SortFilterHandler filter={{ archived: false, shipmentArchived: false, batchArchived: false }}>
+  <SortFilterHandler filter={{ archived: false }}>
     {({ sort, filter, onChangeSortFilter, page, perPage }) => {
       const filterVariables = {
         page,
