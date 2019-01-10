c036f1906b94e29d04d21095397639e43b174234
tostyle <unplugged2d@gmail.com>
2019-01-10 11:22:52 +0900

    feat(filter): add ListConfig to ShipmentList

--- src/modules/shipment/index.list.js
+++ src/modules/shipment/index.list.js
@@ -5,6 +5,7 @@ import { injectIntl } from 'react-intl';
 import type { IntlShape } from 'react-intl';
 import { UIConsumer } from 'modules/ui';
 import FilterToolBar from 'components/common/FilterToolBar';
+import ListConfigProvider, { ListConfigConsumer } from 'components/ListConfig';
 import Layout from 'components/Layout';
 import NavBar from 'components/NavBar';
 import { NewButton, ExportButton } from 'components/Buttons';
@@ -30,8 +31,8 @@ type State = {
   page: number,
 };
 
-class ShipmentListModule extends React.Component<Props, State> {
-  state = {
+const getInitFilter = () => {
+  const state: State = {
     viewType: 'grid',
     filter: {
       query: '',
@@ -44,55 +45,61 @@ class ShipmentListModule extends React.Component<Props, State> {
     perPage: 10,
     page: 1,
   };
+  return state;
+};
 
-  onChangeFilter = (newValue: Object) => {
-    this.setState(prevState => ({ ...prevState, ...newValue }));
-  };
+const ShipmentListModule = (props: Props) => {
+  const { intl } = props;
 
-  render() {
-    const { intl } = this.props;
-    const { filter, sort } = this.state;
+  const sortFields = [
+    { title: intl.formatMessage(messages.updatedAt), value: 'updatedAt' },
+    { title: intl.formatMessage(messages.createdAt), value: 'createdAt' },
+  ];
 
-    const sortFields = [
-      { title: intl.formatMessage(messages.updatedAt), value: 'updatedAt' },
-      { title: intl.formatMessage(messages.createdAt), value: 'createdAt' },
-    ];
-
-    return (
-      <UIConsumer>
-        {uiState => (
+  return (
+    <UIConsumer>
+      {uiState => (
+        <ListConfigProvider filterName="filterShipment" initFilter={getInitFilter()}>
           <Layout
             {...uiState}
             navBar={
-              <NavBar>
-                <FilterToolBar
-                  icon="SHIPMENT"
-                  sortFields={sortFields}
-                  filtersAndSort={this.state}
-                  onChange={this.onChangeFilter}
-                />
-                <Link to="new">
-                  <NewButton />
-                </Link>
-                <ExportButton
-                  type="Shipments"
-                  exportQuery={shipmentsExportQuery}
-                  variables={{
-                    sortBy: {
-                      [sort.field]: sort.direction,
-                    },
-                    filterBy: filter,
-                  }}
-                />
-              </NavBar>
+              <ListConfigConsumer>
+                {({ filter, sort, page, perPage, onChangeFilter }) => (
+                  <NavBar>
+                    <FilterToolBar
+                      icon="SHIPMENT"
+                      sortFields={sortFields}
+                      filtersAndSort={{ page, perPage, sort, filter }}
+                      onChange={onChangeFilter}
+                    />
+                    <Link to="new">
+                      <NewButton />
+                    </Link>
+                    <ExportButton
+                      type="Shipments"
+                      exportQuery={shipmentsExportQuery}
+                      variables={{
+                        sortBy: {
+                          [sort.field]: sort.direction,
+                        },
+                        filterBy: filter,
+                      }}
+                    />
+                  </NavBar>
+                )}
+              </ListConfigConsumer>
             }
           >
-            <ShipmentList {...this.state} />
+            <ListConfigConsumer>
+              {({ filter, sort, page, perPage, viewType }) => (
+                <ShipmentList {...{ filter, sort, page, perPage, viewType }} />
+              )}
+            </ListConfigConsumer>
           </Layout>
-        )}
-      </UIConsumer>
-    );
-  }
-}
+        </ListConfigProvider>
+      )}
+    </UIConsumer>
+  );
+};
 
 export default injectIntl(ShipmentListModule);
