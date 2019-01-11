11a08608dda482cd1e0b45fddbd8d57ed26042b8
tostyle <unplugged2d@gmail.com>
2019-01-11 14:52:50 +0900

    feat(filter): add localFilter to staff query

--- src/modules/partner/list/query.js
+++ src/modules/partner/list/query.js
@@ -3,13 +3,13 @@ import gql from 'graphql-tag';
 import { partnerCardFragment } from 'graphql';
 
 export const partnerListQuery = gql`
-  query($page: Int!, $perPage: Int!, $filter: PartnerFilterInput, $sort: PartnerSortInput) {
+  query($page: Int!, $perPage: Int!, $filterBy: PartnerFilterInput, $sortBy: PartnerSortInput) {
     viewer {
       user {
         id
         group {
           id
-          partners(page: $page, perPage: $perPage, filterBy: $filter, sortBy: $sort) {
+          partners(page: $page, perPage: $perPage, filterBy: $filterBy, sortBy: $sortBy) {
             nodes {
               id
               group {
