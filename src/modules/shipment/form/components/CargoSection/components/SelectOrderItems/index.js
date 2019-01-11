6bb6075756d12a1b313c8b86c6a5d96011bc7b75
tostyle <unplugged2d@gmail.com>
2019-01-11 15:13:47 +0900

    feat(filter): add selectOrderItem localfilter

--- src/modules/shipment/form/components/CargoSection/components/SelectOrderItems/index.js
+++ src/modules/shipment/form/components/CargoSection/components/SelectOrderItems/index.js
@@ -3,7 +3,7 @@ import * as React from 'react';
 import { injectIntl, FormattedMessage } from 'react-intl';
 import type { IntlShape } from 'react-intl';
 import { Query } from 'react-apollo';
-import { ObjectValue, ArrayValue, NumberValue } from 'react-values';
+import { ArrayValue, NumberValue } from 'react-values';
 import GridView from 'components/GridView';
 import IncrementInput from 'components/IncrementInput';
 import Layout from 'components/Layout';
@@ -15,6 +15,7 @@ import { getByPathWithDefault } from 'utils/fp';
 import loadMore from 'utils/loadMore';
 import messages from 'modules/order/messages';
 import type { OrderItem } from 'modules/order/type.js.flow';
+import useListConfig from 'hooks/useListConfig';
 import { ItemWrapperStyle } from './style';
 
 type Props = {
@@ -64,131 +65,117 @@ function SelectOrderItems({ intl, onCancel, onSelect }: Props) {
     { title: intl.formatMessage(messages.updatedAtSort), value: 'updatedAt' },
     { title: intl.formatMessage(messages.createdAtSort), value: 'createdAt' },
   ];
+  const { filterAndSort: filtersAndSort, queryVariables, onChangeFilter: onChange } = useListConfig(
+    {
+      perPage: 20,
+      page: 1,
+      filter: {
+        query: '',
+      },
+      sort: { field: 'updatedAt', direction: 'DESCENDING' },
+    },
+    'filterSelectOrderItems'
+  );
+  console.log('queryVariables', queryVariables);
   return (
-    <ObjectValue
-      defaultValue={{
-        perPage: 20,
-        page: 1,
-        filter: {
-          query: '',
-        },
-        sort: { field: 'updatedAt', direction: 'DESCENDING' },
-      }}
-    >
-      {({ value: filtersAndSort, set: onChange }) => (
-        <Query
-          query={orderItemsQuery}
-          variables={{
-            page: 1,
-            perPage: filtersAndSort.perPage,
-            filter: filtersAndSort.filter,
-            sort: { [filtersAndSort.sort.field]: filtersAndSort.sort.direction },
-          }}
-          fetchPolicy="network-only"
-        >
-          {({ loading, data, error, fetchMore }) => {
-            if (error) {
-              return error.message;
-            }
-
-            const nextPage = getByPathWithDefault(1, 'orderItems.page', data) + 1;
-            const totalPage = getByPathWithDefault(1, 'orderItems.totalPage', data);
-            const hasMore = nextPage <= totalPage;
-
-            const items = getByPathWithDefault([], 'orderItems.nodes', data);
-
-            return (
-              <ArrayValue>
-                {({ value: selected, push, set }) => (
-                  <Layout
-                    navBar={
-                      <SlideViewNavBar>
-                        <EntityIcon icon="ORDER_ITEM" color="ORDER_ITEM" />
-                        <SortInput
-                          sort={
-                            fields.find(item => item.value === filtersAndSort.sort.field) ||
-                            fields[0]
-                          }
-                          ascending={filtersAndSort.sort.direction !== 'DESCENDING'}
-                          fields={fields}
-                          onChange={({ field: { value }, ascending }) =>
-                            onChange({
-                              ...filtersAndSort,
-                              sort: {
-                                field: value,
-                                direction: ascending ? 'ASCENDING' : 'DESCENDING',
-                              },
-                            })
-                          }
-                        />
-                        <SearchInput
-                          value={filtersAndSort.filter.query}
-                          name="search"
-                          onClear={() =>
-                            onChange({
-                              ...filtersAndSort,
-                              filter: { ...filtersAndSort.filter, query: '' },
-                            })
-                          }
-                          onChange={newQuery =>
-                            onChange({
-                              ...filtersAndSort,
-                              filter: { ...filtersAndSort.filter, query: newQuery },
-                            })
-                          }
-                        />
-                        <CancelButton onClick={onCancel} />
-                        <SaveButton
-                          disabled={selected.length === 0}
-                          onClick={() => onSelect(selected)}
-                        />
-                      </SlideViewNavBar>
-                    }
-                  >
-                    <GridView
-                      onLoadMore={() => loadMore({ fetchMore, data }, filtersAndSort, 'orderItems')}
-                      hasMore={hasMore}
-                      isLoading={loading}
-                      itemWidth="195px"
-                      isEmpty={items.length === 0}
-                      emptyMessage={
-                        <FormattedMessage
-                          id="modules.Batches.noOrderItemsFound"
-                          defaultMessage="No items found"
-                        />
+    <Query query={orderItemsQuery} variables={queryVariables} fetchPolicy="network-only">
+      {({ loading, data, error, fetchMore }) => {
+        if (error) {
+          return error.message;
+        }
+        const nextPage = getByPathWithDefault(1, 'orderItems.page', data) + 1;
+        const totalPage = getByPathWithDefault(1, 'orderItems.totalPage', data);
+        const hasMore = nextPage <= totalPage;
+        const items = getByPathWithDefault([], 'orderItems.nodes', data);
+        return (
+          <ArrayValue>
+            {({ value: selected, push, set }) => (
+              <Layout
+                navBar={
+                  <SlideViewNavBar>
+                    <EntityIcon icon="ORDER_ITEM" color="ORDER_ITEM" />
+                    <SortInput
+                      sort={
+                        fields.find(item => item.value === filtersAndSort.sort.field) || fields[0]
                       }
-                    >
-                      {items.map(item => (
-                        <div key={item.id} className={ItemWrapperStyle}>
-                          {selected.includes(item) && (
-                            <NumberValue
-                              defaultValue={1}
-                              onChange={total =>
-                                onChangeProductQuantity({ total, set, selected, item })
-                              }
-                            >
-                              {({ value: num, set: changeNumber }) => (
-                                <IncrementInput value={num} onChange={changeNumber} />
-                              )}
-                            </NumberValue>
+                      ascending={filtersAndSort.sort.direction !== 'DESCENDING'}
+                      fields={fields}
+                      onChange={({ field: { value }, ascending }) =>
+                        onChange({
+                          ...filtersAndSort,
+                          sort: {
+                            field: value,
+                            direction: ascending ? 'ASCENDING' : 'DESCENDING',
+                          },
+                        })
+                      }
+                    />
+                    <SearchInput
+                      value={filtersAndSort.filter.query}
+                      name="search"
+                      onClear={() =>
+                        onChange({
+                          ...filtersAndSort,
+                          filter: { ...filtersAndSort.filter, query: '' },
+                        })
+                      }
+                      onChange={newQuery =>
+                        onChange({
+                          ...filtersAndSort,
+                          filter: { ...filtersAndSort.filter, query: newQuery },
+                        })
+                      }
+                    />
+                    <CancelButton onClick={onCancel} />
+                    <SaveButton
+                      disabled={selected.length === 0}
+                      onClick={() => onSelect(selected)}
+                    />
+                  </SlideViewNavBar>
+                }
+              >
+                <GridView
+                  onLoadMore={() => loadMore({ fetchMore, data }, filtersAndSort, 'orderItems')}
+                  hasMore={hasMore}
+                  isLoading={loading}
+                  itemWidth="195px"
+                  isEmpty={items.length === 0}
+                  emptyMessage={
+                    <FormattedMessage
+                      id="modules.Batches.noOrderItemsFound"
+                      defaultMessage="No items found"
+                    />
+                  }
+                >
+                  {items.map(item => (
+                    <div key={item.id} className={ItemWrapperStyle}>
+                      {selected.includes(item) && (
+                        <NumberValue
+                          defaultValue={1}
+                          onChange={total =>
+                            onChangeProductQuantity({ total, set, selected, item })
+                          }
+                        >
+                          {({ value: num, set: changeNumber }) => (
+                            <IncrementInput value={num} onChange={changeNumber} />
                           )}
-                          <OrderItemCard
-                            item={item}
-                            selectable
-                            selected={selected.includes(item)}
-                            onSelect={() => onSelectProduct({ selected, item, push, set })}
-                          />
-                        </div>
-                      ))}
-                    </GridView>
-                  </Layout>
-                )}
-              </ArrayValue>
-            );
-          }}
-        </Query>
-      )}
-    </ObjectValue>
+                        </NumberValue>
+                      )}
+                      <OrderItemCard
+                        item={item}
+                        selectable
+                        selected={selected.includes(item)}
+                        onSelect={() => onSelectProduct({ selected, item, push, set })}
+                      />
+                    </div>
+                  ))}
+                </GridView>
+              </Layout>
+            )}
+          </ArrayValue>
+        );
+      }}
+    </Query>
   );
 }
 
