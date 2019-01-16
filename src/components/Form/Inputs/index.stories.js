0fae1f853739047e02d52b32a61183303edc91c0
Jifa Jiang <thaddeusjiang@gmail.com>
2019-01-16 14:00:08 +0900

    feat(input): date time input storybook

--- src/components/Form/Inputs/index.stories.js
+++ src/components/Form/Inputs/index.stories.js
@@ -6,8 +6,14 @@ import { action } from '@storybook/addon-actions';
 
 import { DefaultStyle, DateTimeInput } from './index';
 
-storiesOf('Form/Inputs', module).add('DateTimeInput', () => (
-  <DefaultStyle type="date" align="left" width="200px">
-    <DateTimeInput onChange={action('onChange')} />
-  </DefaultStyle>
-));
+storiesOf('Form/Inputs', module)
+  .add('DateTimeInput empty value', () => (
+    <DefaultStyle type="date" align="left" width="200px">
+      <DateTimeInput onChange={action('onChange')} />
+    </DefaultStyle>
+  ))
+  .add('DateTimeInput', () => (
+    <DefaultStyle type="date" align="left" width="200px">
+      <DateTimeInput value="2019-01-16T13:59" onChange={action('onChange')} />
+    </DefaultStyle>
+  ));
