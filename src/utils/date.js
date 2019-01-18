45f41e198a42ae7f35c24eef03c56bda397b412c
tostyle <unplugged2d@gmail.com>
2019-01-18 10:31:45 +0900

    fix(container): remove unused formatToDateTimeDisplay func

--- src/utils/date.js
+++ src/utils/date.js
@@ -5,8 +5,6 @@ export const formatToDateInput = (date: string): string => format(date, 'yyyy-MM
 
 export const formatToDateTimeInput = (time: string): string => format(time, "yyyy-MM-dd'T'hh:mm");
 
-export const formatToDateTimeDisplay = (time: string): string => format(time, 'yyyy/MM/dd hh:mm');
-
 export const formatToDateTimeGraphql = (date: string): string =>
   format(date, "yyyy-MM-dd'T'HH:mm:ssZ");
 
