981d78cbfd8ce5c68ba7eacd19d27948af3ffee5
Kevin Nguyen <kcvin94@gmail.com>
2019-01-21 19:19:30 +0900

    feat(container-cargo): make footer navbar and updated navbar component

--- src/modules/shipment/form/components/ContainerCargoSection/index.js
+++ src/modules/shipment/form/components/ContainerCargoSection/index.js
@@ -20,6 +20,7 @@ import {
   ItemGridStyle,
   ItemStyle,
   EmptyMessageStyle,
+  BatchButtonsWrapperStyle,
 } from './style';
 import SelectBatches from '../SelectBatches';
 
@@ -30,97 +31,7 @@ type Props = {
 function CargoSection({ intl }: Props) {
   return (
     <div className={ItemsSectionWrapperStyle}>
-      <SectionNavBar>
-        <BooleanValue>
-          {({ value: selectBatchesIsOpen, set: selectBatchesSlideToggle }) => (
-            <>
-              <NewButton
-                data-testid="selectBatchesButton"
-                label={intl.formatMessage(messages.selectBatches)}
-                onClick={() => selectBatchesSlideToggle(true)}
-              />
-              <SlideView
-                isOpen={selectBatchesIsOpen}
-                onRequestClose={() => selectBatchesSlideToggle(false)}
-                options={{ width: '1030px' }}
-              >
-                {selectBatchesIsOpen && (
-                  <Subscribe to={[ShipmentBatchesContainer]}>
-                    {({ state: { batches }, setFieldValue }) => (
-                      <SelectBatches
-                        selectedBatches={batches}
-                        onSelect={selected => {
-                          const selectedBatches = selected.map(selectedBatch => ({
-                            ...selectedBatch,
-                            packageQuantity: calculatePackageQuantity(selectedBatch),
-                          }));
-                          setFieldValue('batches', [...batches, ...selectedBatches]);
-                          selectBatchesSlideToggle(false);
-                        }}
-                        onCancel={() => selectBatchesSlideToggle(false)}
-                      />
-                    )}
-                  </Subscribe>
-                )}
-              </SlideView>
-            </>
-          )}
-        </BooleanValue>
-        <BooleanValue>
-          {({ value: createBatchesIsOpen, set: createBatchesSlideToggle }) => (
-            <>
-              <NewButton
-                label={intl.formatMessage(messages.newBatch)}
-                onClick={() => createBatchesSlideToggle(true)}
-              />
-              <SlideView
-                isOpen={createBatchesIsOpen}
-                onRequestClose={() => createBatchesSlideToggle(false)}
-                options={{ width: '1030px' }}
-              >
-                {createBatchesIsOpen && (
-                  <Subscribe to={[ShipmentBatchesContainer]}>
-                    {({ state: { batches }, setFieldValue }) => (
-                      <SelectOrderItems
-                        onSelect={selectedOrderItems => {
-                          const result = selectedOrderItems.map((orderItem, counter) => {
-                            const {
-                              productProvider: {
-                                packageName,
-                                packageCapacity,
-                                packageGrossWeight,
-                                packageVolume,
-                                packageSize,
-                              },
-                            } = orderItem;
-                            return injectUid({
-                              orderItem,
-                              tags: [],
-                              packageName,
-                              packageCapacity,
-                              packageGrossWeight,
-                              packageVolume,
-                              packageSize,
-                              quantity: 0,
-                              isNew: true,
-                              batchAdjustments: [],
-                              no: `batch no ${batches.length + counter + 1}`,
-                              autoCalculatePackageQuantity: true,
-                            });
-                          });
-                          setFieldValue('batches', [...batches, ...result]);
-                          createBatchesSlideToggle(false);
-                        }}
-                        onCancel={() => createBatchesSlideToggle(false)}
-                      />
-                    )}
-                  </Subscribe>
-                )}
-              </SlideView>
-            </>
-          )}
-        </BooleanValue>
-      </SectionNavBar>
+      <SectionNavBar>Hi</SectionNavBar>
       <div className={ItemsSectionBodyStyle}>
         <Subscribe to={[ShipmentBatchesContainer]}>
           {({ state: { batches }, setFieldValue, setFieldArrayValue }) =>
@@ -203,6 +114,99 @@ function CargoSection({ intl }: Props) {
           }
         </Subscribe>
       </div>
+      <SectionNavBar upsideDown>
+        <div className={BatchButtonsWrapperStyle}>
+          <BooleanValue>
+            {({ value: selectBatchesIsOpen, set: selectBatchesSlideToggle }) => (
+              <>
+                <NewButton
+                  data-testid="selectBatchesButton"
+                  label={intl.formatMessage(messages.selectBatches)}
+                  onClick={() => selectBatchesSlideToggle(true)}
+                />
+                <SlideView
+                  isOpen={selectBatchesIsOpen}
+                  onRequestClose={() => selectBatchesSlideToggle(false)}
+                  options={{ width: '1030px' }}
+                >
+                  {selectBatchesIsOpen && (
+                    <Subscribe to={[ShipmentBatchesContainer]}>
+                      {({ state: { batches }, setFieldValue }) => (
+                        <SelectBatches
+                          selectedBatches={batches}
+                          onSelect={selected => {
+                            const selectedBatches = selected.map(selectedBatch => ({
+                              ...selectedBatch,
+                              packageQuantity: calculatePackageQuantity(selectedBatch),
+                            }));
+                            setFieldValue('batches', [...batches, ...selectedBatches]);
+                            selectBatchesSlideToggle(false);
+                          }}
+                          onCancel={() => selectBatchesSlideToggle(false)}
+                        />
+                      )}
+                    </Subscribe>
+                  )}
+                </SlideView>
+              </>
+            )}
+          </BooleanValue>
+          <BooleanValue>
+            {({ value: createBatchesIsOpen, set: createBatchesSlideToggle }) => (
+              <>
+                <NewButton
+                  label={intl.formatMessage(messages.newBatch)}
+                  onClick={() => createBatchesSlideToggle(true)}
+                />
+                <SlideView
+                  isOpen={createBatchesIsOpen}
+                  onRequestClose={() => createBatchesSlideToggle(false)}
+                  options={{ width: '1030px' }}
+                >
+                  {createBatchesIsOpen && (
+                    <Subscribe to={[ShipmentBatchesContainer]}>
+                      {({ state: { batches }, setFieldValue }) => (
+                        <SelectOrderItems
+                          onSelect={selectedOrderItems => {
+                            const result = selectedOrderItems.map((orderItem, counter) => {
+                              const {
+                                productProvider: {
+                                  packageName,
+                                  packageCapacity,
+                                  packageGrossWeight,
+                                  packageVolume,
+                                  packageSize,
+                                },
+                              } = orderItem;
+                              return injectUid({
+                                orderItem,
+                                tags: [],
+                                packageName,
+                                packageCapacity,
+                                packageGrossWeight,
+                                packageVolume,
+                                packageSize,
+                                quantity: 0,
+                                isNew: true,
+                                batchAdjustments: [],
+                                no: `batch no ${batches.length + counter + 1}`,
+                                autoCalculatePackageQuantity: true,
+                              });
+                            });
+                            setFieldValue('batches', [...batches, ...result]);
+                            createBatchesSlideToggle(false);
+                          }}
+                          onCancel={() => createBatchesSlideToggle(false)}
+                        />
+                      )}
+                    </Subscribe>
+                  )}
+                </SlideView>
+              </>
+            )}
+          </BooleanValue>
+        </div>
+      </SectionNavBar>
     </div>
   );
 }
