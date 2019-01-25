4df359bb16869df49ecfd1ae4851fc0e3778c2b5
tostyle <unplugged2d@gmail.com>
2019-01-25 10:15:46 +0900

    feat(export): add unittest

--- src/modules/relationMap/common/TableInlineEdit/__tests__/helper.test.js
+++ src/modules/relationMap/common/TableInlineEdit/__tests__/helper.test.js
@@ -4,14 +4,14 @@ import {
   batchColumnFields,
   shipmentColumnFields,
 } from 'modules/tableTemplate/constants';
-import { getExportRows } from '../helpers';
+import { getExportRows, getExportColumns } from '../helpers';
 import info from './data.json';
 import editData from './editData.json';
 import mappingObjects from './mappingObj.json';
 
 describe('export helper function', () => {
   test('getExportRows', () => {
-    getExportRows({
+    const rows = getExportRows({
       data: { editData, mappingObjects },
       ids: info.ids,
       columns: {
@@ -22,5 +22,23 @@ describe('export helper function', () => {
         shipmentColumnFieldsFilter: shipmentColumnFields,
       },
     });
+    expect(rows.length).toEqual(6);
+    rows.forEach(row => {
+      expect(row.length).toEqual(79);
+    });
+  });
+
+  test('getExportColumns', () => {
+    const intl = {
+      formatMessage: jest.fn(d => d.name),
+    };
+    const columns = getExportColumns(intl, {
+      ...info.columns,
+      orderColumnFieldsFilter: orderColumnFields,
+      orderItemColumnFieldsFilter: orderItemColumnFields,
+      batchColumnFieldsFilter: batchColumnFields,
+      shipmentColumnFieldsFilter: shipmentColumnFields,
+    });
+    expect(columns.length).toEqual(79);
   });
 });
