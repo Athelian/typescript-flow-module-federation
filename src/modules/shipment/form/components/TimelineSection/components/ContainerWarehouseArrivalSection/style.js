5f6b86dbc0fdc1e4abf95c7e8c9df1e859f21e43
Kevin Nguyen <kcvin94@gmail.com>
2019-01-25 15:00:00 +0900

    feat(timeline-section): init the new warehouse arrival area

---
+++ src/modules/shipment/form/components/TimelineSection/components/ContainerWarehouseArrivalSection/style.js
@@ -0,0 +1,24 @@
+// @flow
+import { css } from 'react-emotion';
+import { colors, fontSizes, presets } from 'styles/common';
+
+export const ContainerWarehouseArrivalSectionWrapperStyle: string = css`
+  display: flex;
+  flex-direction: column;
+  flex-shrink: 0;
+  padding: 40px;
+  border-bottom: 1px solid ${colors.GRAY_VERY_LIGHT};
+  width: 490px;
+`;
+
+export const SelectTransportTypeMessageStyle: string = css`
+  ${presets.ELLIPSIS};
+  color: ${colors.GRAY_DARK};
+  font-weight: bold;
+  ${fontSizes.MAIN};
+  text-align: right;
+  width: 200px;
+  flex-shrink: 0;
+  height: 30px;
+  line-height: 30px;
+`;
--- src/modules/shipment/form/components/TimelineSection/components/index.js
+++ src/modules/shipment/form/components/TimelineSection/components/index.js
@@ -1,7 +1,14 @@
 // @flow
 import { VerticalLayout } from './Timeline';
+import ContainerWarehouseArrivalSection from './ContainerWarehouseArrivalSection';
 import TimelineInfoSection from './TimelineInfoSection';
 import VoyageInfoSection from './VoyageInfoSection';
 import VoyageSelector from './VoyageSelector';
 
-export { VerticalLayout, TimelineInfoSection, VoyageInfoSection, VoyageSelector };
+export {
+  VerticalLayout,
+  ContainerWarehouseArrivalSection,
+  TimelineInfoSection,
+  VoyageInfoSection,
+  VoyageSelector,
+};
