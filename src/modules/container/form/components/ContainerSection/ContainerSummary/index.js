9d4a7c68e7d8b1e9f9b9509a3521feb58a98a6a1
tostyle <unplugged2d@gmail.com>
2019-01-25 17:59:30 +0900

    fix(container): remove console.log

--- src/modules/container/form/components/ContainerSection/ContainerSummary/index.js
+++ src/modules/container/form/components/ContainerSection/ContainerSummary/index.js
@@ -23,7 +23,6 @@ export default function ContainerSummary() {
           totalWeight,
           totalPrice,
         } = findSummary(values);
-        console.log(values);
         return (
           <>
             <GridColumn>
