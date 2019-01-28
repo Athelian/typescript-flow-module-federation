ab9aec98fa5deb0240c13971f1f451c86dac5585
Jifa Jiang <thaddeusjiang@gmail.com>
2019-01-28 17:29:42 +0900

    fix(container card): approval button

--- src/components/Cards/ShipmentContainerCard/index.js
+++ src/components/Cards/ShipmentContainerCard/index.js
@@ -14,6 +14,7 @@ import { Label, Display, DefaultStyle } from 'components/Form';
 import SlideView from 'components/SlideView';
 import SelectWareHouse from 'modules/warehouse/common/SelectWareHouse';
 import { getProductImage } from 'components/Cards/utils';
+import { UserConsumer } from 'modules/user';
 import validator from './validator';
 import BaseCard from '../BaseCard';
 import {
@@ -104,229 +105,283 @@ const ShipmentContainerCard = ({
   };
 
   return (
-    <BaseCard
-      icon="CONTAINER"
-      color="CONTAINER"
-      showActionsOnHover
-      selectable={selectable}
-      {...rest}
-    >
-      <div className={CardWrapperStyle} onClick={() => onClick(newContainer)} role="presentation">
-        <div
-          className={ImagePartWrapperStyle}
-          onClick={() => onClick(newContainer)}
-          role="presentation"
+    <UserConsumer>
+      {({ user }) => (
+        <BaseCard
+          icon="CONTAINER"
+          color="CONTAINER"
+          showActionsOnHover
+          selectable={selectable}
+          {...rest}
         >
-          <div className={ImageWrapperStyle}>
-            <img className={ImageStyle} src={productImage} alt="product_image" />
-          </div>
-          <div className={InfoInsideImageWrapperStyle}>
-            <div className={NameStyle}>{product.name}</div>
-            <div className={SerialStyle}>{product.serial}</div>
-          </div>
-        </div>
-
-        <div className={InfoPartWrapperStyle}>
-          <div className={InputStyle} onClick={evt => evt.stopPropagation()} role="presentation">
-            <FormField
-              name={`container.${id}.no`}
-              initValue={no}
-              validator={validation}
-              values={values}
+          <div
+            className={CardWrapperStyle}
+            onClick={() => onClick(newContainer)}
+            role="presentation"
+          >
+            <div
+              className={ImagePartWrapperStyle}
+              onClick={() => onClick(newContainer)}
+              role="presentation"
             >
-              {({ name: fieldName, ...inputHandlers }) =>
-                textInputFactory({
-                  width: '185px',
-                  height: '20px',
-                  inputHandlers: {
-                    ...inputHandlers,
-                    onBlur: evt => {
-                      inputHandlers.onBlur(evt);
-                      update({ ...container, no: inputHandlers.value });
-                    },
-                  },
-                  name: fieldName,
-                  isNew: false,
-                  originalValue: no,
-                  align: 'left',
-                })
-              }
-            </FormField>
-          </div>
-
-          <div className={LabelInputStyle}>
-            <Label>
-              <FormattedMessage id="components.cards.ttlVol" defaultMessage="TTL VOL" />
-            </Label>
-            <Display align="right">
-              <FormattedNumber value={totalVolume.value} suffix={totalVolume.metric} />
-            </Display>
-          </div>
-
-          <div className={LabelInputStyle}>
-            <Label>
-              <FormattedMessage id="components.cards.batches" defaultMessage="BATCHES" />
-            </Label>
-            <Display align="right">
-              <FormattedNumber value={batches.length} />
-            </Display>
-          </div>
-
-          <div className={DividerStyle} />
-
-          <div className={IconInputStyle}>
-            {isNullOrUndefined(warehouse) ? (
-              <div className={WarehouseIconStyle(false)}>
-                <Icon icon="WAREHOUSE" />
+              <div className={ImageWrapperStyle}>
+                <img className={ImageStyle} src={productImage} alt="product_image" />
               </div>
-            ) : (
-              <Link
-                className={WarehouseIconStyle(true)}
-                to={`/warehouse/${encodeId(warehouse.id)}`}
-                onClick={evt => {
-                  evt.stopPropagation();
-                }}
-              >
-                <Icon icon="WAREHOUSE" />
-              </Link>
-            )}
-            <BooleanValue>
-              {({ value: isOpenSelectWarehouse, set: toggleSelectWarehouse }) => (
-                <>
-                  <button type="button" onClick={() => toggleSelectWarehouse(true)}>
-                    <DefaultStyle type="button" height="20px">
-                      <Display align="left">
-                        {isNullOrUndefined(warehouse) ? '' : warehouse.name}
-                      </Display>
-                    </DefaultStyle>
-                  </button>
+              <div className={InfoInsideImageWrapperStyle}>
+                <div className={NameStyle}>{product.name}</div>
+                <div className={SerialStyle}>{product.serial}</div>
+              </div>
+            </div>
 
-                  <SlideView
-                    isOpen={isOpenSelectWarehouse}
-                    onRequestClose={() => toggleSelectWarehouse(false)}
-                    options={{ width: '1030px' }}
+            <div className={InfoPartWrapperStyle}>
+              <div
+                className={InputStyle}
+                onClick={evt => evt.stopPropagation()}
+                role="presentation"
+              >
+                <FormField
+                  name={`container.${id}.no`}
+                  initValue={no}
+                  validator={validation}
+                  values={values}
+                >
+                  {({ name: fieldName, ...inputHandlers }) =>
+                    textInputFactory({
+                      width: '185px',
+                      height: '20px',
+                      inputHandlers: {
+                        ...inputHandlers,
+                        onBlur: evt => {
+                          inputHandlers.onBlur(evt);
+                          update({ ...container, no: inputHandlers.value });
+                        },
+                      },
+                      name: fieldName,
+                      isNew: false,
+                      originalValue: no,
+                      align: 'left',
+                    })
+                  }
+                </FormField>
+              </div>
+
+              <div className={LabelInputStyle}>
+                <Label>
+                  <FormattedMessage id="components.cards.ttlVol" defaultMessage="TTL VOL" />
+                </Label>
+                <Display align="right">
+                  <FormattedNumber value={totalVolume.value} suffix={totalVolume.metric} />
+                </Display>
+              </div>
+
+              <div className={LabelInputStyle}>
+                <Label>
+                  <FormattedMessage id="components.cards.batches" defaultMessage="BATCHES" />
+                </Label>
+                <Display align="right">
+                  <FormattedNumber value={batches.length} />
+                </Display>
+              </div>
+
+              <div className={DividerStyle} />
+
+              <div className={IconInputStyle}>
+                {isNullOrUndefined(warehouse) ? (
+                  <div className={WarehouseIconStyle(false)}>
+                    <Icon icon="WAREHOUSE" />
+                  </div>
+                ) : (
+                  <Link
+                    className={WarehouseIconStyle(true)}
+                    to={`/warehouse/${encodeId(warehouse.id)}`}
+                    onClick={evt => {
+                      evt.stopPropagation();
+                    }}
                   >
-                    {isOpenSelectWarehouse && (
-                      <SelectWareHouse
-                        selected={warehouse}
-                        onCancel={() => toggleSelectWarehouse(false)}
-                        onSelect={newValue => {
-                          toggleSelectWarehouse(false);
+                    <Icon icon="WAREHOUSE" />
+                  </Link>
+                )}
+                <BooleanValue>
+                  {({ value: isOpenSelectWarehouse, set: toggleSelectWarehouse }) => (
+                    <>
+                      <button type="button" onClick={() => toggleSelectWarehouse(true)}>
+                        <DefaultStyle type="button" height="20px">
+                          <Display align="left">
+                            {isNullOrUndefined(warehouse) ? '' : warehouse.name}
+                          </Display>
+                        </DefaultStyle>
+                      </button>
+
+                      <SlideView
+                        isOpen={isOpenSelectWarehouse}
+                        onRequestClose={() => toggleSelectWarehouse(false)}
+                        options={{ width: '1030px' }}
+                      >
+                        {isOpenSelectWarehouse && (
+                          <SelectWareHouse
+                            selected={warehouse}
+                            onCancel={() => toggleSelectWarehouse(false)}
+                            onSelect={newValue => {
+                              toggleSelectWarehouse(false);
+                              update({
+                                ...container,
+                                warehouse: newValue,
+                              });
+                            }}
+                          />
+                        )}
+                      </SlideView>
+                    </>
+                  )}
+                </BooleanValue>
+              </div>
+
+              <div className={LabelStyle}>
+                <Label>
+                  <FormattedMessage
+                    id="components.cards.agreedArrival"
+                    defaultMessage="AGREED ARRIVAL"
+                  />
+                </Label>
+              </div>
+              <div
+                className={InputIconStyle}
+                onClick={evt => evt.stopPropagation()}
+                role="presentation"
+              >
+                <FormField
+                  name={`container.${id}.warehouseArrivalAgreedDate`}
+                  initValue={warehouseArrivalAgreedDate}
+                >
+                  {({ name: fieldName, ...inputHandlers }) =>
+                    dateTimeInputFactory({
+                      width: '165px',
+                      height: '20px',
+                      name: fieldName,
+                      isNew: false,
+                      originalValue: warehouseArrivalAgreedDate,
+                      inputHandlers: {
+                        ...inputHandlers,
+                        onBlur: evt => {
+                          inputHandlers.onBlur(evt);
                           update({
                             ...container,
-                            warehouse: newValue,
+                            warehouseArrivalAgreedDate: inputHandlers.value
+                              ? inputHandlers.value
+                              : null,
                           });
-                        }}
-                      />
-                    )}
-                  </SlideView>
-                </>
-              )}
-            </BooleanValue>
-          </div>
+                        },
+                      },
+                    })
+                  }
+                </FormField>
 
-          <div className={LabelStyle}>
-            <Label>
-              <FormattedMessage
-                id="components.cards.agreedArrival"
-                defaultMessage="AGREED ARRIVAL"
-              />
-            </Label>
-          </div>
-          <div
-            className={InputIconStyle}
-            onClick={evt => evt.stopPropagation()}
-            role="presentation"
-          >
-            <FormField
-              name={`container.${id}.warehouseArrivalAgreedDate`}
-              initValue={warehouseArrivalAgreedDate}
-            >
-              {({ name: fieldName, ...inputHandlers }) =>
-                dateTimeInputFactory({
-                  width: '165px',
-                  height: '20px',
-                  name: fieldName,
-                  isNew: false,
-                  originalValue: warehouseArrivalAgreedDate,
-                  inputHandlers: {
-                    ...inputHandlers,
-                    onBlur: evt => {
-                      inputHandlers.onBlur(evt);
+                {warehouseArrivalAgreedDateApprovedBy ? (
+                  <button
+                    type="button"
+                    className={ApprovalIconStyle(true)}
+                    onClick={() =>
                       update({
                         ...container,
-                        warehouseArrivalAgreedDate: inputHandlers.value
-                          ? inputHandlers.value
-                          : null,
-                      });
-                    },
-                  },
-                })
-              }
-            </FormField>
-            <div className={ApprovalIconStyle(!!warehouseArrivalAgreedDateApprovedBy)}>
-              {warehouseArrivalAgreedDateApprovedBy ? (
-                <Icon icon="CHECKED" />
-              ) : (
-                <Icon icon="UNCHECKED" />
-              )}
-            </div>
-          </div>
-
-          <div className={LabelStyle}>
-            <Label>
-              <FormattedMessage
-                id="components.cards.actualArrival"
-                defaultMessage="ACTUAL ARRIVAL"
-              />
-            </Label>
-          </div>
-          <div
-            className={InputIconStyle}
-            onClick={evt => evt.stopPropagation()}
-            role="presentation"
-          >
-            <FormField
-              name={`container.${id}.warehouseArrivalActualDate`}
-              initValue={warehouseArrivalActualDate}
-            >
-              {({ name: fieldName, ...inputHandlers }) =>
-                dateTimeInputFactory({
-                  width: '165px',
-                  height: '20px',
-                  name: fieldName,
-                  isNew: false,
-                  originalValue: warehouseArrivalActualDate,
-                  inputHandlers: {
-                    ...inputHandlers,
-                    onBlur: evt => {
-                      inputHandlers.onBlur(evt);
+                        warehouseArrivalAgreedDateApprovedBy: null,
+                      })
+                    }
+                  >
+                    <Icon icon="CHECKED" />
+                  </button>
+                ) : (
+                  <button
+                    type="button"
+                    className={ApprovalIconStyle(false)}
+                    onClick={() =>
                       update({
                         ...container,
-                        warehouseArrivalActualDate: inputHandlers.value
-                          ? inputHandlers.value
-                          : null,
-                      });
-                    },
-                  },
-                })
-              }
-            </FormField>
-            <div className={ApprovalIconStyle(!!warehouseArrivalActualDateApprovedBy)}>
-              {warehouseArrivalActualDateApprovedBy ? (
-                <Icon icon="CHECKED" />
-              ) : (
-                <Icon icon="UNCHECKED" />
-              )}
+                        warehouseArrivalAgreedDateApprovedBy: user,
+                      })
+                    }
+                  >
+                    <Icon icon="UNCHECKED" />
+                  </button>
+                )}
+              </div>
+
+              <div className={LabelStyle}>
+                <Label>
+                  <FormattedMessage
+                    id="components.cards.actualArrival"
+                    defaultMessage="ACTUAL ARRIVAL"
+                  />
+                </Label>
+              </div>
+              <div
+                className={InputIconStyle}
+                onClick={evt => evt.stopPropagation()}
+                role="presentation"
+              >
+                <FormField
+                  name={`container.${id}.warehouseArrivalActualDate`}
+                  initValue={warehouseArrivalActualDate}
+                >
+                  {({ name: fieldName, ...inputHandlers }) =>
+                    dateTimeInputFactory({
+                      width: '165px',
+                      height: '20px',
+                      name: fieldName,
+                      isNew: false,
+                      originalValue: warehouseArrivalActualDate,
+                      inputHandlers: {
+                        ...inputHandlers,
+                        onBlur: evt => {
+                          inputHandlers.onBlur(evt);
+                          update({
+                            ...container,
+                            warehouseArrivalActualDate: inputHandlers.value
+                              ? inputHandlers.value
+                              : null,
+                          });
+                        },
+                      },
+                    })
+                  }
+                </FormField>
+
+                {warehouseArrivalActualDateApprovedBy ? (
+                  <button
+                    type="button"
+                    className={ApprovalIconStyle(true)}
+                    onClick={() =>
+                      update({
+                        ...container,
+                        warehouseArrivalActualDateApprovedBy: null,
+                      })
+                    }
+                  >
+                    <Icon icon="CHECKED" />
+                  </button>
+                ) : (
+                  <button
+                    type="button"
+                    className={ApprovalIconStyle(false)}
+                    onClick={() =>
+                      update({
+                        ...container,
+                        warehouseArrivalActualDateApprovedBy: user,
+                      })
+                    }
+                  >
+                    <Icon icon="UNCHECKED" />
+                  </button>
+                )}
+              </div>
+
+              <div className={TagsWrapperStyle}>
+                {tags.length > 0 && tags.map(tag => <Tag key={tag.id} tag={tag} />)}
+              </div>
             </div>
           </div>
-
-          <div className={TagsWrapperStyle}>
-            {tags.length > 0 && tags.map(tag => <Tag key={tag.id} tag={tag} />)}
-          </div>
-        </div>
-      </div>
-    </BaseCard>
+        </BaseCard>
+      )}
+    </UserConsumer>
   );
 };
 
