3ffde5f7a4bf14b86f2b7b6d20377251a0432c70
Kaka Huynh <dunghd.it@gmail.com>
2019-01-30 09:55:19 +0900

    feat(relation-map): add auto fill batch ui

---
+++ src/modules/relationMapBeta/order/components/ActionNavbar/SplitBalancePanel/index.js
@@ -0,0 +1,31 @@
+// @flow
+import React from 'react';
+import Icon from 'components/Icon';
+import { FormattedMessage } from 'react-intl';
+import { BaseButton } from 'components/Buttons';
+import { Label } from 'components/Form';
+import messages from 'modules/relationMap/messages';
+import { colors } from 'styles/common';
+import * as style from './style';
+
+type Props = {
+  onClick: Function,
+};
+const SplitBalancePanel = ({ onClick }: Props) => (
+  <div className={style.ContainerWrapper}>
+    <div className={style.Header}>
+      <Icon icon="ORDER" />
+      <Label color={colors.TEAL}>
+        <FormattedMessage {...messages.autoFillBatch} />
+      </Label>
+      <Icon icon="BATCH" />
+    </div>
+    <BaseButton
+      icon="CONFIRM"
+      label={<FormattedMessage id="components.NavBar.filter.apply" defaultMessage="APPLY" />}
+      onClick={onClick}
+    />
+  </div>
+);
+
+export default SplitBalancePanel;
