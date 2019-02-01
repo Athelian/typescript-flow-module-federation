0480959f483d3391f9ee5815b190f6b0eaf2dcca
tostyle <unplugged2d@gmail.com>
2019-02-01 16:18:31 +0900

    fix(batch): add container to BatchUpdatE

--- src/modules/batch/form/mutation.js
+++ src/modules/batch/form/mutation.js
@@ -134,7 +134,6 @@ export const prepareUpdateBatchInput = (
     tags = [],
     batchAdjustments = [],
     archived,
-    container,
     ...rest
   }: Object,
   inShipmentOrBatchForm: boolean = true,
