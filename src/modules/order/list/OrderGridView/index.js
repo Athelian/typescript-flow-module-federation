0769ecfdcd623714cffb3317a850373cdfa4cd53
TJ Jiang <thaddeusjiang@gmail.com>
2019-02-14 14:16:11 +0900

    feat(order list): permission

--- src/modules/order/list/OrderGridView/index.js
+++ src/modules/order/list/OrderGridView/index.js
@@ -14,71 +14,89 @@ type Props = {
   onLoadMore: Function,
   hasMore: boolean,
   isLoading: boolean,
+  renderItem?: (item: Object) => React.Node,
 };
 
-const OrderGridView = ({ items, onLoadMore, hasMore, isLoading }: Props) => (
-  <PermissionConsumer>
-    {hasPermission => {
-      const canCreate = hasPermission('order.orders.create');
-      const canUpdate = hasPermission('order.orders.update');
-      return (
-        <GridView
-          onLoadMore={onLoadMore}
-          hasMore={hasMore}
-          isLoading={isLoading}
-          itemWidth="195px"
-          isEmpty={items.length === 0}
-          emptyMessage={
-            <FormattedMessage id="modules.Orders.noOrderFound" defaultMessage="No orders found" />
-          }
-        >
-          {items.map(item => (
-            <BooleanValue key={item.id}>
-              {({ value: statusDialogIsOpen, set: dialogToggle }) => (
-                <>
-                  {item.archived ? (
-                    <OrderActivateDialog
-                      onRequestClose={() => dialogToggle(false)}
-                      isOpen={statusDialogIsOpen}
-                      order={item}
-                    />
-                  ) : (
-                    <OrderArchiveDialog
-                      onRequestClose={() => dialogToggle(false)}
-                      isOpen={statusDialogIsOpen}
-                      order={item}
-                    />
-                  )}
-                  <OrderCard
-                    order={item}
-                    actions={[
-                      ...(canCreate
-                        ? [
-                            <CardAction
-                              icon="CLONE"
-                              onClick={() => navigate(`/order/clone/${encodeId(item.id)}`)}
-                            />,
-                          ]
-                        : []),
-                      ...(canUpdate
-                        ? [
-                            <CardAction
-                              icon={item.archived ? 'ACTIVE' : 'ARCHIVE'}
-                              onClick={() => dialogToggle(true)}
-                            />,
-                          ]
-                        : []),
-                    ]}
-                    showActionsOnHover
-                  />
-                </>
-              )}
-            </BooleanValue>
-          ))}
-        </GridView>
-      );
-    }}
-  </PermissionConsumer>
+const defaultRenderItem = ({ canCreate, canUpdate, ...item }: Object): React.Node => (
+  <BooleanValue key={item.id}>
+    {({ value: statusDialogIsOpen, set: dialogToggle }) => (
+      <>
+        {item.archived ? (
+          <OrderActivateDialog
+            onRequestClose={() => dialogToggle(false)}
+            isOpen={statusDialogIsOpen}
+            order={item}
+          />
+        ) : (
+          <OrderArchiveDialog
+            onRequestClose={() => dialogToggle(false)}
+            isOpen={statusDialogIsOpen}
+            order={item}
+          />
+        )}
+        <OrderCard
+          order={item}
+          actions={[
+            ...(canCreate
+              ? [
+                  <CardAction
+                    icon="CLONE"
+                    onClick={() => navigate(`/order/clone/${encodeId(item.id)}`)}
+                  />,
+                ]
+              : []),
+            ...(canUpdate
+              ? [
+                  <CardAction
+                    icon={item.archived ? 'ACTIVE' : 'ARCHIVE'}
+                    onClick={() => dialogToggle(true)}
+                  />,
+                ]
+              : []),
+          ]}
+          showActionsOnHover
+        />
+      </>
+    )}
+  </BooleanValue>
 );
+
+const defaultProps = {
+  renderItem: defaultRenderItem,
+};
+
+class OrderGridView extends React.PureComponent<Props> {
+  static defaultProps = defaultProps;
+
+  render() {
+    const { items, onLoadMore, hasMore, isLoading, renderItem = defaultRenderItem } = this.props;
+
+    return (
+      <PermissionConsumer>
+        {hasPermission => {
+          const canCreate = hasPermission('order.orders.create');
+          const canUpdate = hasPermission('order.orders.update');
+          return (
+            <GridView
+              onLoadMore={onLoadMore}
+              hasMore={hasMore}
+              isLoading={isLoading}
+              itemWidth="195px"
+              isEmpty={items.length === 0}
+              emptyMessage={
+                <FormattedMessage
+                  id="modules.Orders.noOrderFound"
+                  defaultMessage="No orders found"
+                />
+              }
+            >
+              {items.map(item => renderItem({ ...item, canCreate, canUpdate }))}
+            </GridView>
+          );
+        }}
+      </PermissionConsumer>
+    );
+  }
+}
 
 export default OrderGridView;
