d0a6a173e493a89dbe45ec4b524c0bc1430cfb83
Kaka Huynh <dunghd.it@gmail.com>
2019-02-14 15:03:45 +0900

    fix(permission): move the order to its file

--- src/modules/permission/constants/index.js
+++ src/modules/permission/constants/index.js
@@ -1,5 +1,6 @@
 import relationMap from './relationMap';
 import product from './product';
+import order from './order';
 
-export const defaultPermissions = [...product.default];
-export const managerPermissions = [...product.manager, ...relationMap];
+export const defaultPermissions = [...product.default, ...order.default];
+export const managerPermissions = [...product.manager, ...order.manager, ...relationMap];
---
+++ src/modules/permission/constants/order.js
@@ -0,0 +1,6 @@
+const order = {
+  default: ['order.orders.list', 'order.orders.get'],
+  manager: ['order.orders.list', 'order.orders.get', 'order.orders.create', 'order.orders.update'],
+};
+
+export default order;
