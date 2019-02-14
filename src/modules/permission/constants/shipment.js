d7d38bf1000bad81dd55075ed7dfb361f64a1ff2
Kaka Huynh <dunghd.it@gmail.com>
2019-02-14 16:25:28 +0900

    feat(shipment): hide some mutation action on the list

--- src/modules/permission/constants/shipment.js
+++ src/modules/permission/constants/shipment.js
@@ -1,7 +1,7 @@
-const SHIPMENT_LIST = 'shipment.shipments.list';
-const SHIPMENT_GET = 'shipment.shipments.get';
-const SHIPMENT_CREATE = 'shipment.shipments.create';
-const SHIPMENT_UPDATE = 'shipment.shipments.update';
+export const SHIPMENT_LIST = 'shipment.shipments.list';
+export const SHIPMENT_GET = 'shipment.shipments.get';
+export const SHIPMENT_CREATE = 'shipment.shipments.create';
+export const SHIPMENT_UPDATE = 'shipment.shipments.update';
 
 const shipment = {
   default: [SHIPMENT_LIST, SHIPMENT_GET],
