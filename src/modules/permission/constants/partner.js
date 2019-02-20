c151c3bf0c88489a8122921b3c28e31e5c4df6fe
Kaka Huynh <dunghd.it@gmail.com>
2019-02-20 15:08:57 +0900

    feat(permission): add constants for staff and partner

---
+++ src/modules/permission/constants/partner.js
@@ -0,0 +1,4 @@
+export const PARTNER_LIST = 'partner.partners.list';
+export const PARTNER_GET = 'partner.partners.get';
+export const PARTNER_CREATE = 'partner.partners.create';
+export const PARTNER_UPDATE = 'partner.partners.update';
---
+++ src/modules/permission/constants/staff.js
@@ -0,0 +1,4 @@
+export const STAFF_LIST = 'staff.staffs.list';
+export const STAFF_GET = 'staff.staffs.get';
+export const STAFF_CREATE = 'staff.staffs.create';
+export const STAFF_UPDATE = 'staff.staffs.update';
