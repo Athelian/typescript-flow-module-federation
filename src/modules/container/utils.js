57e9d83e7034d4495fe937c65ad76124109497dc
TJ Jiang <thaddeusjiang@gmail.com>
2019-02-22 17:52:10 +0900

    fix(shipment): fix container card volume not update

--- src/modules/container/utils.js
+++ src/modules/container/utils.js
@@ -1,5 +1,7 @@
 // @flow
 import { uniqBy } from 'lodash';
+import { isNullOrUndefined } from 'utils/fp';
+import { convert as convertVolume } from 'modules/form/helpers/metricInput/volumeInput';
 
 type BatchProp = {
   orderItem: {
@@ -9,7 +11,38 @@ type BatchProp = {
   },
 };
 
+type Metric = {
+  metric: string,
+  value: number,
+};
+
 export const uniqueOrders = (batches: Array<BatchProp>): Array<Object> =>
   uniqBy(batches.map(batch => batch.orderItem.order), 'id');
+
+const calculateBatchTotalVolume = (batch: Object): Metric => {
+  const {
+    packageQuantity = 0,
+    packageVolume,
+  }: {
+    packageQuantity: number,
+    packageVolume: Object,
+  } = batch;
+  const volume = isNullOrUndefined(packageVolume)
+    ? 0
+    : convertVolume(packageVolume.value, packageVolume.metric, 'm³');
+
+  return {
+    metric: 'm³',
+    value: packageQuantity * volume,
+  };
+};
+
+export const calculateContainerTotalVolume = ({ batches = [] }: Object): Metric => ({
+  metric: 'm³',
+  value: batches
+    .map(calculateBatchTotalVolume)
+    .map(volume => volume.value)
+    .reduce((a, b) => a + b, 0),
+});
 
 export default uniqueOrders;
