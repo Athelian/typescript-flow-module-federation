4650015bbc330bd3c19a45cc413d5c4fc3a7cef3
Kaka Huynh <dunghd.it@gmail.com>
2019-02-27 10:40:40 +0900

    fix(shipment): show toastr error when api crash

--- src/modules/shipment/index.form.js
+++ src/modules/shipment/index.form.js
@@ -2,6 +2,7 @@
 import * as React from 'react';
 import { FormattedMessage } from 'react-intl';
 import { Subscribe } from 'unstated';
+import { toast } from 'react-toastify';
 import { BooleanValue } from 'react-values';
 import { Mutation } from 'react-apollo';
 import { isNullOrUndefined } from 'utils/fp';
@@ -244,6 +245,11 @@ class ShipmentFormModule extends React.Component<Props> {
   }: ShipmentFormState) => (result: CreateShipmentResponse | UpdateShipmentResponse) => {
     const isNewOrClone = this.isNewOrClone();
     const { redirectAfterSuccess } = this.props;
+
+    if (!result) {
+      toast.error('There was an error. Please try again later');
+      return;
+    }
 
     if (isNewOrClone && result.shipmentCreate) {
       const { shipmentCreate } = result;
