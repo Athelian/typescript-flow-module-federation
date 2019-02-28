889a7eb9095bf56bc98bf5a532616d1d1b0e2389
Kaka Huynh <dunghd.it@gmail.com>
2019-02-28 16:36:12 +0900

    fix(relation-map): update order list after move to new shipment

--- src/modules/relationMap/order/index.js
+++ src/modules/relationMap/order/index.js
@@ -157,12 +157,16 @@ const Order = ({ intl }: Props) => {
                     const [, orderId] = item.split('-');
                     return orderId;
                   });
-                  prevResult.orders.nodes
-                    .filter(order => orderIds.includes(order.id))
-                    .forEach(order => {
-                      // insert on the top
-                      order.shipments.push(responseData.data.shipment);
-                    });
+                  if (prevResult && prevResult.orders && prevResult.orders.nodes) {
+                    prevResult.orders.nodes
+                      .filter(order => orderIds.includes(order.id))
+                      .forEach(order => {
+                        // insert on the top
+                        order.shipments.push(responseData.data.shipment);
+                      });
+                  } else {
+                    // TODO: fetch all orders, might need to refactor later
+                  }
                   scrollIntoView({
                     targetId: `shipment-${newShipmentId}`,
                   });
