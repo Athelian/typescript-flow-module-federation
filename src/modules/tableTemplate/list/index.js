f5ead0fef923ca2d706f68aeab6ef9dd98afc19c
Kaka Huynh <dunghd.it@gmail.com>
2019-03-05 15:33:55 +0900

    refactor(core): change the way to reload data when archive entity

--- src/modules/tableTemplate/list/index.js
+++ src/modules/tableTemplate/list/index.js
@@ -1,6 +1,7 @@
 // @flow
 import * as React from 'react';
 import { Query } from 'react-apollo';
+import apolloClient from 'apollo';
 import { getByPathWithDefault } from 'utils/fp';
 import loadMore from 'utils/loadMore';
 import emitter from 'utils/emitter';
@@ -19,44 +20,42 @@ type Props = {
   page: number,
 };
 
-class TableTemplateList extends React.Component<Props> {
-  render() {
-    const { viewType, ...filtersAndSort } = this.props;
+const TableTemplateList = ({ viewType, ...filtersAndSort }: Props) => {
+  React.useEffect(() => {
+    emitter.once('RELOAD_TEMPLATE', () => {
+      apolloClient.reFetchObservableQueries();
+    });
+  });
 
-    return (
-      <Query
-        query={tableTemplateQuery}
-        variables={{
-          page: 1,
-          ...filtersAndSort,
-        }}
-        fetchPolicy="network-only"
-      >
-        {({ loading, data, fetchMore, error, refetch }) => {
-          if (error) {
-            return error.message;
-          }
+  return (
+    <Query
+      query={tableTemplateQuery}
+      variables={{
+        page: 1,
+        ...filtersAndSort,
+      }}
+      fetchPolicy="network-only"
+    >
+      {({ loading, data, fetchMore, error }) => {
+        if (error) {
+          return error.message;
+        }
 
-          const nextPage = getByPathWithDefault(1, `maskEdits.page`, data) + 1;
-          const totalPage = getByPathWithDefault(1, `maskEdits.totalPage`, data);
-          const hasMore = nextPage <= totalPage;
+        const nextPage = getByPathWithDefault(1, `maskEdits.page`, data) + 1;
+        const totalPage = getByPathWithDefault(1, `maskEdits.totalPage`, data);
+        const hasMore = nextPage <= totalPage;
 
-          emitter.once('RELOAD_TEMPLATE', () => {
-            refetch();
-          });
-
-          return (
-            <TableTemplateGridView
-              items={getByPathWithDefault([], 'maskEdits.nodes', data)}
-              onLoadMore={() => loadMore({ fetchMore, data }, filtersAndSort, 'maskEdits')}
-              hasMore={hasMore}
-              isLoading={loading}
-            />
-          );
-        }}
-      </Query>
-    );
-  }
-}
+        return (
+          <TableTemplateGridView
+            items={getByPathWithDefault([], 'maskEdits.nodes', data)}
+            onLoadMore={() => loadMore({ fetchMore, data }, filtersAndSort, 'maskEdits')}
+            hasMore={hasMore}
+            isLoading={loading}
+          />
+        );
+      }}
+    </Query>
+  );
+};
 
 export default TableTemplateList;
