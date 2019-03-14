396a2657e282fbb92a8a634c86cd9551ef8d2828
maxime.perrimond <maxime.perrimond@zenport.io>
2019-03-14 18:40:22 +0900

    fix(timeline): use inject intl to fix build

--- src/modules/timeline/components/Field/index.js
+++ src/modules/timeline/components/Field/index.js
@@ -1,6 +1,6 @@
 // @flow
 import * as React from 'react';
-import { FormattedMessage } from 'react-intl';
+import { injectIntl, type IntlShape } from 'react-intl';
 import pluralize from 'pluralize';
 import { camelCase } from 'lodash/fp';
 import { FieldStyle } from './style';
@@ -8,18 +8,22 @@ import { FieldStyle } from './style';
 type Props = {
   field: string,
   entityType: string,
+  intl: IntlShape,
 };
 
-const Field = ({ field, entityType }: Props) => {
+const Field = ({ field, entityType, intl }: Props) => {
   const pluralizedEntityType = pluralize(entityType);
   const module = pluralizedEntityType.charAt(0).toUpperCase() + pluralizedEntityType.slice(1);
   const fieldName = camelCase(field);
 
   return (
     <span className={FieldStyle}>
-      <FormattedMessage id={`modules.${module}.${fieldName}`} defaultMessage={fieldName} />
+      {intl.formatMessage({
+        id: `modules.${module}.${fieldName}`,
+        defaultMessage: fieldName,
+      })}
     </span>
   );
 };
 
-export default Field;
+export default injectIntl(Field);
