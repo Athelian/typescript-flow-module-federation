808bca326d82c6903912f7dbe2644cde1a1841a4
Kaka Huynh <dunghd.it@gmail.com>
2019-03-20 15:54:21 +0900

    fix(custom-field): change the way to save the data

--- src/modules/metadata/components/FieldDefinitionsForm/index.js
+++ src/modules/metadata/components/FieldDefinitionsForm/index.js
@@ -1,7 +1,6 @@
 // @flow
 import * as React from 'react';
 import { FormattedMessage } from 'react-intl';
-import { Subscribe } from 'unstated';
 // $FlowFixMe https://github.com/atlassian/react-beautiful-dnd/issues/286#issuecomment-426604779
 import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 import {
@@ -9,26 +8,20 @@ import {
   CUSTOM_FIELD_DEFINITIONS_UPDATE,
   CUSTOM_FIELD_DEFINITIONS_DELETE,
 } from 'modules/permission/constants/customFields';
-import { PermissionConsumer } from 'modules/permission';
+import usePermission from 'hooks/usePermission';
 import { NewButton } from 'components/Buttons';
 import GridColumn from 'components/GridColumn';
 import { DefaultCustomFieldDefinitionStyle } from 'components/Form';
 import { uuid } from 'utils/id';
-import { isEquals } from 'utils/fp';
-import FieldDefinitionsContainer from 'modules/metadata/components/FieldDefinitionsFormWrapper/container';
 import { ContainerStyle, AddButtonWrapperStyle } from './style';
 
 type OptionalProps = {
   fieldDefinitions: Array<Object>,
-  onFormReady: Function,
+  setFieldArrayValue: Function,
+  removeArrayItem: Function,
 };
 
 type Props = OptionalProps & {};
-
-const defaultProps = {
-  fieldDefinitions: [],
-  onFormReady: () => {},
-};
 
 const reorder = (list, startIndex, endIndex) => {
   const result = Array.from(list);
@@ -37,124 +30,91 @@ const reorder = (list, startIndex, endIndex) => {
   return result;
 };
 
-class FieldDefinitionsForm extends React.Component<Props> {
-  static defaultProps = defaultProps;
+const FieldDefinitionsForm = ({ fieldDefinitions, setFieldArrayValue, removeArrayItem }: Props) => {
+  const { hasPermission } = usePermission();
+  const allowCreate = hasPermission(CUSTOM_FIELD_DEFINITIONS_CREATE);
+  const allowUpdate = hasPermission(CUSTOM_FIELD_DEFINITIONS_UPDATE);
+  const allowDelete = hasPermission(CUSTOM_FIELD_DEFINITIONS_DELETE);
 
-  componentDidMount() {
-    const { onFormReady } = this.props;
-    if (onFormReady) onFormReady();
-  }
+  return (
+    <div className={ContainerStyle}>
+      {allowUpdate ? (
+        <DragDropContext
+          onDragEnd={(result: any) => {
+            if (!result.destination) {
+              return;
+            }
 
-  shouldComponentUpdate(nextProps: Props) {
-    const { fieldDefinitions } = this.props;
-    return !isEquals(fieldDefinitions, nextProps.fieldDefinitions);
-  }
-
-  componentDidUpdate() {
-    const { onFormReady } = this.props;
-    if (onFormReady) onFormReady();
-  }
-
-  render() {
-    return (
-      <PermissionConsumer>
-        {hasPermission => {
-          const allowCreate = hasPermission(CUSTOM_FIELD_DEFINITIONS_CREATE);
-          const allowUpdate = hasPermission(CUSTOM_FIELD_DEFINITIONS_UPDATE);
-          const allowDelete = hasPermission(CUSTOM_FIELD_DEFINITIONS_DELETE);
-
-          return (
-            <Subscribe to={[FieldDefinitionsContainer]}>
-              {({ state, setFieldArrayValue, removeArrayItem }) => {
-                const { fieldDefinitions } = state;
-                return (
-                  <div className={ContainerStyle}>
-                    {allowUpdate ? (
-                      <DragDropContext
-                        onDragEnd={(result: any) => {
-                          if (!result.destination) {
-                            return;
-                          }
-
-                          setFieldArrayValue(
-                            'fieldDefinitions',
-                            reorder(fieldDefinitions, result.source.index, result.destination.index)
-                          );
-                        }}
+            setFieldArrayValue(
+              'fieldDefinitions',
+              reorder(fieldDefinitions, result.source.index, result.destination.index)
+            );
+          }}
+        >
+          <Droppable droppableId="droppable">
+            {dropProvided => (
+              <div ref={dropProvided.innerRef}>
+                <GridColumn gap="10px">
+                  {fieldDefinitions &&
+                    fieldDefinitions.map((fieldDefinition, index) => (
+                      <Draggable
+                        key={fieldDefinition.id}
+                        draggableId={fieldDefinition.id}
+                        index={index}
                       >
-                        <Droppable droppableId="droppable">
-                          {dropProvided => (
-                            <div ref={dropProvided.innerRef}>
-                              <GridColumn gap="10px">
-                                {fieldDefinitions &&
-                                  fieldDefinitions.map((fieldDefinition, index) => (
-                                    <Draggable
-                                      key={fieldDefinition.id}
-                                      draggableId={fieldDefinition.id}
-                                      index={index}
-                                    >
-                                      {provided => (
-                                        <div ref={provided.innerRef} {...provided.draggableProps}>
-                                          <DefaultCustomFieldDefinitionStyle
-                                            dragHandleProps={provided.dragHandleProps}
-                                            targetName={`fieldDefinitions.${index}`}
-                                            fieldName={fieldDefinition.name}
-                                            setFieldValue={setFieldArrayValue}
-                                            onRemove={() =>
-                                              removeArrayItem(`fieldDefinitions.${index}`)
-                                            }
-                                            editable
-                                            deletable={allowDelete}
-                                          />
-                                        </div>
-                                      )}
-                                    </Draggable>
-                                  ))}
-                              </GridColumn>
-                            </div>
-                          )}
-                        </Droppable>
-                      </DragDropContext>
-                    ) : (
-                      <GridColumn gap="10px">
-                        {fieldDefinitions &&
-                          fieldDefinitions.map(fieldDefinition => (
+                        {provided => (
+                          <div ref={provided.innerRef} {...provided.draggableProps}>
                             <DefaultCustomFieldDefinitionStyle
+                              dragHandleProps={provided.dragHandleProps}
+                              targetName={`fieldDefinitions.${index}`}
                               fieldName={fieldDefinition.name}
-                              editable={false}
+                              setFieldValue={setFieldArrayValue}
+                              onRemove={() => removeArrayItem(`fieldDefinitions.${index}`)}
+                              editable
                               deletable={allowDelete}
                             />
-                          ))}
-                      </GridColumn>
-                    )}
+                          </div>
+                        )}
+                      </Draggable>
+                    ))}
+                </GridColumn>
+              </div>
+            )}
+          </Droppable>
+        </DragDropContext>
+      ) : (
+        <GridColumn gap="10px">
+          {fieldDefinitions &&
+            fieldDefinitions.map(fieldDefinition => (
+              <DefaultCustomFieldDefinitionStyle
+                fieldName={fieldDefinition.name}
+                editable={false}
+                deletable={allowDelete}
+              />
+            ))}
+        </GridColumn>
+      )}
 
-                    {allowCreate && (
-                      <div className={AddButtonWrapperStyle}>
-                        <NewButton
-                          label={
-                            <FormattedMessage
-                              id="modules.metadata.addCustomField"
-                              defaultMessage="ADD CUSTOM FIELD"
-                            />
-                          }
-                          onClick={() => {
-                            setFieldArrayValue('fieldDefinitions', [
-                              ...fieldDefinitions,
-                              { id: uuid(), name: '', isNew: true },
-                            ]);
-                          }}
-                        />
-                      </div>
-                    )}
-                  </div>
-                );
-              }}
-            </Subscribe>
-          );
-        }}
-      </PermissionConsumer>
-    );
-  }
-}
+      {allowCreate && (
+        <div className={AddButtonWrapperStyle}>
+          <NewButton
+            label={
+              <FormattedMessage
+                id="modules.metadata.addCustomField"
+                defaultMessage="ADD CUSTOM FIELD"
+              />
+            }
+            onClick={() => {
+              setFieldArrayValue('fieldDefinitions', [
+                ...fieldDefinitions,
+                { id: uuid(), name: '', isNew: true },
+              ]);
+            }}
+          />
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default FieldDefinitionsForm;
