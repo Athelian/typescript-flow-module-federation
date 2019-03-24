b8ed16646c7bf7ceb66b32611d0a7ce39a40eb32
Kaka Huynh <dunghd.it@gmail.com>
2019-03-24 17:06:37 +0900

    refactor(relation-map): remove timeout for wrapper card

--- src/components/RelationMap/OrderElement/WrapperCard/index.js
+++ src/components/RelationMap/OrderElement/WrapperCard/index.js
@@ -3,66 +3,23 @@ import * as React from 'react';
 import * as style from './style';
 
 type OptionalProps = {
-  onMouseEnter?: Function,
-  onMouseLeave?: Function,
-  onClick?: Function,
-  onDoubleClick?: Function,
   fit: boolean,
 };
 type Props = OptionalProps & {
   children: React.Node,
 };
 
-class WrapperCard extends React.PureComponent<Props> {
-  static defaultProps = {
-    fit: false,
-  };
+const defaultProps = {
+  fit: false,
+};
+const WrapperCard = ({ children, fit, ...rest }: Props) => {
+  return (
+    <div role="presentation" className={style.OuterCardWrapperStyle(fit)} {...rest}>
+      <div className={style.InnerCardWrapperStyle}>{children}</div>
+    </div>
+  );
+};
 
-  constructor() {
-    super();
-    this.clickTimeout = null;
-  }
-
-  clearClickTimeout = () => {
-    if (this.clickTimeout) {
-      clearTimeout(this.clickTimeout);
-      this.clickTimeout = null;
-    }
-  };
-
-  handleClicks = () => {
-    const { onClick, onDoubleClick } = this.props;
-    if (this.clickTimeout !== null) {
-      if (onDoubleClick) {
-        onDoubleClick();
-      }
-      this.clearClickTimeout();
-    } else {
-      this.clickTimeout = setTimeout(() => {
-        if (onClick) {
-          onClick();
-        }
-        this.clearClickTimeout();
-      }, 200);
-    }
-  };
-
-  clickTimeout: ?TimeoutID;
-
-  render() {
-    const { onMouseEnter, onMouseLeave, children, fit } = this.props;
-    return (
-      <div
-        role="presentation"
-        className={style.OuterCardWrapperStyle(fit)}
-        onClick={this.handleClicks}
-        {...(onMouseEnter ? { onMouseEnter } : {})}
-        {...(onMouseLeave ? { onMouseLeave } : {})}
-      >
-        <div className={style.InnerCardWrapperStyle}>{children}</div>
-      </div>
-    );
-  }
-}
+WrapperCard.defaultProps = defaultProps;
 
 export default WrapperCard;
