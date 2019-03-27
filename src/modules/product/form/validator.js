68bfe2b911f56cea0487c0d0a965e7f8a45fd418
Kevin Nguyen <kcvin94@gmail.com>
2019-03-27 16:31:09 +0900

    fix(product): add front validation for jan and hs code

--- src/modules/product/form/validator.js
+++ src/modules/product/form/validator.js
@@ -1,9 +1,33 @@
 // @flow
+import * as React from 'react';
 import * as Yup from 'yup';
+import { FormattedMessage } from 'react-intl';
 
 export default Yup.object().shape({
   name: Yup.string().required(),
   serial: Yup.string().required(),
+  janCode: Yup.string().test(
+    'janCode',
+    <FormattedMessage
+      id="modules.Products.janCodeValidation"
+      defaultMessage="JAN Code must be exactly 13 characters"
+    />,
+    value => {
+      if (!value || (value && value.length === 13)) return true;
+      return false;
+    }
+  ),
+  hsCode: Yup.string().test(
+    'hsCode',
+    <FormattedMessage
+      id="modules.Products.hsCodeValidation"
+      defaultMessage="HS Code must be exactly 10 characters"
+    />,
+    value => {
+      if (!value || (value && value.length === 10)) return true;
+      return false;
+    }
+  ),
   productProviders: Yup.array()
     .of(Yup.string().min(1))
     .required(),
