63e1a80cd10662ba23aad480c5069351c2fa6df1
Kaka Huynh <dunghd.it@gmail.com>
2019-04-08 16:13:11 +0900

    feat(shipment): add validator for batch card on shipment/container

--- src/modules/container/form/validator.js
+++ src/modules/container/form/validator.js
@@ -3,4 +3,10 @@ import * as Yup from 'yup';
 
 export default Yup.object().shape({
   no: Yup.string().required(),
+  batches: Yup.array().of(
+    Yup.object().shape({
+      no: Yup.string().required(),
+      quantity: Yup.number(),
+    })
+  ),
 });
