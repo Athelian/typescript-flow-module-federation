0a77404c49c6bea35f3c447da0b2519afb5d946d
Kevin Nguyen <kcvin94@gmail.com>
2019-04-17 14:37:03 +0900

    fix(tm): fix autodates

--- src/modules/task/form/components/ParentEntity/components/ShipmentValueSpy/helper.js
+++ src/modules/task/form/components/ParentEntity/components/ShipmentValueSpy/helper.js
@@ -3,10 +3,17 @@ import { getByPath } from 'utils/fp';
 import { getLatestDate } from 'utils/shipment';
 
 export const getValueBy = (field: string, values: Object) => {
-  if (field.toLowerCase().includes('date')) {
-    return getByPath(field, values);
+  const currentValue = getByPath(field, values);
+  const { hasOwnProperty } = Object.prototype;
+
+  if (
+    currentValue &&
+    (hasOwnProperty.call(currentValue, 'timelineDateRevisions') ||
+      hasOwnProperty.call(currentValue, 'date'))
+  ) {
+    return getLatestDate(currentValue);
   }
-  return getLatestDate(getByPath(field, values));
+  return currentValue;
 };
 
 export default getValueBy;
