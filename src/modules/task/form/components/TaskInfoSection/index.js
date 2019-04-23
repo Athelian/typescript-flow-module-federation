fa56eb038e7350551f042c59d33dc756e4f65f81
Kaka Huynh <dunghd.it@gmail.com>
2019-04-23 16:16:33 +0900

    fix(task): add manual mode for binding data

--- src/modules/task/form/components/TaskInfoSection/index.js
+++ src/modules/task/form/components/TaskInfoSection/index.js
@@ -126,6 +126,8 @@ const TaskInfoSection = ({ intl, task, isInTemplate, hideParentInfo, parentEntit
     });
   }
 
+  const manualMode = parentEntity === 'Product';
+
   const parentValues = React.useRef(initDuration);
 
   const onChangeBinding = React.useCallback(
@@ -168,14 +170,27 @@ const TaskInfoSection = ({ intl, task, isInTemplate, hideParentInfo, parentEntit
             });
             break;
           }
-          default: {
+          case 'OrderItem':
+          case 'Order': {
             onChange({
               [`${field}Binding`]:
                 field === 'dueDate' ? START_DATE : orderBinding(intl).issuedAt.field,
               [`${field}Interval`]: { days: 0 },
             });
-            emitter.emit('FIND_ORDER_VALUE', {
+            emitter.emit(`FIND_${type.toUpperCase()}_VALUE`, {
               field: field === 'dueDate' ? START_DATE : orderBinding(intl).issuedAt.field,
+              entityId: getByPath('entity.id', task),
+              selectedField: field,
+            });
+            break;
+          }
+          default: {
+            onChange({
+              [`${field}Binding`]: START_DATE,
+              [`${field}Interval`]: { days: 0 },
+            });
+            emitter.emit(`FIND_${type.toUpperCase()}_VALUE`, {
+              START_DATE,
               entityId: getByPath('entity.id', task),
               selectedField: field,
             });
@@ -312,23 +327,25 @@ const TaskInfoSection = ({ intl, task, isInTemplate, hideParentInfo, parentEntit
                             />
                           </div>
 
-                          <div className={RadioWrapperStyle('bottom')}>
-                            <RadioInput
-                              align="right"
-                              selected={!manualSettings.startDate}
-                              onToggle={() =>
-                                manualSettings.startDate
-                                  ? onChangeBinding({
-                                      type: entity,
-                                      field: 'startDate',
-                                      isManual: false,
-                                      onChange: setFieldValues,
-                                    })
-                                  : () => {}
-                              }
-                              editable={editable}
-                            />
-                          </div>
+                          {!manualMode && (
+                            <div className={RadioWrapperStyle('bottom')}>
+                              <RadioInput
+                                align="right"
+                                selected={!manualSettings.startDate}
+                                onToggle={() =>
+                                  manualSettings.startDate
+                                    ? onChangeBinding({
+                                        type: entity,
+                                        field: 'startDate',
+                                        isManual: false,
+                                        onChange: setFieldValues,
+                                      })
+                                    : () => {}
+                                }
+                                editable={editable}
+                              />
+                            </div>
+                          )}
 
                           {isInTemplate ? (
                             <Display
@@ -503,17 +520,28 @@ const TaskInfoSection = ({ intl, task, isInTemplate, hideParentInfo, parentEntit
                             </ObjectValue>
                           ) : (
                             <Display color="GRAY_LIGHT" width="200px" height="30px">
-                              {editable ? (
-                                <FormattedMessage
-                                  id="modules.Tasks.chooseDataBinding"
-                                  defaultMessage="Choose data to sync from"
-                                />
-                              ) : (
-                                <FormattedMessage
-                                  id="modules.Tasks.noEventBindingChosen"
-                                  defaultMessage="No event binding chosen"
-                                />
-                              )}
+                              {(() => {
+                                if (manualMode) {
+                                  return (
+                                    <FormattedMessage
+                                      id="modules.Tasks.dataSyncingUnavailable"
+                                      defaultMessage="Data syncing unavailable"
+                                    />
+                                  );
+                                }
+
+                                return editable ? (
+                                  <FormattedMessage
+                                    id="modules.Tasks.chooseDataBinding"
+                                    defaultMessage="Choose data to sync from"
+                                  />
+                                ) : (
+                                  <FormattedMessage
+                                    id="modules.Tasks.noEventBindingChosen"
+                                    defaultMessage="No event binding chosen"
+                                  />
+                                );
+                              })()}
                             </Display>
                           )}
                         </GridColumn>
@@ -716,16 +744,28 @@ const TaskInfoSection = ({ intl, task, isInTemplate, hideParentInfo, parentEntit
                                     {({ ...inputHandlers }) => (
                                       <SelectInputFactory
                                         {...inputHandlers}
-                                        items={[
-                                          {
-                                            value: START_DATE,
-                                            label: intl.formatMessage({
-                                              id: 'modules.Tasks.startDate',
-                                              defaultMessage: 'START DATE',
-                                            }),
-                                          },
-                                          ...getFieldsByEntity(entity, intl),
-                                        ]}
+                                        items={
+                                          manualMode
+                                            ? [
+                                                {
+                                                  value: START_DATE,
+                                                  label: intl.formatMessage({
+                                                    id: 'modules.Tasks.startDate',
+                                                    defaultMessage: 'START DATE',
+                                                  }),
+                                                },
+                                              ]
+                                            : [
+                                                {
+                                                  value: START_DATE,
+                                                  label: intl.formatMessage({
+                                                    id: 'modules.Tasks.startDate',
+                                                    defaultMessage: 'START DATE',
+                                                  }),
+                                                },
+                                                ...getFieldsByEntity(entity, intl),
+                                              ]
+                                        }
                                         editable={editable}
                                         required
                                         hideTooltip
