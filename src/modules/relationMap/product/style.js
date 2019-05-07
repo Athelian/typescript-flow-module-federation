1760d39e3b229ce060b9397ea7fbe918da0741fa
Kaka Huynh <dunghd.it@gmail.com>
2019-05-07 11:14:32 +0900

    style(relation-map): change product focus view as feedback

--- src/modules/relationMap/product/style.js
+++ src/modules/relationMap/product/style.js
@@ -3,14 +3,14 @@ import { css } from 'react-emotion';
 import { layout, scrollbars } from 'styles/common';
 
 export const WrapperStyle: string = css`
-  height: 100vh;
-  overflow: auto;
-`;
-
-export const ProductListWrapperStyle: string = css`
+  height: 100%;
+  flex: 1;
   overflow-x: hidden;
   overflow-y: auto;
   ${scrollbars.MAIN};
+`;
+
+export const ProductListWrapperStyle: string = css`
   min-height: 0;
   display: grid;
   ${layout.GRID_VERTICAL};
