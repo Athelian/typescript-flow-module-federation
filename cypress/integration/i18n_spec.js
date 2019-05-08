0ecdfee4aed8b67e0c82a91d6c95ab6edaea7f58
TJ Jiang <thaddeusjiang@gmail.com>
2019-05-08 19:23:35 +0900

    test(i18n): fix e2e test

--- cypress/integration/i18n_spec.js
+++ cypress/integration/i18n_spec.js
@@ -11,7 +11,7 @@ describe('I18n', () => {
       cy.task('me', token).then(({ data: { viewer: { user } } }) => {
         cy.visit('/')
           .wait(1000)
-          .contains(user.language === 'en' ? 'RELATION MAP' : 'リレーションマップ');
+          .contains(user.language === 'en' ? 'RELATIONAL MAP' : 'リレーションマップ');
       });
     });
   });
@@ -20,19 +20,19 @@ describe('I18n', () => {
     cy.task('token').then(({ data: { login: { token } } }) => {
       cy.task('me', token).then(({ data: { viewer: { user } } }) => {
         cy.task('language', {
-          ...token,
+          token,
           variables: { id: user.id, input: { language: 'ja' } },
         }).then(() => {
           cy.visit('/')
             .wait(1000)
             .contains('リレーションマップ');
           cy.task('language', {
-            ...token,
+            token,
             variables: { id: user.id, input: { language: 'en' } },
           }).then(() => {
             cy.visit('/')
               .wait(1000)
-              .contains('RELATION MAP');
+              .contains('RELATIONAL MAP');
           });
         });
       });
