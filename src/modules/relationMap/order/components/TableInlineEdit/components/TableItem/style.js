05f90c4377c9df21cfe8d99bf98a0b4e670bee0b
Kevin Nguyen <kcvin94@gmail.com>
2019-05-09 17:30:54 +0900

    fix(toggle): style touchup

--- src/modules/relationMap/order/components/TableInlineEdit/components/TableItem/style.js
+++ src/modules/relationMap/order/components/TableInlineEdit/components/TableItem/style.js
@@ -7,6 +7,9 @@ export const WrapperStyle: string = css`
 `;
 
 export const ItemStyle: string = css`
+  display: flex;
+  align-items: center;
+  justify-content: center;
   width: 200px;
   height: 30px;
   margin: 5px;
