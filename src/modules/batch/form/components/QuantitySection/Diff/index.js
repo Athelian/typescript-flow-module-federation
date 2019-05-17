8a9f5f7fc6962d30c6892a65ab61ef3e9035b234
TJ Jiang <thaddeusjiang@gmail.com>
2019-05-17 19:41:16 +0900

    fix(batch): fix comments

--- src/modules/batch/form/components/QuantitySection/Diff/index.js
+++ src/modules/batch/form/components/QuantitySection/Diff/index.js
@@ -6,16 +6,20 @@ import FormattedNumber from 'components/FormattedNumber';
 
 const Diff = ({ before, after }: { before: number, after: number }) => {
   const diff = minus(after, before);
+  if (diff === 0) {
+    return null;
+  }
+
   if (diff < 0) {
     return (
-      <Display color="RED">
+      <Display color="RED" height="30px">
         <FormattedNumber value={diff} />
       </Display>
     );
   }
 
   return (
-    <Display color="TEAL">
+    <Display color="TEAL" height="30px">
       <FormattedNumber value={diff} prefix="+" />
     </Display>
   );
