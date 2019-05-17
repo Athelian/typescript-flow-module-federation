0950dfd0eae94ba70b066e504d8bc6391c53bb44
Kevin Nguyen <kcvin94@gmail.com>
2019-05-17 19:07:50 +0900

    fix(rm-edit): fix quantity adjustment styling

--- src/modules/relationMap/order/components/TableInlineEdit/components/TableItem/components/InlineNumberAdjustmentInput.js
+++ src/modules/relationMap/order/components/TableInlineEdit/components/TableItem/components/InlineNumberAdjustmentInput.js
@@ -1,11 +1,10 @@
 // @flow
 import * as React from 'react';
 import { FormattedMessage } from 'react-intl';
-import { DefaultStyle, NumberInput } from 'components/Form';
 import { NewButton } from 'components/Buttons';
-import { useNumberInput } from 'modules/form/hooks';
 import emitter from 'utils/emitter';
 import InlineSelectEnumInput from './InlineSelectEnumInput';
+import InlineNumberInput from './InlineNumberInput';
 import { InlineRowStyle } from './style';
 
 type OptionalProps = {
@@ -28,39 +27,23 @@ const defaultProps = {
 
 export default function InlineNumberAdjustmentInput({ name, value, isRequired, id }: Props) {
   const hasQuantityYet = !!value;
-  const { hasError, isFocused, ...inputHandlers } = useNumberInput(
-    value && value.quantity ? value.quantity : 0,
-    {
-      isRequired,
-    }
-  );
   return hasQuantityYet ? (
     <div className={InlineRowStyle}>
-      <DefaultStyle width="97.5px" type="text" isFocused={isFocused} hasError={hasError}>
-        <InlineSelectEnumInput
-          value={value && value.type ? value.type : ''}
-          enumType="BatchQuantityRevisionType"
-          id={`input-${id}`}
-          name={`${name}.type`}
-          width="97.5px"
-        />
-      </DefaultStyle>
-      <DefaultStyle width="97.5px" type="number" isFocused={isFocused} hasError={hasError}>
-        <NumberInput
-          name={`${name}.quantity`}
-          id={`input-${id}`}
-          {...inputHandlers}
-          onBlur={() => {
-            inputHandlers.onBlur();
-            emitter.emit('INLINE_CHANGE', {
-              hasError,
-              name: `${name}.quantity`,
-              value: inputHandlers.value,
-            });
-          }}
-          align="right"
-        />
-      </DefaultStyle>
+      <InlineSelectEnumInput
+        id={`input-${id}`}
+        name={`${name}.type`}
+        value={value && value.type ? value.type : ''}
+        enumType="BatchQuantityRevisionType"
+        isRequired={isRequired}
+        width="97.5px"
+      />
+      <InlineNumberInput
+        id={`input-${id}`}
+        name={`${name}.quantity`}
+        value={value && value.quantity ? value.quantity : 0}
+        isRequired={isRequired}
+        width="97.5px"
+      />
     </div>
   ) : (
     <NewButton
