c4fae028998c8c92d54779ee94d043a8167caa60
Dung Duc Huynh (Kaka) <870029+jellydn@users.noreply.github.com>
2019-05-23 11:40:00 +0900

    fix(place-holder): remove concurrent mode

--- src/components/PlaceHolder/QueryPlaceHolder/index.js
+++ src/components/PlaceHolder/QueryPlaceHolder/index.js
@@ -36,45 +36,42 @@ export default function QueryPlaceHolder({
   const isReady = useOnScreen(ref, { rootMargin: '0px', threshold: 0.2 });
 
   return (
-    // $FlowFixMe: not have type yet
-    <React.unstable_ConcurrentMode>
-      <div ref={ref}>
-        {(() => {
-          if (entityId) {
-            if (!isReady || isLoading) return <PlaceHolder />;
-
-            return (
-              <Query
-                query={query}
-                variables={{
-                  id: entityId,
-                }}
-                fetchPolicy="network-only"
-                onCompleted={onCompleted}
-              >
-                {({ loading, data, error }) => {
-                  if (error) {
-                    if (error.message && error.message.includes('403')) {
-                      navigate('/403');
-                    }
-
-                    return error.message;
-                  }
-
-                  if (loading) return <PlaceHolder />;
-
-                  return children({ data });
-                }}
-              </Query>
-            );
-          }
-
+    <div ref={ref}>
+      {(() => {
+        if (entityId) {
           if (!isReady || isLoading) return <PlaceHolder />;
 
-          return children({});
-        })()}
-      </div>
-    </React.unstable_ConcurrentMode>
+          return (
+            <Query
+              query={query}
+              variables={{
+                id: entityId,
+              }}
+              fetchPolicy="network-only"
+              onCompleted={onCompleted}
+            >
+              {({ loading, data, error }) => {
+                if (error) {
+                  if (error.message && error.message.includes('403')) {
+                    navigate('/403');
+                  }
+
+                  return error.message;
+                }
+
+                if (loading) return <PlaceHolder />;
+
+                return children({ data });
+              }}
+            </Query>
+          );
+        }
+
+        if (!isReady || isLoading) return <PlaceHolder />;
+
+        return children({});
+      })()}
+    </div>
   );
 }
 
