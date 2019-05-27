aac4eb6a27e7d58fdfe013ec5eb93c938b55f28f
t.kaseda <kaseda@sendee.jp>
2019-05-27 19:08:05 +0900

    feat(task): fix udpateat and createdat inverse

--- src/modules/task/index.list.js
+++ src/modules/task/index.list.js
@@ -32,8 +32,8 @@ const TaskModule = (props: Props) => {
   const { intl } = props;
 
   const sortFields = [
-    { title: intl.formatMessage(messages.updatedAt), value: 'createdAt' },
-    { title: intl.formatMessage(messages.createdAt), value: 'updatedAt' },
+    { title: intl.formatMessage(messages.updatedAt), value: 'updatedAt' },
+    { title: intl.formatMessage(messages.createdAt), value: 'createdAt' },
     { title: intl.formatMessage(messages.name), value: 'name' },
     { title: intl.formatMessage(messages.startDate), value: 'startDate' },
     { title: intl.formatMessage(messages.dueDate), value: 'dueDate' },
