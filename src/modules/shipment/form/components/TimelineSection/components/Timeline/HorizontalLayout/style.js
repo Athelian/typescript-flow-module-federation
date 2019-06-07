ee722a36278dc656e3964d332b9f48ba042d6a96
TJ Jiang <thaddeusjiang@gmail.com>
2019-06-07 17:39:06 +0900

    feat(shipment card): unclick when no form permission

--- src/modules/shipment/form/components/TimelineSection/components/Timeline/HorizontalLayout/style.js
+++ src/modules/shipment/form/components/TimelineSection/components/Timeline/HorizontalLayout/style.js
@@ -2,12 +2,13 @@
 import { css } from 'react-emotion';
 import { layout } from 'styles/common';
 
-export const HorizontalLayoutWrapperStyle: string = css`
+export const HorizontalLayoutWrapperStyle = (clickable: boolean) => css`
   ${layout.GRID_VERTICAL};
   width: 100%;
   padding: 10px 0;
   grid-gap: 5px;
   padding: 10px 5px 10px 20px;
+  ${!clickable && 'pointer-events: none'};
 `;
 
 export default HorizontalLayoutWrapperStyle;
