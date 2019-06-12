70ff6be955abb11071070cc1065d7ba096ee7db0
Kaka Huynh <dunghd.it@gmail.com>
2019-06-12 11:23:12 +0900

    fix(task): remove check task form permission

--- src/modules/task/index.js
+++ src/modules/task/index.js
@@ -3,19 +3,17 @@ import * as React from 'react';
 import { Provider } from 'unstated';
 import { Router } from '@reach/router';
 import withForbidden from 'hoc/withForbidden';
-import { TASK_FORM, TASK_LIST } from 'modules/permission/constants/task';
+import { TASK_LIST } from 'modules/permission/constants/task';
 import TaskListModule from './index.list';
 import TaskFormModule from './index.form';
 
-const TaskFormModuleWrapper = withForbidden(TaskFormModule, TASK_FORM);
 const TaskModuleListWrapper = withForbidden(TaskListModule, TASK_LIST);
 
 const TaskApp = () => (
   <Provider>
     <Router>
       <TaskModuleListWrapper path="/" />
-      <TaskFormModuleWrapper path="new" />
-      <TaskFormModuleWrapper path=":taskId" />
+      <TaskFormModule path=":taskId" />
     </Router>
   </Provider>
 );
