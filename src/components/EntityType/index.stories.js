3469cac0f4a90ca6ec2c6517ffb24ad4114471e3
Kaka Huynh <dunghd.it@gmail.com>
2019-06-19 17:04:47 +0900

    chore(components): remove unused

--- src/components/EntityType/index.stories.js
+++
@@ -1,6 +0,0 @@
-/* eslint-disable import/no-extraneous-dependencies */
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-import EntityType from './index';
-
-storiesOf('EntityType', module).add('default', () => <EntityType name="test" />);
