9aba8f9f46b95b6e9e0c95ffa6229f80d25b480d
TJ Jiang <thaddeusjiang@gmail.com>
2019-06-21 17:03:07 +0900

    refactor(storybook): refactor

--- src/components/Cards/GrayCard/index.stories.js
+++ src/components/Cards/GrayCard/index.stories.js
@@ -1,8 +1,7 @@
 import * as React from 'react';
 /* eslint-disable import/no-extraneous-dependencies */
 import { storiesOf } from '@storybook/react';
-// action
 
 import GrayCard from './index';
 
-storiesOf('GrayCard', module).add('default', () => <GrayCard width="400px" height="400px" />);
+storiesOf('Card', module).add('GrayCard', () => <GrayCard width="400px" height="400px" />);
