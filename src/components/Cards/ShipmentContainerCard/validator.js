e79666b9656a9e029843b35fb30324f5720e68c9
Kaka Huynh <dunghd.it@gmail.com>
2019-07-03 18:24:36 +0900

    fix(flow): add type for validation

--- src/components/Cards/ShipmentContainerCard/validator.js
+++ src/components/Cards/ShipmentContainerCard/validator.js
@@ -3,10 +3,10 @@ import * as React from 'react';
 import * as Yup from 'yup';
 import { FormattedMessage } from 'react-intl';
 
-const validator = ({ no }: { no: string }) =>
+const validator = ({ no }: { no: string }): Object =>
   Yup.object().shape({
     [no]: Yup.string().required(
-      <FormattedMessage id="validation.no.required" defaultMessage="No is required field" />
+      (<FormattedMessage id="validation.no.required" defaultMessage="No is required field" />: any)
     ),
   });
 
