631968697afbe2c5ed488631c8d4309d81b388e5
TJ Jiang <thaddeusjiang@gmail.com>
2019-07-08 14:04:13 +0900

    feat(select warehouse): add filter and sort

--- src/modules/warehouse/common/SelectWareHouse/index.js
+++ src/modules/warehouse/common/SelectWareHouse/index.js
@@ -1,17 +1,23 @@
 // @flow
 import * as React from 'react';
+import { injectIntl } from 'react-intl';
+import type { IntlShape } from 'react-intl';
 import { Query } from 'react-apollo';
 import { ObjectValue } from 'react-values';
 import { isEquals, getByPathWithDefault } from 'utils/fp';
 import loadMore from 'utils/loadMore';
 import Layout from 'components/Layout';
-import { SlideViewNavBar, EntityIcon } from 'components/NavBar';
+import FilterToolBar from 'components/common/FilterToolBar';
+import useFilter from 'hooks/useFilter';
+import { SlideViewNavBar } from 'components/NavBar';
 import { SaveButton, CancelButton } from 'components/Buttons';
 import WarehouseGridView from 'modules/warehouse/list/WarehouseGridView';
 import { WarehouseCard } from 'components/Cards';
 import { warehouseListQuery } from 'modules/warehouse/list/query';
+import messages from 'modules/warehouse/messages';
 
 type Props = {
+  intl: IntlShape,
   selected?: ?{
     id: string,
     name: string,
@@ -27,67 +33,96 @@ const defaultProps = {
   },
 };
 
-const SelectWareHouse = ({ selected, onCancel, onSelect }: Props) => (
-  <Query
-    query={warehouseListQuery}
-    variables={{
-      page: 1,
-      perPage: 10,
-    }}
-    fetchPolicy="network-only"
-  >
-    {({ loading, data, fetchMore, error }) => {
-      if (error) {
-        return error.message;
-      }
-      const nextPage = getByPathWithDefault(1, 'warehouses.page', data) + 1;
-      const totalPage = getByPathWithDefault(1, 'warehouses.totalPage', data);
-      const hasMore = nextPage <= totalPage;
+const getInitFilter = () => {
+  const state = {
+    filter: {},
+    sort: {
+      field: 'updatedAt',
+      direction: 'DESCENDING',
+    },
+    perPage: 10,
+    page: 1,
+  };
+  return state;
+};
 
-      return (
-        <ObjectValue defaultValue={selected}>
-          {({ value, set }) => (
-            <Layout
-              navBar={
-                <SlideViewNavBar>
-                  <EntityIcon icon="WAREHOUSE" color="WAREHOUSE" />
-                  <CancelButton onClick={onCancel} />
-                  <SaveButton
-                    disabled={isEquals(value, selected)}
-                    onClick={() => onSelect(value)}
-                  />
-                </SlideViewNavBar>
-              }
-            >
-              <WarehouseGridView
-                hasMore={hasMore}
-                isLoading={loading}
-                onLoadMore={() => loadMore({ fetchMore, data }, {}, 'warehouses')}
-                items={getByPathWithDefault([], 'warehouses.nodes', data)}
-                renderItem={({ item }) => (
-                  <WarehouseCard
-                    warehouse={item}
-                    onSelect={() => {
-                      if (value && item.id === value.id) {
-                        set(null);
-                      } else {
-                        set(item);
-                      }
-                    }}
-                    selectable
-                    selected={value && item.id === value.id}
-                    key={item.id}
-                  />
-                )}
-              />
-            </Layout>
-          )}
-        </ObjectValue>
-      );
-    }}
-  </Query>
-);
+const SelectWareHouse = ({ intl, selected, onCancel, onSelect }: Props) => {
+  const sortFields = [
+    { title: intl.formatMessage(messages.updatedAt), value: 'updatedAt' },
+    { title: intl.formatMessage(messages.createdAt), value: 'createdAt' },
+  ];
+  const { filterAndSort, queryVariables, onChangeFilter } = useFilter(
+    getInitFilter(),
+    'filterWarehouse'
+  );
 
+  return (
+    <Query
+      query={warehouseListQuery}
+      variables={{
+        page: 1,
+        perPage: 10,
+        ...queryVariables,
+      }}
+      fetchPolicy="network-only"
+    >
+      {({ loading, data, fetchMore, error }) => {
+        if (error) {
+          return error.message;
+        }
+        const nextPage = getByPathWithDefault(1, 'warehouses.page', data) + 1;
+        const totalPage = getByPathWithDefault(1, 'warehouses.totalPage', data);
+        const hasMore = nextPage <= totalPage;
+
+        return (
+          <ObjectValue defaultValue={selected}>
+            {({ value, set }) => (
+              <Layout
+                navBar={
+                  <SlideViewNavBar>
+                    <FilterToolBar
+                      icon="WAREHOUSE"
+                      sortFields={sortFields}
+                      filtersAndSort={filterAndSort}
+                      onChange={onChangeFilter}
+                    />
+                    <CancelButton onClick={onCancel} />
+                    <SaveButton
+                      disabled={isEquals(value, selected)}
+                      onClick={() => onSelect(value)}
+                    />
+                  </SlideViewNavBar>
+                }
+              >
+                <WarehouseGridView
+                  hasMore={hasMore}
+                  isLoading={loading}
+                  onLoadMore={() => loadMore({ fetchMore, data }, queryVariables, 'warehouses')}
+                  items={getByPathWithDefault([], 'warehouses.nodes', data)}
+                  renderItem={({ item }) => (
+                    <WarehouseCard
+                      warehouse={item}
+                      onSelect={() => {
+                        if (value && item.id === value.id) {
+                          set(null);
+                        } else {
+                          set(item);
+                        }
+                      }}
+                      selectable
+                      selected={value && item.id === value.id}
+                      key={item.id}
+                    />
+                  )}
+                />
+              </Layout>
+            )}
+          </ObjectValue>
+        );
+      }}
+    </Query>
+  );
+};
 SelectWareHouse.defaultProps = defaultProps;
 
-export default SelectWareHouse;
+export default injectIntl(SelectWareHouse);
