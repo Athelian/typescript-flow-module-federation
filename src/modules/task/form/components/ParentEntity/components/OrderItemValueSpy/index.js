8d1d6f8c6d555af36a2a1136e2da47a9b514e4ab
Kaka Huynh <dunghd.it@gmail.com>
2019-07-08 15:58:45 +0900

    feat(task): add binding for order item

--- src/modules/task/form/components/ParentEntity/components/OrderItemValueSpy/index.js
+++ src/modules/task/form/components/ParentEntity/components/OrderItemValueSpy/index.js
@@ -1,14 +1,14 @@
 // @flow
 import * as React from 'react';
-import type { Order, Task } from 'generated/graphql';
 import client from 'apollo';
 import emitter from 'utils/emitter';
 import { getByPath } from 'utils/fp';
 import logger from 'utils/logger';
-import { START_DATE } from 'modules/task/form/components/TaskInfoSection/constants';
 import { decodeId } from 'utils/id';
-import { calculateDate, findDuration } from 'modules/task/form/components/TaskInfoSection/helpers';
 import { orderItemAutoDateQuery } from './query';
+import { mappingDate } from '../mappingDate';
+import { autoCalculateDate, bindingRelateField } from '../autoCalculateDate';
+import type { Offset, BindingField, Duration } from '../type.js.flow';
 
 type Props = {
   values: Object,
@@ -22,27 +22,34 @@ export const MappingFields = {
   OrderItemOrderIssuedAt: 'issuedAt',
   ProjectDueDate: 'milestone.project.dueDate',
   MilestoneDueDate: 'milestone.dueDate',
-};
-
-const mappingDate = ({ field, task, values }: { field: string, task: Task, values: Order }) => {
-  const path = MappingFields[field] || 'N/A';
-  if (path.includes('milestone')) {
-    return getByPath(path, task);
-  }
-
-  return getByPath(path, values);
+  TaskStartDate: 'startDate',
+  TaskDueDate: 'dueDate',
 };
 
 export default function OrderItemValueSpy({ entity, values, task, location, setTaskValue }: Props) {
   React.useEffect(() => {
     emitter.addListener('FIND_ORDERITEM_VALUE', (bindingData: mixed) => {
       const field = getByPath('field', bindingData);
-      const selectedField = getByPath('selectedField', bindingData);
-      const autoDateDuration = getByPath('autoDateDuration', bindingData);
-      const autoDateOffset = getByPath('autoDateOffset', bindingData);
+      const selectedField: BindingField = getByPath('selectedField', bindingData);
+      const autoDateDuration: Duration = getByPath('autoDateDuration', bindingData);
+      const autoDateOffset: Offset = getByPath('autoDateOffset', bindingData);
+      const hasCircleBindingError: boolean = getByPath('hasCircleBindingError', bindingData);
+      logger.warn({
+        field,
+        selectedField,
+        autoDateDuration,
+        autoDateOffset,
+        hasCircleBindingError,
+      });
+
+      if (hasCircleBindingError) {
+        setTaskValue('dueDate', '');
+        setTaskValue('startDate', '');
+        return;
+      }
+
       const { pathname } = location;
-      const isUnderRelationMapOrProject =
-        pathname.includes('/relation-map') || pathname.includes('/project');
+      const isUnderOrderModule = pathname.includes('/order/');
       const [, activeType, orderItemId] = pathname.split('/') || [];
       logger.warn({
         field,
@@ -50,10 +57,12 @@ export default function OrderItemValueSpy({ entity, values, task, location, setT
         orderItemId,
         location,
         entity,
+        activeType,
       });
       // We will query the order data if open a task on relation map or from order item detail
-      if (isUnderRelationMapOrProject || (orderItemId && activeType === 'order-item')) {
-        const entityId = isUnderRelationMapOrProject ? entity.id : decodeId(orderItemId);
+      if (!isUnderOrderModule) {
+        const entityId =
+          orderItemId && activeType === 'order-item' ? decodeId(orderItemId) : task.entity.id;
         logger.warn('query order data for id', client, entityId);
         // TODO: This flag will be used for showing loading on UI
         emitter.emit('LIVE_VALUE_PROCESS', true);
@@ -68,91 +77,43 @@ export default function OrderItemValueSpy({ entity, values, task, location, setT
 
             logger.warn('order item data', data);
             const { order } = data.orderItem;
-
-            let date = mappingDate({ field, task, values: order });
-            if (autoDateDuration) {
-              date = calculateDate({
-                date,
-                duration: autoDateDuration.metric,
-                offset:
-                  autoDateOffset === 'after' ? autoDateDuration.value : -autoDateDuration.value,
-              });
-            }
-
-            if (field !== START_DATE) {
-              setTaskValue(selectedField, date);
-              emitter.emit('LIVE_VALUE', field, date);
-            } else {
-              setTaskValue(
-                selectedField,
-                autoDateDuration
-                  ? calculateDate({
-                      date: task.startDate,
-                      duration: autoDateDuration.metric,
-                      offset:
-                        autoDateOffset === 'after'
-                          ? autoDateDuration.value
-                          : -autoDateDuration.value,
-                    })
-                  : task.startDate
-              );
-            }
-
-            // we need to set the due date if those field are binding together
-            if (selectedField === 'startDate') {
-              if (task.dueDateBinding === START_DATE) {
-                const { weeks, months, days } = task.dueDateInterval || {};
-                setTaskValue(
-                  'dueDate',
-                  calculateDate({
-                    date,
-                    duration: findDuration({ weeks, months }),
-                    offset: weeks || months || days,
-                  })
-                );
-              }
-            }
+            let date = mappingDate({
+              field,
+              task,
+              values: order,
+              mappingFields: MappingFields,
+            });
+            date = autoCalculateDate({
+              autoDateDuration,
+              date,
+              autoDateOffset,
+              field,
+              setTaskValue,
+              selectedField,
+            });
+            bindingRelateField({
+              selectedField,
+              task,
+              date,
+              setTaskValue,
+            });
           });
       } else {
-        let date = mappingDate({ field, task, values });
-        if (autoDateDuration) {
-          date = calculateDate({
-            date,
-            duration: autoDateDuration.metric,
-            offset: autoDateOffset === 'after' ? autoDateDuration.value : -autoDateDuration.value,
-          });
-        }
-        if (field !== START_DATE) {
-          setTaskValue(selectedField, date);
-          emitter.emit('LIVE_VALUE', field, date);
-        } else {
-          setTaskValue(
-            selectedField,
-            autoDateDuration
-              ? calculateDate({
-                  date: task.startDate,
-                  duration: autoDateDuration.metric,
-                  offset:
-                    autoDateOffset === 'after' ? autoDateDuration.value : -autoDateDuration.value,
-                })
-              : task.startDate
-          );
-        }
-
-        // we need to set the due date if those field are binding together
-        if (selectedField === 'startDate') {
-          if (task.dueDateBinding === START_DATE) {
-            const { weeks, months, days } = task.dueDateInterval || {};
-            setTaskValue(
-              'dueDate',
-              calculateDate({
-                date,
-                duration: findDuration({ weeks, months }),
-                offset: weeks || months || days,
-              })
-            );
-          }
-        }
+        let date = mappingDate({ field, task, values, mappingFields: MappingFields });
+        date = autoCalculateDate({
+          autoDateDuration,
+          date,
+          autoDateOffset,
+          field,
+          setTaskValue,
+          selectedField,
+        });
+        bindingRelateField({
+          selectedField,
+          date,
+          task,
+          setTaskValue,
+        });
       }
     });
 
