4ecd65a78e308c581085b5dc218e0e734b8cfe40
TJ Jiang <thaddeusjiang@gmail.com>
2019-08-28 10:55:59 +0900

    fix(product providers section): remove item without removeNulls

--- src/modules/product/form/containers/productProviders.js
+++ src/modules/product/form/containers/productProviders.js
@@ -3,7 +3,7 @@ import { Container } from 'unstated';
 import { set, unset, cloneDeep } from 'lodash';
 import update from 'immutability-helper';
 import { isEquals } from 'utils/fp';
-import { removeNulls, extractForbiddenId } from 'utils/data';
+import { extractForbiddenId } from 'utils/data';
 
 type Price = {
   amount: number,
@@ -61,11 +61,11 @@ export default class ProductProvidersContainer extends Container<FormState> {
     );
   };
 
-  removeArrayItem = (path: string) => {
+  removeArrayItem = (index: number) => {
     this.setState(prevState => {
       const cloneState = cloneDeep(prevState);
-      unset(cloneState, path);
-      return removeNulls(cloneState);
+      unset(cloneState, `productProviders.${index}`);
+      return cloneState;
     });
   };
 
