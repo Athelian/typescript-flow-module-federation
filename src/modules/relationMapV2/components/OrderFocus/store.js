f3899696a58f2d307d0c0b04a084f4f889f08aa5
Kaka Huynh <dunghd.it@gmail.com>
2019-08-29 16:37:04 +0900

    fix(nrm): remove the targeting entities after edit entity

--- src/modules/relationMapV2/components/OrderFocus/store.js
+++ src/modules/relationMapV2/components/OrderFocus/store.js
@@ -1,8 +1,10 @@
 // @flow
 import { createContext } from 'react';
-import type { OrderPayload } from 'generated/graphql';
+import type { OrderPayload, OrderItemPayload } from 'generated/graphql';
+import { intersection } from 'lodash';
 import produce from 'immer';
 import update from 'immutability-helper';
+import { ORDER_ITEM, BATCH } from 'modules/relationMapV2/constants';
 import type { State } from './type.js.flow';
 
 type ContextProps = {|
@@ -52,6 +54,7 @@ export function reducer(
       | 'TARGET'
       | 'TARGET_ALL'
       | 'TARGET_TREE'
+      | 'RECHECK_TARGET'
       | 'DND'
       | 'START_DND'
       | 'END_DND'
@@ -65,6 +68,9 @@ export function reducer(
       entity?: string,
       targets?: Array<string>,
       orders?: Array<OrderPayload>,
+      orderUpdate?: OrderPayload,
+      orderItemUpdate?: OrderItemPayload,
+      mapping?: Object,
       [string]: mixed,
     },
   }
@@ -155,6 +161,69 @@ export function reducer(
           }
         });
       });
+    case 'RECHECK_TARGET': {
+      // $FlowIssue order may be forbidden or not found so flow complain about the id is not found on this case
+      if (action.payload?.orderUpdate?.id) {
+        return produce(state, draft => {
+          // $FlowIssue order may be forbidden or not found so flow complain about the id is not found on this case
+          const orderId = action.payload?.orderUpdate?.id ?? '';
+          if (!orderId) {
+            return;
+          }
+          // $FlowIssue order may be forbidden or not found so flow complain about the orderItems is not found on this case
+          const orderItems = action.payload?.orderUpdate?.orderItems ?? [];
+          const previousIds = {
+            orderItemIds: action.payload?.mapping?.orders?.[orderId].orderItems ?? [],
+            mapping: {},
+          };
+          previousIds.orderItemIds.forEach(itemId => {
+            previousIds.mapping[itemId] =
+              action.payload?.mapping?.orderItems?.[itemId]?.batches ?? [];
+          });
+          const orderItemIds = orderItems.map(item => item.id);
+          const existItemIds = intersection(previousIds.orderItemIds, orderItemIds);
+          previousIds.orderItemIds.forEach(itemId => {
+            if (!existItemIds.includes(itemId)) {
+              // remove from targeting
+              draft.targets.splice(draft.targets.indexOf(`${ORDER_ITEM}-${itemId || ''}`), 1);
+              previousIds.mapping[itemId].forEach(batchId => {
+                draft.targets.splice(draft.targets.indexOf(`${BATCH}-${batchId || ''}`), 1);
+              });
+            } else {
+              // find a batch was removed from item
+              const existBatchIds = intersection(
+                previousIds.mapping[itemId],
+                (orderItems?.[itemId]?.batches ?? []).map(batch => batch.id)
+              );
+              previousIds.mapping[itemId].forEach(batchId => {
+                if (!existBatchIds.includes(batchId))
+                  draft.targets.splice(draft.targets.indexOf(`${BATCH}-${batchId || ''}`), 1);
+              });
+            }
+          });
+        });
+      }
+
+      // $FlowIssue order item may be forbidden or not found so flow complain about the id is not found on this case
+      if (action.payload?.orderItemUpdate?.id) {
+        return produce(state, draft => {
+          // $FlowIssue order item may be forbidden or not found so flow complain about the id is not found on this case
+          const itemId = action.payload?.orderItemUpdate?.id ?? '';
+          const previousBatchIds = action.payload?.mapping?.orderItems?.[itemId]?.batches ?? [];
+          // $FlowIssue order may be forbidden or not found so flow complain about the orderItems is not found on this case
+          const batches = action.payload?.orderItemUpdate?.batches ?? [];
+          const existBatchIds = intersection(previousBatchIds, batches.map(batch => batch.id));
+
+          // find a batch was removed from item
+          previousBatchIds.forEach(batchId => {
+            if (!existBatchIds.includes(batchId))
+              draft.targets.splice(draft.targets.indexOf(`${BATCH}-${batchId || ''}`), 1);
+          });
+        });
+      }
+
+      return state;
+    }
     case 'DND': {
       return update(state, {
         moveEntity: {
