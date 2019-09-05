110282a37ac69c15dd4a2d656289bb6d75423199
Kevin Nguyen <kcvin94@gmail.com>
2019-09-05 13:35:26 +0900

    fix(project): styling fix for end padding of scroll

--- src/modules/project/form/components/ProjectSection/style.js
+++ src/modules/project/form/components/ProjectSection/style.js
@@ -84,23 +84,19 @@ export const ExpandWrapperStyle = css`
   }
 `;
 
-export const ProjectCardBodyStyle = (numOfMilestones: number): string => css`
+export const ProjectCardBodyStyle = (numOfColumns: number): string => css`
   display: grid;
-  grid-auto-columns: minmax(143.75px, 1fr);
-  grid-auto-flow: column;
+  grid-template-columns: repeat(${numOfColumns}, minmax(143.75px, 1fr)) 35px;
   width: 100%;
   height: 100%;
   overflow-y: hidden;
   overflow-x: overlay;
-  padding: 10px 35px;
+  padding: 10px 0 10px 20px;
   ${scrollbars.SMALL};
-  ${numOfMilestones > 6 &&
-    `
   &::after {
-    content: "";
+    content: '';
     display: block;
     width: 35px;
     height: 100%;
   }
-`};
 `;
