590f4e5a5d7a0cc612dfc8332f9e0e694e1ffbe8
TJ Jiang <thaddeusjiang@gmail.com>
2019-09-20 10:42:41 +0900

    fix(milestone section): styling

--- src/modules/projectTemplate/form/components/MilestonesSection/style.js
+++ src/modules/projectTemplate/form/components/MilestonesSection/style.js
@@ -1,11 +1,13 @@
 // @flow
 import { css } from 'react-emotion';
-import { scrollbars } from 'styles/common';
+import { presets, scrollbars } from 'styles/common';
 
 export const MilestonesSectionWrapperStyle: string = css`
+  ${presets.BOX};
   display: flex;
   flex: 1;
-  width: 100%;
+  width: 880px;
+  padding: 40px 0;
   overflow-y: hidden;
   overflow-x: overlay;
   ${scrollbars.MAIN};
