cadbdb30ba6f2c2ea1eb951a692aa730edefa6cd
TJ Jiang <thaddeusjiang@gmail.com>
2019-09-25 12:07:07 +0900

    refactor(task item): merge draggable props in one element

--- src/modules/project/form/components/MilestonesSection/components/TaskItem/index.js
+++ src/modules/project/form/components/MilestonesSection/components/TaskItem/index.js
@@ -27,99 +27,99 @@ type Props = {|
 
 function TaskItem({ task, isDragging, provided, onChange, onRemove }: Props) {
   return (
-    <div
-      className={TaskItemWrapperStyle(isDragging)}
-      ref={provided.innerRef}
-      {...provided.draggableProps}
-    >
-      <PartnerPermissionsWrapper data={task} key={task.id}>
-        {(permissions, isOwner) => (
-          <BooleanValue>
-            {({ value: isOpen, set: toggleTaskForm }) => (
-              <>
-                <div {...provided.dragHandleProps}>
-                  <TaskCard
-                    hideProjectInfo
-                    entity={{
-                      ...task.entity,
-                      ...getByPath('order', task),
-                      ...getByPath('orderItem', task),
-                      ...getByPath('batch', task),
-                      ...getByPath('product', task),
-                      ...getByPath('productProvider', task),
-                      ...getByPath('shipment', task),
-                    }}
-                    task={task}
-                    actions={[
-                      (permissions.includes(TASK_SET_MILESTONE) ||
-                        permissions.includes(PROJECT_UPDATE) ||
-                        permissions.includes(MILESTONE_UPDATE)) && (
-                        <CardAction
-                          icon="CLEAR"
-                          hoverColor="RED"
-                          onClick={() => onRemove(task.id, false)}
-                        />
-                      ),
-                      // Hidden on UI until we have create new task for project form
-                      permissions.includes(TASK_DELETE) && false && (
-                        <CardAction
-                          icon="REMOVE"
-                          hoverColor="RED"
-                          onClick={() => onRemove(task.id, true)}
-                        />
-                      ),
-                    ].filter(Boolean)}
-                    position={task.milestoneSort + 1}
-                    editable={checkEditableFromEntity(
-                      getByPath('entity.__typename', task),
-                      (checkPermission: string | Array<string>) => {
-                        if (Array.isArray(checkPermission)) {
-                          return intersection(permissions, checkPermission).length > 0;
-                        }
-                        return permissions.includes(checkPermission);
-                      }
-                    )}
-                    navigable={{ project: false }}
-                    saveOnBlur={value => onChange(task.id, value)}
-                    onClick={() => {
-                      // This is using for fixing a edge case when on blur doesn't fire on inline edit for task card
-                      if (document.activeElement) document.activeElement.blur();
-                      setTimeout(() => toggleTaskForm(true), 200);
-                    }}
-                  />
-                </div>
-                <SlideView
-                  isOpen={isOpen}
-                  onRequestClose={() => toggleTaskForm(false)}
-                  shouldConfirm={() => document.getElementById('task_form_save_button')}
-                >
-                  {isOpen && (
-                    <QueryFormPermissionContext.Provider
-                      value={{
-                        isOwner,
-                        permissions,
-                      }}
-                    >
-                      <TaskFormInSlide
-                        inParentEntityForm={false}
-                        isInProject
-                        groupIds={parseGroupIds(task)}
-                        entity={task.entity}
-                        task={{ ...task, sort: task.milestoneSort + 1 }}
-                        onSave={value => {
-                          onChange(task.id, value);
-                          toggleTaskForm(false);
-                        }}
+    <PartnerPermissionsWrapper data={task} key={task.id}>
+      {(permissions, isOwner) => (
+        <BooleanValue>
+          {({ value: isOpen, set: toggleTaskForm }) => (
+            <>
+              <div
+                className={TaskItemWrapperStyle(isDragging)}
+                ref={provided.innerRef}
+                {...provided.draggableProps}
+                {...provided.dragHandleProps}
+              >
+                <TaskCard
+                  hideProjectInfo
+                  entity={{
+                    ...task.entity,
+                    ...getByPath('order', task),
+                    ...getByPath('orderItem', task),
+                    ...getByPath('batch', task),
+                    ...getByPath('product', task),
+                    ...getByPath('productProvider', task),
+                    ...getByPath('shipment', task),
+                  }}
+                  task={task}
+                  actions={[
+                    (permissions.includes(TASK_SET_MILESTONE) ||
+                      permissions.includes(PROJECT_UPDATE) ||
+                      permissions.includes(MILESTONE_UPDATE)) && (
+                      <CardAction
+                        icon="CLEAR"
+                        hoverColor="RED"
+                        onClick={() => onRemove(task.id, false)}
                       />
-                    </QueryFormPermissionContext.Provider>
+                    ),
+                    // Hidden on UI until we have create new task for project form
+                    permissions.includes(TASK_DELETE) && false && (
+                      <CardAction
+                        icon="REMOVE"
+                        hoverColor="RED"
+                        onClick={() => onRemove(task.id, true)}
+                      />
+                    ),
+                  ].filter(Boolean)}
+                  position={task.milestoneSort + 1}
+                  editable={checkEditableFromEntity(
+                    getByPath('entity.__typename', task),
+                    (checkPermission: string | Array<string>) => {
+                      if (Array.isArray(checkPermission)) {
+                        return intersection(permissions, checkPermission).length > 0;
+                      }
+                      return permissions.includes(checkPermission);
+                    }
                   )}
-                </SlideView>
-              </>
-            )}
-          </BooleanValue>
-        )}
-      </PartnerPermissionsWrapper>
-    </div>
+                  navigable={{ project: false }}
+                  saveOnBlur={value => onChange(task.id, value)}
+                  onClick={() => {
+                    // This is using for fixing a edge case when on blur doesn't fire on inline edit for task card
+                    if (document.activeElement) document.activeElement.blur();
+                    setTimeout(() => toggleTaskForm(true), 200);
+                  }}
+                />
+              </div>
+
+              <SlideView
+                isOpen={isOpen}
+                onRequestClose={() => toggleTaskForm(false)}
+                shouldConfirm={() => document.getElementById('task_form_save_button')}
+              >
+                {isOpen && (
+                  <QueryFormPermissionContext.Provider
+                    value={{
+                      isOwner,
+                      permissions,
+                    }}
+                  >
+                    <TaskFormInSlide
+                      inParentEntityForm={false}
+                      isInProject
+                      groupIds={parseGroupIds(task)}
+                      entity={task.entity}
+                      task={{ ...task, sort: task.milestoneSort + 1 }}
+                      onSave={value => {
+                        onChange(task.id, value);
+                        toggleTaskForm(false);
+                      }}
+                    />
+                  </QueryFormPermissionContext.Provider>
+                )}
+              </SlideView>
+            </>
+          )}
+        </BooleanValue>
+      )}
+    </PartnerPermissionsWrapper>
   );
 }
 
