04042c5ac2d544638aa72172dd4fc4eca43599a6
Kaka Huynh <dunghd.it@gmail.com>
2019-09-30 19:31:53 +0900

    feat(nrm): add i18n and check permission for add tags

--- src/modules/relationMapV2/components/AddTags/index.js
+++ src/modules/relationMapV2/components/AddTags/index.js
@@ -1,9 +1,17 @@
 // @flow
 import * as React from 'react';
 import { FormattedMessage } from 'react-intl';
+import { useAllHasPermission } from 'components/Context/Permissions';
 import { RelationMapContext } from 'modules/relationMapV2/components/OrderFocus/store';
+import { Entities } from 'modules/relationMapV2/store';
 import { useLazyQuery, useMutation } from '@apollo/react-hooks';
 import { ORDER, ORDER_ITEM, BATCH, CONTAINER, SHIPMENT } from 'modules/relationMapV2/constants';
+import { TAG_LIST } from 'modules/permission/constants/tag';
+import { ORDER_UPDATE, ORDER_SET_TAGS } from 'modules/permission/constants/order';
+import { BATCH_UPDATE, BATCH_SET_TAGS } from 'modules/permission/constants/batch';
+import { ORDER_ITEMS_UPDATE, ORDER_ITEMS_SET_TAGS } from 'modules/permission/constants/orderItem';
+import { CONTAINER_UPDATE, CONTAINER_SET_TAGS } from 'modules/permission/constants/container';
+import { SHIPMENT_UPDATE, SHIPMENT_SET_TAGS } from 'modules/permission/constants/shipment';
 import { SaveButton, CancelButton } from 'components/Buttons';
 import Dialog from 'components/Dialog';
 import Icon from 'components/Icon';
@@ -24,6 +32,7 @@ type Props = {|
 |};
 
 export default function AddTags({ onSuccess }: Props) {
+  const { mapping } = Entities.useContainer();
   const [tags, setTags] = React.useState([]);
   const { dispatch, state } = React.useContext(RelationMapContext);
   const [loadOrders, ordersResult] = useLazyQuery(ordersByIDsQuery);
@@ -43,15 +52,64 @@ export default function AddTags({ onSuccess }: Props) {
   }, [isOpen]);
 
   const orderIds = targetedIds(targets, ORDER);
+  const hasOrderPermissions = useAllHasPermission(
+    orderIds.map(id => mapping.entities?.orders?.[id]?.ownedBy).filter(Boolean)
+  );
   const totalOrders = orderIds.length;
   const itemIds = targetedIds(targets, ORDER_ITEM);
+  const hasItemPermissions = useAllHasPermission(
+    itemIds.map(id => mapping.entities?.orderItems?.[id]?.ownedBy).filter(Boolean)
+  );
   const totalOrderItems = itemIds.length;
   const batchIds = targetedIds(targets, BATCH);
+  const hasBatchPermissions = useAllHasPermission(
+    batchIds.map(id => mapping.entities?.batches?.[id]?.ownedBy).filter(Boolean)
+  );
   const totalBatches = batchIds.length;
   const containerIds = targetedIds(targets, CONTAINER);
+  const hasContainerPermissions = useAllHasPermission(
+    containerIds.map(id => mapping.entities?.containers?.[id]?.ownedBy).filter(Boolean)
+  );
   const totalContainers = containerIds.length;
   const shipmentIds = targetedIds(targets, SHIPMENT);
+  const hasShipmentPermissions = useAllHasPermission(
+    shipmentIds.map(id => mapping.entities?.shipments?.[id]?.ownedBy).filter(Boolean)
+  );
   const totalShipments = shipmentIds.length;
+
+  const hasPermission = (permission: string | Array<string>) => {
+    switch (source) {
+      case ORDER:
+        return hasOrderPermissions(permission);
+      case ORDER_ITEM:
+        return hasItemPermissions(permission);
+      case BATCH:
+        return hasBatchPermissions(permission);
+      case CONTAINER:
+        return hasContainerPermissions(permission);
+      case SHIPMENT:
+        return hasShipmentPermissions(permission);
+      default:
+        return false;
+    }
+  };
+
+  const allowToUpdate = () => {
+    switch (source) {
+      case ORDER:
+        return hasPermission([ORDER_UPDATE, ORDER_SET_TAGS]);
+      case ORDER_ITEM:
+        return hasPermission([ORDER_ITEMS_UPDATE, ORDER_ITEMS_SET_TAGS]);
+      case BATCH:
+        return hasPermission([BATCH_UPDATE, BATCH_SET_TAGS]);
+      case CONTAINER:
+        return hasPermission([CONTAINER_UPDATE, CONTAINER_SET_TAGS]);
+      case SHIPMENT:
+        return hasPermission([SHIPMENT_UPDATE, SHIPMENT_SET_TAGS]);
+      default:
+        return false;
+    }
+  };
 
   const onCancel = React.useCallback(() => {
     dispatch({
@@ -202,37 +260,107 @@ export default function AddTags({ onSuccess }: Props) {
     }
   };
 
+  const noPermission = !allowToUpdate();
+
+  if (noPermission) {
+    return (
+      <Dialog isOpen={isOpen} width="450px">
+        <div className={DialogStyle}>
+          <h3 className={ConfirmMessageStyle}>
+            <FormattedMessage
+              id="modules.RelationMap.tags.noPermission"
+              defaultMessage="At least one {source} {entity} selected does not allow you to add tags.Please reselect and try again."
+              values={{
+                source,
+                entity: <Icon icon={source.toUpperCase()} />,
+              }}
+            />
+          </h3>
+          <div className={ButtonsStyle}>
+            <CancelButton disabled={Boolean(isProcessing)} onClick={onCancel} />
+          </div>
+        </div>
+      </Dialog>
+    );
+  }
+
   return (
     <Dialog isOpen={isOpen} width="450px">
       <div className={DialogStyle}>
         <h3 className={ConfirmMessageStyle}>
-          {isProcessing ? 'Adding Tags ' : 'Select tags to add to the'}
-          {totalOrders > 0 && source === ORDER && (
-            <>
-              {totalOrders} <Icon icon="ORDER" />
-            </>
+          {isProcessing ? (
+            <FormattedMessage
+              id="modules.RelationMap.tags.add"
+              defaultMessage="Adding tags to add to the {source} that you have selected."
+              values={{
+                source: (
+                  <>
+                    {totalOrders > 0 && source === ORDER && (
+                      <>
+                        {totalOrders} <Icon icon="ORDER" />
+                      </>
+                    )}
+                    {totalOrderItems > 0 && source === ORDER_ITEM && (
+                      <>
+                        {totalOrderItems} <Icon icon="ORDER_ITEM" />
+                      </>
+                    )}
+                    {totalBatches > 0 && source === BATCH && (
+                      <>
+                        {totalBatches} <Icon icon="BATCH" />
+                      </>
+                    )}
+                    {totalContainers > 0 && source === CONTAINER && (
+                      <>
+                        {totalContainers} <Icon icon="CONTAINER" />
+                      </>
+                    )}
+                    {totalShipments > 0 && source === SHIPMENT && (
+                      <>
+                        {totalShipments} <Icon icon="SHIPMENT" />
+                      </>
+                    )}
+                  </>
+                ),
+              }}
+            />
+          ) : (
+            <FormattedMessage
+              id="modules.RelationMap.tags.guideline"
+              defaultMessage="Select tags to add to the {source} that you have selected."
+              values={{
+                source: (
+                  <>
+                    {totalOrders > 0 && source === ORDER && (
+                      <>
+                        {totalOrders} <Icon icon="ORDER" />
+                      </>
+                    )}
+                    {totalOrderItems > 0 && source === ORDER_ITEM && (
+                      <>
+                        {totalOrderItems} <Icon icon="ORDER_ITEM" />
+                      </>
+                    )}
+                    {totalBatches > 0 && source === BATCH && (
+                      <>
+                        {totalBatches} <Icon icon="BATCH" />
+                      </>
+                    )}
+                    {totalContainers > 0 && source === CONTAINER && (
+                      <>
+                        {totalContainers} <Icon icon="CONTAINER" />
+                      </>
+                    )}
+                    {totalShipments > 0 && source === SHIPMENT && (
+                      <>
+                        {totalShipments} <Icon icon="SHIPMENT" />
+                      </>
+                    )}
+                  </>
+                ),
+              }}
+            />
           )}
-          {totalOrderItems > 0 && source === ORDER_ITEM && (
-            <>
-              {totalOrderItems} <Icon icon="ORDER_ITEM" />
-            </>
-          )}
-          {totalBatches > 0 && source === BATCH && (
-            <>
-              {totalBatches} <Icon icon="BATCH" />
-            </>
-          )}
-          {totalContainers > 0 && source === CONTAINER && (
-            <>
-              {totalContainers} <Icon icon="CONTAINER" />
-            </>
-          )}
-          {totalShipments > 0 && source === SHIPMENT && (
-            <>
-              {totalShipments} <Icon icon="SHIPMENT" />
-            </>
-          )}
-          that you have selected
         </h3>
         <FieldItem
           vertical
@@ -249,13 +377,12 @@ export default function AddTags({ onSuccess }: Props) {
               values={tags}
               onChange={setTags}
               onClickRemove={tag => setTags(tags.filter(({ id }) => tag.id !== id))}
-              // TODO: check permission cross entities
               editable={
                 isProcessing
                   ? { set: false, remove: false }
                   : {
-                      set: true,
-                      remove: true,
+                      set: hasPermission(TAG_LIST) && allowToUpdate(),
+                      remove: allowToUpdate(),
                     }
               }
             />
