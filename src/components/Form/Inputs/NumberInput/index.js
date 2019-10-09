5258fddb41eaf0ef146e95f29509381f869aea0b
TJ Jiang <thaddeusjiang@gmail.com>
2019-10-09 13:35:56 +0900

    fix(number input): clear leading 0 on blur

--- src/components/Form/Inputs/NumberInput/index.js
+++ src/components/Form/Inputs/NumberInput/index.js
@@ -52,6 +52,7 @@ class NumberInput extends React.Component<Props> {
     const { onBlur, nullable } = this.props;
 
     if (onBlur) {
+      const { inputRef } = this.props;
       if (!nullable && evt.target.value === '') {
         onBlur({
           ...evt,
@@ -60,14 +61,22 @@ class NumberInput extends React.Component<Props> {
             value: 0,
           },
         });
+        if (inputRef) {
+          inputRef.current.value = 0;
+        }
       } else {
+        const value = toFloatNullable(evt.target.value);
         onBlur({
           ...evt,
           target: {
             ...evt.target,
-            value: toFloatNullable(evt.target.value),
+            value,
           },
         });
+
+        if (inputRef) {
+          inputRef.current.value = value;
+        }
       }
     }
   };
