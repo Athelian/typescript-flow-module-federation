fe2f081273a13a27896da42ca0e26ba81026f09c
maxime.perrimond <maxime.perrimond@zenport.io>
2019-10-11 14:40:20 +0900

    feat(shipment): setup shipment filter

--- src/components/NavBar/components/Sort/configs.js
+++ src/components/NavBar/components/Sort/configs.js
@@ -1,5 +1,6 @@
 // @flow
 import orderMessages from 'modules/order/messages';
+import { shipmentSortMessages as shipmentMessages } from 'modules/shipment/messages';
 import warehouseMessages from 'modules/warehouse/messages';
 import partnerMessages from 'modules/partner/messages';
 import type { SortConfig } from './index';
@@ -14,6 +15,24 @@ export const OrderSortConfig: Array<SortConfig> = [
   { message: orderMessages.currency, field: 'currency' },
   { message: orderMessages.incoterm, field: 'incoterm' },
   { message: orderMessages.deliveryPlace, field: 'deliveryPlace' },
+];
+
+export const ShipmentSortConfig: Array<SortConfig> = [
+  { message: shipmentMessages.updatedAt, field: 'updatedAt' },
+  { message: shipmentMessages.createdAt, field: 'createdAt' },
+  { message: shipmentMessages.shipmentId, field: 'no' },
+  { message: shipmentMessages.blNo, field: 'blNo' },
+  { message: shipmentMessages.vesselName, field: 'vesselName' },
+  { message: shipmentMessages.cargoReady, field: 'cargoReady' },
+  { message: shipmentMessages.loadPortDeparture, field: 'loadPortDeparture' },
+  { message: shipmentMessages.firstTransitPortArrival, field: 'firstTransitPortArrival' },
+  { message: shipmentMessages.firstTransitPortDeparture, field: 'firstTransitPortDeparture' },
+  { message: shipmentMessages.secondTransitPortArrival, field: 'secondTransitPortArrival' },
+  { message: shipmentMessages.secondTransitPortDeparture, field: 'secondTransitPortDeparture' },
+  { message: shipmentMessages.dischargePortArrival, field: 'dischargePortArrival' },
+  { message: shipmentMessages.customClearance, field: 'customClearance' },
+  { message: shipmentMessages.warehouseArrival, field: 'warehouseArrival' },
+  { message: shipmentMessages.deliveryReady, field: 'deliveryReady' },
 ];
 
 export const WarehouseSortConfig: Array<SortConfig> = [
