b478d4f58002bf445f978f1fbf1e7ffef765c2c6
maxime.perrimond <maxime.perrimond@zenport.io>
2019-10-11 17:58:29 +0900

    feat(warehouse): setup filter

--- src/modules/warehouse/index.list.js
+++ src/modules/warehouse/index.list.js
@@ -1,55 +1,49 @@
 // @flow
 import * as React from 'react';
 import { Link } from '@reach/router';
-import { injectIntl } from 'react-intl';
-import type { IntlShape } from 'react-intl';
 import { WAREHOUSE_CREATE } from 'modules/permission/constants/warehouse';
-import usePermission from 'hooks/usePermission';
-import FilterToolBar from 'components/common/FilterToolBar';
-import useFilter from 'hooks/useFilter';
 import { Content } from 'components/Layout';
-import { NavBar } from 'components/NavBar';
+import {
+  EntityIcon,
+  Filter,
+  NavBar,
+  WarehouseFilterConfig,
+  WarehouseSortConfig,
+  Search,
+  Sort,
+} from 'components/NavBar';
 import { NewButton } from 'components/Buttons';
+import { useViewerHasPermissions } from 'contexts/Permissions';
+import useFilterSort from 'hooks/useFilterSort';
 import WarehouseList from './list';
-import { warehousesDefaultQueryVariables } from './constants';
-import messages from './messages';
 
-type Props = {
-  intl: IntlShape,
-};
-
-const WarehouseModule = (props: Props) => {
-  const { intl } = props;
-
-  const sortFields = [
-    { title: intl.formatMessage(messages.updatedAt), value: 'updatedAt' },
-    { title: intl.formatMessage(messages.createdAt), value: 'createdAt' },
-  ];
-  const { filterAndSort, queryVariables, onChangeFilter } = useFilter(
-    warehousesDefaultQueryVariables,
-    'warehousesFilter'
+const WarehouseListModule = () => {
+  const { query, filterBy, sortBy, setQuery, setFilterBy, setSortBy } = useFilterSort(
+    { query: '', archived: false },
+    { updatedAt: 'DESCENDING' },
+    'warehouse_cards'
   );
-  const { hasPermission } = usePermission();
-  const allowCreate = hasPermission(WAREHOUSE_CREATE);
+
+  const hasPermissions = useViewerHasPermissions();
+
   return (
     <Content>
       <NavBar>
-        <FilterToolBar
-          icon="WAREHOUSE"
-          sortFields={sortFields}
-          filtersAndSort={filterAndSort}
-          onChange={onChangeFilter}
-          canSearch
-        />
-        {allowCreate && (
-          <Link to="new">
+        <EntityIcon icon="WAREHOUSE" color="WAREHOUSE" subIcon="CARDS" />
+
+        <Filter config={WarehouseFilterConfig} filterBy={filterBy} onChange={setFilterBy} />
+        <Search query={query} onChange={setQuery} />
+        <Sort config={WarehouseSortConfig} sortBy={sortBy} onChange={setSortBy} />
+
+        {hasPermissions(WAREHOUSE_CREATE) && (
+          <Link to="/warehouse/new">
             <NewButton />
           </Link>
         )}
       </NavBar>
-      <WarehouseList {...queryVariables} />
+      <WarehouseList filterBy={{ query, ...filterBy }} sortBy={sortBy} page={1} perPage={10} />
     </Content>
   );
 };
 
-export default injectIntl(WarehouseModule);
+export default WarehouseListModule;
