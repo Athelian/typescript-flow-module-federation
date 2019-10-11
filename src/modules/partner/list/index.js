bcefd9153b4538aff37f5e073fced3d96ff3dc94
maxime.perrimond <maxime.perrimond@zenport.io>
2019-10-11 17:00:14 +0900

    feat(partner): setup filter

--- src/modules/partner/list/index.js
+++ src/modules/partner/list/index.js
@@ -3,14 +3,13 @@ import * as React from 'react';
 import { Query } from 'react-apollo';
 import { getByPathWithDefault } from 'utils/fp';
 import loadMore from 'utils/loadMore';
+import type { FilterBy, SortBy } from 'types';
 import PartnerGridView from './PartnerGridView';
 import { partnerListQuery } from './query';
 
 type Props = {
-  filterBy: {},
-  sortBy: {
-    [field: string]: string,
-  },
+  filterBy: FilterBy,
+  sortBy: SortBy,
   perPage: number,
 };
 
