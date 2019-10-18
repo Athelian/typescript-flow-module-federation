34a3c57a488e33316d299432596c2d6532511080
Kevin Nguyen <kcvin94@gmail.com>
2019-10-18 12:34:09 +0900

    feat(gtv): refactor textarea input, fixing alot of issues

--- src/components/Sheet/CellRenderer/Cell/CellInput/Inputs/TextAreaInput/TextAreaInputDialog.js
+++ src/components/Sheet/CellRenderer/Cell/CellInput/Inputs/TextAreaInput/TextAreaInputDialog.js
@@ -1,37 +1,59 @@
 // @flow
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 import { useIntl } from 'react-intl';
+import usePrevious from 'hooks/usePrevious';
+import Dialog from 'components/Dialog';
 import { TextAreaInputDialogWrapperStyle, TextAreaInputStyle } from './style';
 
 type Props = {
-  inputRef: Object,
   value: string,
+  onChange: string => void,
+  focus: boolean,
+  onBlur: () => void,
 };
 
-const TextAreaInputDialog = ({ inputRef, value }: Props) => {
-  const [content, setContent] = React.useState(value || '');
+const TextAreaInputDialog = ({ value, onChange, focus, onBlur }: Props) => {
   const intl = useIntl();
+  const inputRef = useRef(null);
+  const prevFocus = usePrevious(focus);
+
+  useEffect(() => {
+    if (!prevFocus && focus) {
+      setTimeout(() => {
+        if (inputRef && inputRef.current) {
+          inputRef.current.select();
+        }
+      }, 200);
+    }
+  }, [focus, prevFocus]);
 
   return (
-    <div className={TextAreaInputDialogWrapperStyle}>
-      <textarea
-        ref={inputRef}
-        className={TextAreaInputStyle}
-        value={content}
-        placeholder={intl.formatMessage({
-          id: 'components.sheet.textarea.placeholder',
-          defaultMessage: 'Please enter a value',
-        })}
-        spellCheck={false}
-        onChange={e => setContent(e.target.value)}
-        onKeyDown={e => {
-          if (e.key === 'Tab') {
-            e.preventDefault();
-          }
-          e.stopPropagation();
-        }}
-      />
-    </div>
+    <Dialog
+      isOpen={focus}
+      onRequestClose={() => {
+        onBlur();
+      }}
+    >
+      <div className={TextAreaInputDialogWrapperStyle}>
+        <textarea
+          ref={inputRef}
+          className={TextAreaInputStyle}
+          value={value}
+          placeholder={intl.formatMessage({
+            id: 'components.sheet.textarea.placeholder',
+            defaultMessage: 'Please enter a value',
+          })}
+          spellCheck={false}
+          onChange={event => onChange(event.target.value)}
+          onKeyDown={event => {
+            if (event.key === 'Tab') {
+              event.preventDefault();
+              event.stopPropagation();
+            }
+          }}
+        />
+      </div>
+    </Dialog>
   );
 };
 
--- src/components/Sheet/CellRenderer/Cell/CellInput/Inputs/TextAreaInput/index.js
+++ src/components/Sheet/CellRenderer/Cell/CellInput/Inputs/TextAreaInput/index.js
@@ -1,82 +1,44 @@
 // @flow
-import React, { useRef, useEffect } from 'react';
+import React from 'react';
 import { useIntl } from 'react-intl';
-import Dialog from 'components/Dialog';
-import type { InputProps } from '../../types';
 import InputWrapper from '../InputWrapper';
 import TextAreaInputDialog from './TextAreaInputDialog';
 
-const TextInput = ({
-  value,
-  focus,
-  onChange,
-  onFocus,
-  onBlur,
-  readonly,
-  onKeyDown,
-}: InputProps<string>) => {
-  const [isOpen, setIsOpen] = React.useState(false);
+type Props = {
+  value: string,
+  focus: boolean,
+  readonly: boolean,
+  onChange: string => void,
+  onFocus: () => void,
+  onBlur: () => void,
+};
+
+const TextInput = ({ value, focus, onChange, onFocus, onBlur, readonly }: Props) => {
   const intl = useIntl();
-  const inputRef = useRef(null);
-  useEffect(() => {
-    if (isOpen) {
-      setTimeout(() => {
-        if (inputRef && inputRef.current) {
-          inputRef.current.select();
-        }
-      }, 200);
-    }
-  }, [isOpen]);
 
   return (
     <>
       <InputWrapper focus={focus}>
         {({ ref }) => (
-          <>
-            <input
-              ref={ref}
-              readOnly
-              spellCheck={false}
-              value={value || ''}
-              onClick={() => {
-                if (!readonly) {
-                  onFocus();
-                  setIsOpen(true);
-                }
-              }}
-              onKeyDown={e => {
-                if (!readonly && e.key === 'Enter') {
-                  onFocus();
-                  setIsOpen(true);
-                }
-                e.preventDefault();
-                e.stopPropagation();
-              }}
-              placeholder={intl.formatMessage({
-                id: 'components.sheet.textarea.placeholder',
-                defaultMessage: 'Please enter a value',
-              })}
-              onFocus={onFocus}
-            />
-            <Dialog
-              isOpen={isOpen}
-              onRequestClose={() => {
-                onChange(inputRef?.current?.value ?? '');
-                onBlur();
-                setIsOpen(false);
-              }}
-            >
-              {isOpen && (
-                <TextAreaInputDialog
-                  inputRef={inputRef}
-                  value={value || ''}
-                  onKeyDown={onKeyDown}
-                />
-              )}
-            </Dialog>
-          </>
+          <input
+            ref={ref}
+            readOnly
+            spellCheck={false}
+            value={value || ''}
+            onClick={() => {
+              if (!readonly) {
+                onFocus();
+              }
+            }}
+            placeholder={intl.formatMessage({
+              id: 'components.sheet.textarea.placeholder',
+              defaultMessage: 'Please enter a value',
+            })}
+          />
         )}
       </InputWrapper>
+
+      <TextAreaInputDialog value={value || ''} onChange={onChange} focus={focus} onBlur={onBlur} />
     </>
   );
 };
