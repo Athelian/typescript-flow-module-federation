4478619d8d62197e51d2f021b4cf3ca80508d49c
Kevin Nguyen <kcvin94@gmail.com>
2019-10-21 17:52:23 +0900

    feat(gtv): style touchup static metric input

--- src/components/Sheet/CellRenderer/Cell/CellDisplay/Displays/DisplayWrapper/style.js
+++ src/components/Sheet/CellRenderer/Cell/CellDisplay/Displays/DisplayWrapper/style.js
@@ -2,12 +2,12 @@
 import { css } from 'react-emotion';
 import { colors, fontSizes, layout, presets } from 'styles/common';
 
-export const DisplayWrapperStyle: string = css`
+export const DisplayWrapperStyle = (width: string): string => css`
   ${layout.LAYOUT};
   ${layout.HORIZONTAL};
   ${layout.CENTER};
   padding: 5px;
-  width: 100%;
+  width: ${width};
   & > span {
     ${fontSizes.MAIN};
     ${presets.ELLIPSIS};
--- src/components/Sheet/CellRenderer/Cell/CellInput/Inputs/StaticMetricValueInput/index.js
+++ src/components/Sheet/CellRenderer/Cell/CellInput/Inputs/StaticMetricValueInput/index.js
@@ -37,7 +37,7 @@ const StaticMetricValueInput = ({
             onBlur={onBlur}
             onKeyDown={onKeyDown}
           />
-          <DisplayWrapper>
+          <DisplayWrapper width="min-content">
             <span>{metric}</span>
           </DisplayWrapper>
         </>
