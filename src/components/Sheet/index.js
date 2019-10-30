f0622f2dd27211e103e8f989253400f87ae6d61d
maxime.perrimond <maxime.perrimond@zenport.io>
2019-10-30 14:43:11 +0900

    feat(sheet): preserve filter and columns

--- src/components/Sheet/index.js
+++ src/components/Sheet/index.js
@@ -7,10 +7,12 @@ import {
 } from './SheetState/transformer';
 import ColumnsConfig from './ColumnsConfig';
 import Sheet from './Sheet';
+import useSheet from './useSheet';
 
 export type { ColumnConfig, ColumnSort };
 export {
   Sheet,
+  useSheet,
   ColumnsConfig,
   transformValueField,
   transformReadonlyField,
