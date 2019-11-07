0306b1b04c59085d2d585c8a9ec9d9f3a919ab05
Kaka Huynh <dunghd.it@gmail.com>
2019-11-07 19:45:33 +0900

    feat(gtv): add batch column

--- src/modules/batch/sheet/transformer.js
+++ src/modules/batch/sheet/transformer.js
@@ -1,13 +1,285 @@
 // @flow
 import type { Batch } from 'generated/graphql';
 import type { CellValue } from 'components/Sheet/SheetState/types';
+import {
+  transformComputedField,
+  transformReadonlyField,
+  transformValueField,
+} from 'components/Sheet';
+import {
+  BATCH_SET_DELIVERY_DATE,
+  BATCH_SET_DESIRED_DATE,
+  BATCH_SET_EXPIRY,
+  BATCH_SET_MEMO,
+  BATCH_SET_NO,
+  BATCH_SET_PACKAGE_CAPACITY,
+  BATCH_SET_PACKAGE_NAME,
+  BATCH_SET_PACKAGE_QUANTITY,
+  BATCH_SET_PACKAGE_SIZE,
+  BATCH_SET_PACKAGE_VOLUME,
+  BATCH_SET_PACKAGE_WEIGHT,
+  BATCH_SET_PRODUCTION_DATE,
+  BATCH_SET_QUANTITY,
+  BATCH_SET_QUANTITY_ADJUSTMENTS,
+  BATCH_SET_TAGS,
+  BATCH_SET_TASKS,
+  BATCH_UPDATE,
+} from 'modules/permission/constants/batch';
+import { calculateVolume, getBatchLatestQuantity } from 'utils/batch';
+import { defaultVolumeMetric } from 'utils/metric';
+
+function transformBatch(basePath: string, batch: Batch): Array<CellValue> {
+  return [
+    {
+      columnKey: 'batch.created',
+      type: 'date_user',
+      ...transformComputedField(basePath, batch, 'created', currentBatch => {
+        return currentBatch
+          ? {
+              at: new Date(currentBatch.createdAt),
+              by: currentBatch.createdBy,
+            }
+          : null;
+      }),
+    },
+    {
+      columnKey: 'batch.createdBy',
+      type: 'text',
+      ...transformReadonlyField(basePath, batch, 'createdBy', batch?.createdBy ?? null),
+    },
+    {
+      columnKey: 'batch.createdAt',
+      type: 'text',
+      ...transformReadonlyField(basePath, batch, 'createdAt', batch?.createdAt ?? null),
+    },
+    {
+      columnKey: 'batch.updated',
+      type: 'date_user',
+      ...transformComputedField(basePath, batch, 'updated', currentBatch => {
+        return currentBatch
+          ? {
+              at: new Date(currentBatch.updatedAt),
+              by: currentBatch.updatedBy,
+            }
+          : null;
+      }),
+    },
+    {
+      columnKey: 'batch.updatedBy',
+      type: 'text',
+      ...transformReadonlyField(basePath, batch, 'updatedBy', batch?.updatedBy ?? null),
+    },
+    {
+      columnKey: 'batch.updatedAt',
+      type: 'text',
+      ...transformReadonlyField(basePath, batch, 'updatedAt', batch?.updatedAt ?? null),
+    },
+    {
+      columnKey: 'batch.archived',
+      type: 'status',
+      ...transformComputedField(basePath, batch, 'archived', currentBatch => {
+        if (currentBatch?.shipment) {
+          return currentBatch?.orderItem?.order?.archived && currentBatch?.shipment?.archived;
+        }
+        return currentBatch?.orderItem?.order?.archived;
+      }),
+    },
+    {
+      columnKey: 'batch.no',
+      type: 'text',
+      ...transformValueField(
+        basePath,
+        batch,
+        'no',
+        hasPermission => hasPermission(BATCH_UPDATE) || hasPermission(BATCH_SET_NO)
+      ),
+    },
+    {
+      columnKey: 'batch.deliveredAt',
+      type: 'date',
+      ...transformValueField(
+        basePath,
+        batch,
+        'deliveredAt',
+        hasPermission => hasPermission(BATCH_UPDATE) || hasPermission(BATCH_SET_DELIVERY_DATE)
+      ),
+    },
+    {
+      columnKey: 'batch.desiredAt',
+      type: 'date',
+      ...transformValueField(
+        basePath,
+        batch,
+        'desiredAt',
+        hasPermission => hasPermission(BATCH_UPDATE) || hasPermission(BATCH_SET_DESIRED_DATE)
+      ),
+    },
+    {
+      columnKey: 'batch.expiredAt',
+      type: 'date',
+      ...transformValueField(
+        basePath,
+        batch,
+        'expiredAt',
+        hasPermission => hasPermission(BATCH_UPDATE) || hasPermission(BATCH_SET_EXPIRY)
+      ),
+    },
+    {
+      columnKey: 'batch.producedAt',
+      type: 'date',
+      ...transformValueField(
+        basePath,
+        batch,
+        'producedAt',
+        hasPermission => hasPermission(BATCH_UPDATE) || hasPermission(BATCH_SET_PRODUCTION_DATE)
+      ),
+    },
+    {
+      columnKey: 'batch.tags',
+      type: 'batch_tags',
+      ...transformValueField(
+        basePath,
+        batch,
+        'tags',
+        hasPermission => hasPermission(BATCH_UPDATE) || hasPermission(BATCH_SET_TAGS)
+      ),
+    },
+    {
+      columnKey: 'batch.memo',
+      type: 'textarea',
+      ...transformValueField(
+        basePath,
+        batch,
+        'memo',
+        hasPermission => hasPermission(BATCH_UPDATE) || hasPermission(BATCH_SET_MEMO)
+      ),
+    },
+    {
+      columnKey: 'batch.latestQuantity',
+      type: 'number',
+      ...transformComputedField(basePath, batch, 'latestQuantity', currentBatch => {
+        return getBatchLatestQuantity(currentBatch);
+      }),
+    },
+    {
+      columnKey: 'batch.quantity',
+      type: 'number',
+      ...transformValueField(
+        basePath,
+        batch,
+        'quantity',
+        hasPermission => hasPermission(BATCH_UPDATE) || hasPermission(BATCH_SET_QUANTITY)
+      ),
+    },
+    {
+      columnKey: 'batch.quantityRevisions',
+      type: 'quantity_revisions',
+      ...transformValueField(
+        basePath,
+        batch,
+        'batchQuantityRevisions',
+        hasPermission =>
+          hasPermission(BATCH_UPDATE) || hasPermission(BATCH_SET_QUANTITY_ADJUSTMENTS)
+      ),
+    },
+    {
+      columnKey: 'batch.packageName',
+      type: 'text',
+      ...transformValueField(
+        basePath,
+        batch,
+        'packageName',
+        hasPermission => hasPermission(BATCH_UPDATE) || hasPermission(BATCH_SET_PACKAGE_NAME)
+      ),
+    },
+    {
+      columnKey: 'batch.packageCapacity',
+      type: 'number',
+      ...transformValueField(
+        basePath,
+        batch,
+        'packageCapacity',
+        hasPermission => hasPermission(BATCH_UPDATE) || hasPermission(BATCH_SET_PACKAGE_CAPACITY)
+      ),
+    },
+    {
+      columnKey: 'batch.packageQuantity',
+      type: 'number_toggle',
+      computed: currentBatch => {
+        const latestQuantity = getBatchLatestQuantity(currentBatch);
+        const packageCapacity = currentBatch?.packageCapacity ?? 0;
+        return packageCapacity ? latestQuantity / packageCapacity : 0;
+      },
+      ...transformValueField(
+        basePath,
+        batch,
+        'packageQuantity',
+        hasPermission => hasPermission(BATCH_UPDATE) || hasPermission(BATCH_SET_PACKAGE_QUANTITY)
+      ),
+    },
+    {
+      columnKey: 'batch.packageGrossWeight',
+      type: 'mass',
+      ...transformValueField(
+        basePath,
+        batch,
+        'packageGrossWeight',
+        hasPermission => hasPermission(BATCH_UPDATE) || hasPermission(BATCH_SET_PACKAGE_WEIGHT)
+      ),
+    },
+    {
+      columnKey: 'batch.packageVolume',
+      type: 'volume_toggle',
+      computed: currentBatch => {
+        return calculateVolume(
+          currentBatch?.packageVolume?.value ?? { value: 0, metric: defaultVolumeMetric },
+          currentBatch?.packageSize
+        );
+      },
+      ...transformValueField(
+        basePath,
+        batch,
+        'packageVolume',
+        hasPermission => hasPermission(BATCH_UPDATE) || hasPermission(BATCH_SET_PACKAGE_VOLUME)
+      ),
+    },
+    {
+      columnKey: 'batch.packageSize',
+      type: 'size',
+      ...transformValueField(
+        basePath,
+        batch,
+        'packageSize',
+        hasPermission => hasPermission(BATCH_UPDATE) || hasPermission(BATCH_SET_PACKAGE_SIZE)
+      ),
+    },
+    {
+      columnKey: 'batch.todo',
+      type: 'batch_tasks',
+      computed: item => ({
+        entityId: batch?.id,
+        groupIds: [item.importer?.id, item.exporter?.id].filter(Boolean),
+      }),
+      ...transformValueField(
+        basePath,
+        batch,
+        'todo',
+        hasPermission => hasPermission(BATCH_UPDATE) || hasPermission(BATCH_SET_TASKS)
+      ),
+    },
+    {
+      columnKey: 'batch.logs',
+      type: 'batch_logs',
+      ...transformValueField(basePath, batch, 'id', () => true),
+    },
+  ].map(c => ({
+    ...c,
+    disabled: !batch,
+  }));
+}
 
 export default function transformer(index: number, batch: Batch): Array<Array<CellValue>> {
-  console.warn({
-    index,
-    batch,
-  });
-  const rows = [];
+  const batchCells = transformBatch(`${index}`, batch);
 
-  return rows;
+  return [batchCells];
 }
