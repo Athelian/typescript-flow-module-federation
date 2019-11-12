4d2d66a83b5b4181f68376dc60f9f98bd79254e3
Dung Duc Huynh (Kaka) <870029+jellydn@users.noreply.github.com>
2019-11-12 19:50:42 +0900

    fix(tableTemplate): only refresh on table setting

--- src/modules/tableTemplate/list/index.js
+++ src/modules/tableTemplate/list/index.js
@@ -1,6 +1,5 @@
 // @flow
 import * as React from 'react';
-import { Location } from '@reach/router';
 import { Query } from 'react-apollo';
 import emitter from 'utils/emitter';
 import { getByPathWithDefault } from 'utils/fp';
@@ -20,43 +19,40 @@ type Props = {
 };
 
 const TableTemplateList = ({ ...filtersAndSort }: Props) => {
+  const isTableTemplate = window.location.href.includes('templates');
   return (
-    <Location>
-      {({ location }) => (
-        <Query
-          query={tableTemplateQuery}
-          variables={{
-            page: 1,
-            ...filtersAndSort,
-          }}
-          fetchPolicy="network-only"
-        >
-          {({ loading, data, fetchMore, refetch, error }) => {
-            if (error) {
-              return error.message;
-            }
+    <Query
+      query={tableTemplateQuery}
+      variables={{
+        page: 1,
+        ...filtersAndSort,
+      }}
+      fetchPolicy="network-only"
+    >
+      {({ loading, data, fetchMore, refetch, error }) => {
+        if (error) {
+          return error.message;
+        }
 
-            const nextPage = getByPathWithDefault(1, `maskEdits.page`, data) + 1;
-            const totalPage = getByPathWithDefault(1, `maskEdits.totalPage`, data);
-            const hasMore = nextPage <= totalPage;
+        const nextPage = getByPathWithDefault(1, `maskEdits.page`, data) + 1;
+        const totalPage = getByPathWithDefault(1, `maskEdits.totalPage`, data);
+        const hasMore = nextPage <= totalPage;
 
-            emitter.removeAllListeners('REFETCH_TABLE_TEMPLATES');
-            emitter.addListener('REFETCH_TABLE_TEMPLATES', () => {
-              if (location.pathname.includes('templates')) refetch(tableTemplateQuery);
-            });
+        emitter.removeAllListeners('REFETCH_TABLE_TEMPLATES');
+        emitter.addListener('REFETCH_TABLE_TEMPLATES', () => {
+          if (isTableTemplate) refetch(tableTemplateQuery);
+        });
 
-            return (
-              <TableTemplateGridView
-                items={getByPathWithDefault([], 'maskEdits.nodes', data)}
-                onLoadMore={() => loadMore({ fetchMore, data }, filtersAndSort, 'maskEdits')}
-                hasMore={hasMore}
-                isLoading={loading}
-              />
-            );
-          }}
-        </Query>
-      )}
-    </Location>
+        return (
+          <TableTemplateGridView
+            items={getByPathWithDefault([], 'maskEdits.nodes', data)}
+            onLoadMore={() => loadMore({ fetchMore, data }, filtersAndSort, 'maskEdits')}
+            hasMore={hasMore}
+            isLoading={loading}
+          />
+        );
+      }}
+    </Query>
   );
 };
 
