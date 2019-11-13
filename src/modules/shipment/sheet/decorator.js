2f1294a58facd9a53105f3cf385948fc38add0d4
Kevin Nguyen <kcvin94@gmail.com>
2019-11-13 17:37:02 +0900

    fix(gtv): fix package volume

--- src/modules/shipment/sheet/decorator.js
+++ src/modules/shipment/sheet/decorator.js
@@ -1,4 +1,5 @@
 // @flow
+import { defaultVolumeMetric } from 'utils/metric';
 
 function decorateBatch(batch: Object): Object {
   return {
@@ -6,6 +7,13 @@ function decorateBatch(batch: Object): Object {
     packageQuantity: {
       value: batch.packageQuantity || 0,
       auto: batch.autoCalculatePackageQuantity || false,
+    },
+    packageVolume: {
+      value: {
+        value: batch.packageVolume?.value ?? 0,
+        metric: batch.packageVolume?.metric ?? defaultVolumeMetric,
+      },
+      auto: batch.autoCalculatePackageVolume ?? false,
     },
   };
 }
