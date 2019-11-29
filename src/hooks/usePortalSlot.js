dfa4c84de8584451ca4787958bdd0ac6bca4ce51
maxime.perrimond <maxime.perrimond@zenport.io>
2019-11-29 16:40:16 +0900

    fix(test): add portal root during test

--- src/hooks/usePortalSlot.js
+++ src/hooks/usePortalSlot.js
@@ -1,25 +1,30 @@
 // @flow
 import * as React from 'react';
 
-const root = document.getElementById('portal-root');
-if (!root) {
-  throw new Error('Element #portal-root is missing in document.');
-}
-
+const PORTAL_ROOT = 'portal-root';
 const PORTAL_NAME_ATTRIBUTE = 'data-portal-name';
 
+export function getPortalRoot(): HTMLElement {
+  const root = document.getElementById(PORTAL_ROOT);
+  if (!root) {
+    throw new Error('Element #portal-root is missing in document.');
+  }
+
+  return root;
+}
+
 export function hasInPortal(name: string): boolean {
-  return !!root.querySelector(`[${PORTAL_NAME_ATTRIBUTE}="${name}"]`);
+  return !!getPortalRoot().querySelector(`[${PORTAL_NAME_ATTRIBUTE}="${name}"]`);
 }
 
 export default function usePortalSlot(name: string = 'generic'): HTMLDivElement {
   const slotRef = React.useRef<HTMLDivElement | null>(null);
 
   React.useEffect(() => {
-    if (slotRef.current) root.appendChild(slotRef.current);
+    if (slotRef.current) getPortalRoot().appendChild(slotRef.current);
 
     return () => {
-      if (slotRef.current) root.removeChild(slotRef.current);
+      if (slotRef.current) getPortalRoot().removeChild(slotRef.current);
     };
   }, []);
 
