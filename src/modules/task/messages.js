5e6cea7656412554e5b65aeb5f3a4060460003ac
Kaka Huynh <dunghd.it@gmail.com>
2019-12-12 15:51:20 +0900

    fix(i18n): duplicate message id

--- src/modules/task/messages.js
+++ src/modules/task/messages.js
@@ -71,95 +71,95 @@ export default defineMessages({
     defaultMessage: 'Parent Type',
   },
   TaskStartDate: {
-    id: 'modules.Tasks.startDate',
+    id: 'modules.Tasks.TaskStartDate',
     defaultMessage: 'START DATE',
   },
   TaskDueDate: {
-    id: 'modules.Tasks.dueDate',
+    id: 'modules.Tasks.TaskDueDate',
     defaultMessage: 'DUE DATE',
   },
   ProjectDueDate: {
-    id: 'modules.projectTemplate.projectDueDate',
+    id: 'modules.Tasks.ProjectDueDate',
     defaultMessage: 'PROJECT DUE DATE',
   },
   MilestoneDueDate: {
-    id: 'modules.Tasks.milestoneDueDate',
+    id: 'modules.Tasks.MilestoneDueDate',
     defaultMessage: 'MILESTONE DUE DATE',
   },
   OrderIssuedAt: {
-    id: 'modules.Tasks.orderIssuedAt',
+    id: 'modules.Tasks.OrderIssuedAt',
     defaultMessage: 'PO DATE',
   },
   OrderDeliveryDate: {
-    id: 'modules.Tasks.orderDeliveryDate',
+    id: 'modules.Tasks.OrderDeliveryDate',
     defaultMessage: 'CONTRACT DELIVERY DATE',
   },
   OrderItemOrderIssuedAt: {
-    id: 'modules.Tasks.orderItemOrderIssuedAt',
+    id: 'modules.Tasks.OrderItemOrderIssuedAt',
     defaultMessage: 'PO DATE',
   },
   OrderItemOrderDeliveryDate: {
-    id: 'modules.Tasks.orderItemOrderDeliveryDate',
+    id: 'modules.Tasks.OrderItemOrderDeliveryDate',
     defaultMessage: 'CONTRACT DELIVERY DATE',
   },
   BatchDeliveredAt: {
-    id: 'modules.Tasks.batchDeliveredAt',
+    id: 'modules.Tasks.BatchDeliveredAt',
     defaultMessage: 'DELIVERY DATE',
   },
   BatchDesiredAt: {
-    id: 'modules.Tasks.batchDesiredAt',
+    id: 'modules.Tasks.BatchDesiredAt',
     defaultMessage: 'DESIRED DATE',
   },
   BatchProducedAt: {
-    id: 'modules.Tasks.batchProducedAt',
+    id: 'modules.Tasks.BatchProducedAt',
     defaultMessage: 'PRODUCTION DATE',
   },
   BatchExpiredAt: {
-    id: 'modules.Tasks.batchExpiredAt',
+    id: 'modules.Tasks.BatchExpiredAt',
     defaultMessage: 'EXPIRY DATE',
   },
   ShipmentBlDate: {
-    id: 'modules.Tasks.shipmentBlDate',
+    id: 'modules.Tasks.ShipmentBlDate',
     defaultMessage: 'B/L DATE',
   },
   ShipmentBookingDate: {
-    id: 'modules.Tasks.shipmentBookingDate',
+    id: 'modules.Tasks.ShipmentBookingDate',
     defaultMessage: 'BOOKING DATE',
   },
   ShipmentCargoReady: {
-    id: 'modules.Tasks.shipmentCargoReady',
+    id: 'modules.Tasks.ShipmentCargoReady',
     defaultMessage: 'CARGO READY',
   },
   ShipmentLoadPortDeparture: {
-    id: 'modules.Tasks.shipmentLoadPortDeparture',
+    id: 'modules.Tasks.ShipmentLoadPortDeparture',
     defaultMessage: 'LOAD PORT DEPARTURE',
   },
   ShipmentFirstTransitPortArrival: {
-    id: 'modules.Tasks.shipmentFirstTransitPortArrival',
+    id: 'modules.Tasks.ShipmentFirstTransitPortArrival',
     defaultMessage: 'FIRST TRANSIT PORT ARRIVAL',
   },
   ShipmentFirstTransitPortDeparture: {
-    id: 'modules.Tasks.shipmentFirstTransitPortDeparture',
+    id: 'modules.Tasks.ShipmentFirstTransitPortDeparture',
     defaultMessage: 'FIRST TRANSIT PORT DEPARTURE',
   },
   ShipmentSecondTransitPortArrival: {
-    id: 'modules.Tasks.shipmentSecondTransitPortArrival',
+    id: 'modules.Tasks.ShipmentSecondTransitPortArrival',
     defaultMessage: 'SECOND TRANSIT PORT ARRIVAL',
   },
   ShipmentSecondTransitPortDeparture: {
-    id: 'modules.Tasks.shipmentSecondTransitPortDeparture',
+    id: 'modules.Tasks.ShipmentSecondTransitPortDeparture',
     defaultMessage: 'SECOND TRANSIT PORT DEPARTURE',
   },
   ShipmentDischargePortArrival: {
-    id: 'modules.Tasks.shipmentDischargePortArrival',
+    id: 'modules.Tasks.ShipmentDischargePortArrival',
     defaultMessage: 'DISCHARGE PORT ARRIVAL',
   },
   ShipmentCustomClearance: {
-    id: 'modules.Tasks.shipmentCustomClearance',
+    id: 'modules.Tasks.ShipmentCustomClearance',
     defaultMessage: 'CUSTOMS CLEARANCE',
   },
   ShipmentWarehouseArrival: {
-    id: 'modules.Tasks.shipmentWarehouseArrival',
+    id: 'modules.Tasks.ShipmentWarehouseArrival',
     defaultMessage: 'WAREHOUSE ARRIVAL',
   },
   ShipmentDeliveryReady: {
