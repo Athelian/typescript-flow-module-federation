34b28b7bb4dea744f9ece1bc1a3acd1eec371a7b
Kevin Nguyen <kcvin94@gmail.com>
2019-12-23 16:57:18 +0900

    fix(flow): fix function

--- src/modules/dashboard/index.js
+++ src/modules/dashboard/index.js
@@ -5,7 +5,6 @@ import { ORDER_LIST } from 'modules/permission/constants/order';
 import { SHIPMENT_LIST } from 'modules/permission/constants/shipment';
 import { PRODUCT_LIST } from 'modules/permission/constants/product';
 import LoadingIcon from 'components/LoadingIcon';
-import useUser from 'hooks/useUser';
 import { useViewerHasPermissions, useViewerPermissions } from 'contexts/Permissions';
 
 const findRedirectUrlBaseOnPermission = (hasPermission: string => boolean) => {
@@ -18,14 +17,10 @@ const findRedirectUrlBaseOnPermission = (hasPermission: string => boolean) => {
 const DashBoard = () => {
   const { loading } = useViewerPermissions();
   const hasPermission = useViewerHasPermissions();
-  const { isUsingLegacyFeatures } = useUser();
   return loading ? (
     <LoadingIcon />
   ) : (
-    <Redirect
-      to={findRedirectUrlBaseOnPermission(hasPermission, isUsingLegacyFeatures())}
-      noThrow
-    />
+    <Redirect to={findRedirectUrlBaseOnPermission(hasPermission)} noThrow />
   );
 };
 
