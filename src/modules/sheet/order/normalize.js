662db98d368c29a18ddfdd3644651bbeb5e53fcb
Kevin Nguyen <kcvin94@gmail.com>
2020-02-18 13:22:51 +0900

    fix(gtv): style document input and fix mutation

--- src/modules/sheet/order/normalize.js
+++ src/modules/sheet/order/normalize.js
@@ -17,8 +17,18 @@ export default function normalizeSheetOrderInput(
     case 'files':
       return {
         files: newValue.map(
-          ({ __typename, entity: e, ownedBy, tags, path, uploading, progress, size, ...rest }) =>
-            rest
+          ({
+            __typename,
+            entity: e,
+            ownedBy,
+            tags,
+            path,
+            uploading,
+            progress,
+            size,
+            isNew,
+            ...rest
+          }) => rest
         ),
       };
     case 'tags':
