54848f49ef411bb237e2f58045571bc5f2cca9ef
hydRAnger <armyiljfe@gmail.com>
2020-03-03 22:57:45 +0900

    chore(gtv): style partner code

--- src/components/Sheet/CellRenderer/Cell/CellDisplay/Displays/PartnerDisplay/index.js
+++ src/components/Sheet/CellRenderer/Cell/CellDisplay/Displays/PartnerDisplay/index.js
@@ -5,17 +5,20 @@ import { Display } from 'components/Form';
 import CornerIcon from 'components/CornerIcon';
 import type { DisplayProps } from 'components/Sheet/CellRenderer/Cell/CellDisplay/types';
 import { CellDisplayWrapperStyle } from 'components/Sheet/CellRenderer/Cell/CellDisplay/Common/style';
-import { CardStyle } from './style';
+import { CardStyle, PartnerNameStyle, PartnerCodeStyle } from './style';
 
 const PartnerDisplay = ({ value }: DisplayProps<Object | null>) => {
-  const name = value?.partner?.name || value?.name || '';
+  const name = value?.partner?.name || value?.name || value?.organization?.name || '';
   const code = value?.partner?.code || '';
 
   return (
     <div className={CellDisplayWrapperStyle}>
       {value && (
-        <div className={CardStyle}>
-          <Display height="20px">{`${name} ${code}`}</Display>
+        <div className={CardStyle(!!name && !!code)}>
+          <Display height={name && code ? '40px' : '20px'}>
+            <div className={PartnerNameStyle}>{`Partner Name: ${name}`}</div>
+            <div className={PartnerCodeStyle}>{`Partner Code: ${code}`}</div>
+          </Display>
 
           <CornerIcon icon="PARTNER" color={colors.PARTNER} />
         </div>
--- src/components/Sheet/CellRenderer/Cell/CellDisplay/Displays/PartnerDisplay/style.js
+++ src/components/Sheet/CellRenderer/Cell/CellDisplay/Displays/PartnerDisplay/style.js
@@ -1,8 +1,8 @@
 // @flow
 import { css } from 'react-emotion';
-import { layout, colors, borderRadiuses } from 'styles/common';
+import { layout, colors, fontSizesWithHeights, presets, borderRadiuses } from 'styles/common';
 
-export const CardStyle: string = css`
+export const CardStyle = (isTwoLine: boolean): string => css`
   ${layout.HORIZONTAL};
   ${borderRadiuses.MAIN};
   background-color: ${colors.WHITE};
@@ -10,6 +10,23 @@ export const CardStyle: string = css`
   border-bottom-right-radius: 0;
   width: 100%;
   min-width: 40px;
-  height: 20px;
+  ${isTwoLine ? `height: 40px;` : `height: 40px;`}
   box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+`;
+
+export const PartnerNameStyle: string = css`
+  color: ${colors.BLACK};
+  font-weight: bold;
+  ${fontSizesWithHeights.MAIN};
+  ${presets.ELLIPSIS};
+  width: 100%;
+  padding: 0 10px;
+`;
+
+export const PartnerCodeStyle: string = css`
+  ${fontSizesWithHeights.SMALL};
+  color: ${colors.BLACK};
+  ${presets.ELLIPSIS};
+  padding: 0 10px;
+  width: 100%;
 `;
