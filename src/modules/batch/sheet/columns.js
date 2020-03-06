329864e091b0a58d5df25585654f4b1355ad27ac
hydRAnger <armyiljfe@gmail.com>
2020-03-06 15:23:05 +0900

    chore(container): fetch container custom fields

--- src/modules/batch/sheet/columns.js
+++ src/modules/batch/sheet/columns.js
@@ -16,6 +16,7 @@ export const FieldDefinitionEntityTypes = [
   'Shipment',
   'Product',
   'ProductProvider',
+  'Container',
 ];
 
 export const BatchSheetColumnGroups = [
--- src/modules/order/sheet/columns.js
+++ src/modules/order/sheet/columns.js
@@ -16,6 +16,7 @@ export const FieldDefinitionEntityTypes = [
   'OrderItem',
   'Batch',
   'Shipment',
+  'Container',
 ];
 
 export const OrderSheetColumnGroups = [
