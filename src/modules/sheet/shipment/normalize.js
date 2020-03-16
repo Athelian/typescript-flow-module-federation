bc958cea624160bb8e3f7fc7664a8a6c64e82cb6
Kevin Nguyen <kcvin94@gmail.com>
2020-03-16 14:45:46 +0900

    fix(gtv): fix document upload

--- src/modules/sheet/shipment/normalize.js
+++ src/modules/sheet/shipment/normalize.js
@@ -51,6 +51,7 @@ export default function normalizeSheetShipmentInput(
             progress,
             size,
             isNew,
+            createdAt,
             ...rest
           }) => rest
         ),
