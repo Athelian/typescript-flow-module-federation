c0195d8cf6b51160c5af56421714b045c92df405
Kevin Nguyen <kcvin94@gmail.com>
2020-09-29 12:44:43 +0900

    feat(milestone): handle tz milestone cards

--- src/components/Cards/MilestoneCard/index.js
+++ src/components/Cards/MilestoneCard/index.js
@@ -2,11 +2,12 @@
 import React from 'react';
 import { FormattedMessage } from 'react-intl';
 import { Display, FieldItem, Label } from 'components/Form';
-import FormattedDate from 'components/FormattedDate';
+import FormattedDateTZ from 'components/FormattedDateTZ';
 import FormattedNumber from 'components/FormattedNumber';
 import TaskStatusChart from 'components/TaskStatusChart';
 import Icon from 'components/Icon';
 import withForbiddenCard from 'hoc/withForbiddenCard';
+import useUser from 'hooks/useUser';
 import BaseCard from '../BaseCard';
 import {
   MilestoneCardStyle,
@@ -17,7 +18,7 @@ import {
   MilestoneStatusIconStyle,
 } from './style';
 
-type Props = {|
+type Props = {
   milestone: {
     name: string,
     dueDate: string,
@@ -31,9 +32,10 @@ type Props = {|
     },
     completedAt: string,
   },
-|};
+};
 
 const MilestoneCard = ({ milestone, ...rest }: Props) => {
+  const { user } = useUser();
   const { name, dueDate, taskCount = {}, completedAt } = milestone;
   const { count, remain, inProgress, skipped, completed, delayed } = taskCount;
 
@@ -53,7 +55,7 @@ const MilestoneCard = ({ milestone, ...rest }: Props) => {
             }
             input={
               <Display>
-                <FormattedDate value={dueDate} />
+                <FormattedDateTZ value={dueDate} user={user} />
               </Display>
             }
           />
