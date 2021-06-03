05fe007d28278a677ef7fff16c47b682e710dd6f
Hank Wang <467745+hanksudo@users.noreply.github.com>
2021-06-03 17:26:11 +0900

    feat: revise password validation Localization

--- src/modules/profile/security/validator.js
+++ src/modules/profile/security/validator.js
@@ -1,43 +1,42 @@
 // @flow
 import * as React from 'react';
-import { FormattedMessage } from 'react-intl';
+import { type IntlShape, FormattedMessage } from 'react-intl';
 import * as Yup from 'yup';
 
 const passwordValidationRule = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,})/;
-const validator: Object = Yup.object().shape({
-  currentPassword: Yup.string()
-    .required()
-    .min(6),
-  newPassword: Yup.string()
-    .required()
-    .matches(passwordValidationRule, () => {
-      return (
-        <FormattedMessage
-          id="modules.profile.passwordInvalid"
-          defaultMessage="Must Contain 8 Characters, One Uppercase, One Lowercase and One Number"
-        />
-      );
-    }),
-  confirmPassword: Yup.string()
-    .required()
-    .matches(passwordValidationRule, () => {
-      return (
-        <FormattedMessage
-          id="modules.profile.passwordInvalid"
-          defaultMessage="Must Contain 8 Characters, One Uppercase, One Lowercase and One Number"
-        />
-      );
-    })
-    .oneOf(
-      // $FlowFixMe type not support yet
-      [Yup.ref('newPassword'), null],
-      ((
-        <FormattedMessage
-          id="modules.profile.passwordNotMatch"
-          defaultMessage="Password don't match"
-        />
-      ): any)
-    ),
-});
+const validator = (intl: IntlShape): Object =>
+  Yup.object().shape({
+    currentPassword: Yup.string()
+      .required()
+      .min(6),
+    newPassword: Yup.string()
+      .required()
+      .matches(
+        passwordValidationRule,
+        intl.formatMessage({
+          id: 'modules.profile.passwordInvalid',
+          defaultMessage: 'Must Contain 8 Characters, One Uppercase, One Lowercase and One Number',
+        })
+      ),
+    confirmPassword: Yup.string()
+      .required()
+      .matches(
+        passwordValidationRule,
+        intl.formatMessage({
+          id: 'modules.profile.passwordInvalid',
+          defaultMessage: 'Must Contain 8 Characters, One Uppercase, One Lowercase and One Number',
+        })
+      )
+      .oneOf(
+        // $FlowFixMe type not support yet
+        [Yup.ref('newPassword'), null],
+        ((
+          <FormattedMessage
+            id="modules.profile.passwordNotMatch"
+            defaultMessage="Password don't match"
+          />
+        ): any)
+      ),
+  });
 
 export default validator;
--- src/modules/resetPassword/validator.js
+++ src/modules/resetPassword/validator.js
@@ -1,40 +1,39 @@
 // @flow
 import * as Yup from 'yup';
-import { FormattedMessage } from 'react-intl';
+import { type IntlShape, FormattedMessage } from 'react-intl';
 import * as React from 'react';
 
 const passwordValidationRule = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,})/;
-const validator: Object = Yup.object().shape({
-  password: Yup.string()
-    .required()
-    .matches(passwordValidationRule, () => {
-      return (
-        <FormattedMessage
-          id="modules.resetPassword.passwordInvalid"
-          defaultMessage="Must Contain 8 Characters, One Uppercase, One Lowercase and One Number"
-        />
-      );
-    }),
-  confirmPassword: Yup.string()
-    .required()
-    .matches(passwordValidationRule, () => {
-      return (
-        <FormattedMessage
-          id="modules.resetPassword.passwordInvalid"
-          defaultMessage="Must Contain 8 Characters, One Uppercase, One Lowercase and One Number"
-        />
-      );
-    })
-    .oneOf(
-      // $FlowFixMe type not support yet
-      [Yup.ref('password'), null],
-      ((
-        <FormattedMessage
-          id="modules.resetPassword.passwordNotMatch"
-          defaultMessage="Password don't match"
-        />
-      ): any)
-    ),
-});
+const validator = (intl: IntlShape): Object =>
+  Yup.object().shape({
+    password: Yup.string()
+      .required()
+      .matches(
+        passwordValidationRule,
+        intl.formatMessage({
+          id: 'modules.resetPassword.passwordInvalid',
+          defaultMessage: 'Must Contain 8 Characters, One Uppercase, One Lowercase and One Number',
+        })
+      ),
+    confirmPassword: Yup.string()
+      .required()
+      .matches(
+        passwordValidationRule,
+        intl.formatMessage({
+          id: 'modules.resetPassword.passwordInvalid',
+          defaultMessage: 'Must Contain 8 Characters, One Uppercase, One Lowercase and One Number',
+        })
+      )
+      .oneOf(
+        // $FlowFixMe type not support yet
+        [Yup.ref('password'), null],
+        ((
+          <FormattedMessage
+            id="modules.resetPassword.passwordNotMatch"
+            defaultMessage="Password don't match"
+          />
+        ): any)
+      ),
+  });
 
 export default validator;
