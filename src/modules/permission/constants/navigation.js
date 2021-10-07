5f9205129b9806671447279962c3470987a4e448
Hank Wang <467745+hanksudo@users.noreply.github.com>
2021-10-07 15:57:10 +0900

    feat: init navigation permissions

---
+++ src/modules/permission/constants/navigation.js
@@ -0,0 +1,38 @@
+// @flow
+
+export const NAVIGATION_ORDERS_MAP = 'navigation.orders.map';
+export const NAVIGATION_ORDERS_TABLE = 'navigation.orders.table';
+export const NAVIGATION_ORDERS_CARD = 'navigation.orders.card';
+
+export const NAVIGATION_ORDER_ITEMS_CARD = 'navigation.orderitems.card';
+
+export const NAVIGATION_BATCH_TABLE = 'navigation.batch.table';
+export const NAVIGATION_BATCH_CARD = 'navigation.batch.card';
+
+export const NAVIGATION_SHIPMENTS_MAP = 'navigation.shipments.map';
+export const NAVIGATION_SHIPMENTS_TABLE = 'navigation.shipments.table';
+export const NAVIGATION_SHIPMENTS_TABLE_ΒETA = 'navigation.shipments.tableβ';
+export const NAVIGATION_SHIPMENTS_CARD = 'navigation.shipments.card';
+
+export const NAVIGATION_CONTAINERS_CARD = 'navigation.containers.card';
+
+export const NAVIGATION_PRODUCTS_CARD = 'navigation.products.card';
+
+export const NAVIGATION_PROJECTS_TABLE = 'navigation.projects.table';
+export const NAVIGATION_PROJECTS_TABLEΒ = 'navigation.projects.tableβ';
+export const NAVIGATION_PROJECTS_CARD = 'navigation.projects.card';
+
+export const NAVIGATION_TASKS_CARD = 'navigation.tasks.card';
+
+export const NAVIGATION_DOCUMENTS_CARD = 'navigation.documents.card';
+
+export const NAVIGATION_NETWORK_WAREHOUSES = 'navigation.network.warehouses';
+export const NAVIGATION_NETWORK_PARTNERS = 'navigation.network.partners';
+export const NAVIGATION_NETWORK_USERS = 'navigation.network.users';
+
+export const NAVIGATION_TEMPLATES_CUSTOMFILEDS = 'navigation.templates.customFileds';
+export const NAVIGATION_TEMPLATES_TABLE = 'navigation.templates.table';
+export const NAVIGATION_TEMPLATES_TASKS = 'navigation.templates.tasks';
+export const NAVIGATION_TEMPLATES_PROJECTS = 'navigation.templates.projects';
+
+export const NAVIGATION_TAGS_CARD = 'navigation.tags.card';
