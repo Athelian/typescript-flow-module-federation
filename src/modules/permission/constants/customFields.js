d53811c7752d243d0556d80bc734f592db92e12c
Hank Wang <467745+hanksudo@users.noreply.github.com>
2021-10-08 00:40:50 +0900

    feat: remove unused permissions

--- src/modules/permission/constants/customFields.js
+++ src/modules/permission/constants/customFields.js
@@ -1,10 +1,7 @@
 // @flow
 
-export const CUSTOM_FIELD_DEFINITIONS_LIST = 'customFields.fieldDefinitions.list';
 export const CUSTOM_FIELD_DEFINITIONS_CREATE = 'customFields.fieldDefinitions.create';
 export const CUSTOM_FIELD_DEFINITIONS_UPDATE = 'customFields.fieldDefinitions.update';
 export const CUSTOM_FIELD_DEFINITIONS_DELETE = 'customFields.fieldDefinitions.delete';
-export const CUSTOM_FIELD_MASKS_LIST = 'customFields.masks.list';
-export const CUSTOM_FIELD_MASKS_FORM = 'customFields.masks.form';
 export const CUSTOM_FIELD_MASKS_CREATE = 'customFields.masks.create';
 export const CUSTOM_FIELD_MASKS_UPDATE = 'customFields.masks.update';
