44e27b45c1c6739d4983c97313679d5089a7c585
Erik Eggers <em.eggers@gmail.com>
2021-10-20 20:08:11 -0400

    fix: remove comments

--- src/modules/order/form/components/DocumentsSection/index.js
+++ src/modules/order/form/components/DocumentsSection/index.js
@@ -51,17 +51,6 @@ type Props = {
 function DocumentsSection({ isLoading, entityId, type }: Props) {
   const { isOwner } = usePartnerPermission();
   const { hasPermission } = usePermission(isOwner);
-  // const canUpload = hasPermission([ORDER_SET_DOCUMENTS, ORDER_DOCUMENT_CREATE, DOCUMENT_CREATE]);
-  // const canAddOrphan = hasPermission([ORDER_SET_DOCUMENTS, ORDER_UPDATE]);
-  // const canViewForm = hasPermission(ORDER_DOCUMENT_FORM);
-  // const canDownload = hasPermission(ORDER_DOWNLOAD_DOCUMENTS);
-  // const canChangeType = hasPermission([
-  //   ORDER_SET_DOCUMENTS,
-  //   DOCUMENT_SET_TYPE,
-  //   ORDER_DOCUMENT_SET_TYPE,
-  //   DOCUMENT_UPDATE,
-  // ]);
-  // const canDelete = hasPermission([ORDER_SET_DOCUMENTS, ORDER_DOCUMENT_DELETE, DOCUMENT_DELETE]);
 
   let canDelete = true;
   let canUpload = true;
