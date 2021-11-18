b73b06fea3d171b7908e2facf95b50891770b1a2
Junichi <7353004+junichimi@users.noreply.github.com>
2021-11-18 16:38:47 +0800

    feat(custom-fields): add date input option on custom fields

--- src/components/Form/Inputs/Styles/DefaultStyle/DefaultCustomFieldDefinitionStyle/index.js
+++ src/components/Form/Inputs/Styles/DefaultStyle/DefaultCustomFieldDefinitionStyle/index.js
@@ -1,9 +1,17 @@
 // @flow
 import * as React from 'react';
-import { FormattedMessage } from 'react-intl';
+import { formatToDateInput, isValidDate } from 'utils/date';
 import Icon from 'components/Icon';
 import { FormField } from 'modules/form';
-import { Label, TextInput, DefaultStyle, Display } from 'components/Form';
+import {
+  Label,
+  TextInput,
+  DateInput,
+  SelectInput,
+  DefaultStyle,
+  DefaultSelect,
+  DefaultOptions,
+} from 'components/Form';
 import {
   DefaultCustomFieldDefinitionWrapperStyle,
   DraggingIconStyle,
@@ -14,11 +22,13 @@ import {
 type OptionalProps = {
   editable: boolean,
   deletable: boolean,
+  isNew: boolean,
   onRemove: Function,
 };
 
 type Props = OptionalProps & {
   fieldName: any,
+  fieldType: string,
   targetName: string,
   setFieldValue: Function,
   dragHandleProps?: any,
@@ -32,25 +42,49 @@ const defaultProps = {
 
 const DefaultCustomFieldDefinitionStyle = ({
   fieldName,
+  fieldType,
   dragHandleProps,
   targetName,
   setFieldValue,
+  isNew,
   editable,
   deletable,
   onRemove,
-}: Props) => (
-  <div className={DefaultCustomFieldDefinitionWrapperStyle}>
-    {editable ? (
-      <div className={DraggingIconStyle} {...dragHandleProps}>
-        <Icon icon="DRAG_HANDLE" />
-      </div>
-    ) : (
-      <div className={CustomFieldIconStyle}>
-        <Icon icon="METADATA" />
-      </div>
-    )}
+}: Props) => {
+  const inputField = React.useMemo(() => {
+    if (!editable) {
+      return <Label width="200px">{fieldName}</Label>;
+    }
 
-    {editable ? (
+    if (fieldType === 'Date') {
+      const newDateValue = formatToDateInput(
+        isValidDate(fieldName) ? new Date() : formatToDateInput(new Date())
+      );
+
+      return (
+        <FormField
+          name={`${targetName}.name`}
+          initValue={newDateValue}
+          setFieldValue={setFieldValue}
+        >
+          {({ name, ...inputHandlers }) => {
+            const { isFocused, isTouched, errorMessage, ...rest } = inputHandlers;
+            return (
+              <DefaultStyle
+                width="200px"
+                isFocused={isFocused}
+                hasError={isTouched && errorMessage}
+                type="label"
+              >
+                <DateInput name={name} width="200px" {...rest} />
+              </DefaultStyle>
+            );
+          }}
+        </FormField>
+      );
+    }
+
+    return (
       <FormField name={`${targetName}.name`} initValue={fieldName} setFieldValue={setFieldValue}>
         {({ name, ...inputHandlers }) => {
           const { isFocused, isTouched, errorMessage, ...rest } = inputHandlers;
@@ -66,24 +100,61 @@ const DefaultCustomFieldDefinitionStyle = ({
           );
         }}
       </FormField>
-    ) : (
-      <Label width="200px">{fieldName}</Label>
-    )}
+    );
+  }, [editable, setFieldValue, targetName, fieldType, fieldName]);
 
-    <Display width="200px" height="30px" color="GRAY_LIGHT" style={{ userSelect: 'none' }}>
-      <FormattedMessage
-        id="components.inputs.customFieldDefinitionPlaceholder"
-        defaultMessage="Value will be entered here"
-      />
-    </Display>
+  return (
+    <div className={DefaultCustomFieldDefinitionWrapperStyle}>
+      {editable ? (
+        <div className={DraggingIconStyle} {...dragHandleProps}>
+          <Icon icon="DRAG_HANDLE" />
+        </div>
+      ) : (
+        <div className={CustomFieldIconStyle}>
+          <Icon icon="METADATA" />
+        </div>
+      )}
 
-    {deletable && (
-      <button className={RemoveButtonStyle} onClick={onRemove} type="button">
-        <Icon icon="REMOVE" />
-      </button>
-    )}
-  </div>
-);
+      {inputField}
+
+      <FormField name={`${targetName}.type`} initValue={fieldType} setFieldValue={setFieldValue}>
+        {({ name, ...inputHandlers }) => {
+          return (
+            <SelectInput
+              {...inputHandlers}
+              onChange={newFieldType => {
+                setFieldValue(name, newFieldType);
+                setFieldValue(
+                  `${targetName}.name`,
+                  newFieldType === 'Date' ? formatToDateInput(new Date()) : ''
+                );
+              }}
+              name={name}
+              items={['Text', 'Date']}
+              renderSelect={({ ...rest }) => (
+                <DefaultSelect required width="200px" height="30px" {...rest} />
+              )}
+              renderOptions={({ ...rest }) => <DefaultOptions width="200px" {...rest} />}
+              itemToString={item => item || ''}
+              itemToValue={item => item}
+              type="label"
+              align="left"
+              readOnlyWidth="200px"
+              readOnlyHeight="30px"
+              readOnly={!isNew || !editable}
+            />
+          );
+        }}
+      </FormField>
+
+      {deletable && (
+        <button className={RemoveButtonStyle} onClick={onRemove} type="button">
+          <Icon icon="REMOVE" />
+        </button>
+      )}
+    </div>
+  );
+};
 
 DefaultCustomFieldDefinitionStyle.defaultProps = defaultProps;
 
