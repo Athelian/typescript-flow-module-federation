50663d09ba409951257bf2175f57e82d3cbed0eb
Junichi <7353004+junichimi@users.noreply.github.com>
2021-12-03 11:04:36 +0800

    fix(table): adds missing summary fragments

--- src/modules/sheet/batch/fragment.js
+++ src/modules/sheet/batch/fragment.js
@@ -46,12 +46,18 @@ export const sheetBatchFragment = gql`
       ...forbiddenFragment
     }
     todo {
-      tasks {
+      tasks @skip(if: $isSummary) {
         ...taskWithoutParentInfoFragment
+        __typename
       }
-      taskTemplate {
+      taskTemplate @skip(if: $isSummary) {
         ...taskTemplateCardFragment
+        __typename
       }
+      tasks @include(if: $isSummary) {
+        ...taskInfoSummaryFragment
+      }
+      __typename
     }
     sort
     containerSort
