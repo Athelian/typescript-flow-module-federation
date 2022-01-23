d0d34dc2af62365dc2d0f3d55dcd9a8ecb0d38d6
Eliot <ejaustinforbes@gmail.com>
2022-01-23 18:12:40 +0900

    app1 deploy

--- app1/src/App.js
+++ app1/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Adapter from './adapter';
 
-const RemoteButton = React.lazy(() => import('app2/Button'));
+// const RemoteButton = React.lazy(() => import('app2/Button'));
 
 // const ModernComponent = React.lazy(() => import("app2/ModernComponent"));
 // Hooks not suppoorted, uncomment to verify this is a pre-hooks react version being used.
@@ -24,18 +24,18 @@ class App extends React.Component {
         <h1>Basic Host-Remote</h1>
         <h2>App 1, Uses react version not compatible with hooks</h2>
         <input onChange={this.setValue} placeholder="Type something into this input" />
-        <Adapter
+        {/* <Adapter
           // any other props, passed to ModernComponent
           {...this.state}
           importer={() => import('app2/ModernComponent')}
         >
           <h3>And these are children passed into it from the legacy app</h3>
-        </Adapter>
+        </Adapter> */}
         {/*This will Fail*/}
         {/*<HookComponent/>*/}
-        <React.Suspense fallback="Loading Button">
-          <RemoteButton />
-        </React.Suspense>
+        {/* <React.Suspense fallback="Loading Button"> */}
+          {/* <RemoteButton /> */}
+        {/* </React.Suspense> */}
         {/*This will fail without Adapter*/}
         {/*<React.Suspense fallback="Loading Button">*/}
         {/*  <ModernComponent />*/}
