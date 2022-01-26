7763514093af91a17097e6bf9dd64c2a5f6c1817
Eliot <ejaustinforbes@gmail.com>
2022-01-26 18:00:39 +0900

    fix(apollo-client): cache does not need clearing
    see: https://tinyurl.com/ym788s63, caches do not persist between tabs

--- src/components/DeployNotifier/index.js
+++ src/components/DeployNotifier/index.js
@@ -2,7 +2,6 @@
 import * as React from 'react';
 import { injectIntl, type IntlShape } from 'react-intl';
 import firebase from 'firebase';
-import apolloClient from 'apollo';
 import * as serviceWorker from 'serviceWorker';
 
 type Props = {
@@ -23,8 +22,6 @@ const DeployNotifier = ({ revision, revisionKey, intl }: Props) => {
 
       if (revision !== currentRevision) {
         serviceWorker.unregister();
-        // refer apollo client doc https://www.apollographql.com/docs/react/recipes/authentication#login-logouts
-        apolloClient.resetStore();
       }
     });
   }, [intl, intl.locale, revision, revisionKey]);
