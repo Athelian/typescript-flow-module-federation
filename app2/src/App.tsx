2bff091cee69b8f3e858052bea5bf892035e5d08
Eliot Austin-Forbes <ejaustinforbes@gmail.com>
2022-06-19 18:55:13 +0900

    chore(update): update typescript prop

--- app2/src/App.tsx
+++ app2/src/App.tsx
@@ -5,7 +5,7 @@ const App = () => (
   <div>
     <h1>Basic Host-Remote</h1>
     <h2>App 2, uses TypeScript React</h2>
-    <LocalButton text="Hi this text was sent from Flow" />
+    <LocalButton text="Hi this text was sent from Typescript" />
   </div>
 );
 
